7bc89d0df7ba424bea2ce0089f27142490d5c52b
James Smith <james@floppy.org.uk>
2021-01-30 18:23:33 +0000

    new rails app woohoo
    Generated with railsnew.io:
    `rails new van_dam --skip-action-mailbox --skip-action-text --skip-system-test --skip-test --template https://www.railsbytes.com/script/zyvso6`

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,14 @@
+// Load all the controllers within this directory and all subdirectories. 
+// Controller files must be named *_controller.js.
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
+
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+StimulusReflex.debug = process.env.RAILS_ENV === 'development'
