a8f87cf03fe70ac132210bc1c6343808aac6e105
James Smith <james@floppy.org.uk>
2021-02-06 23:37:13 +0000

    auto-group parts

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -2,5 +2,6 @@ class ModelsController < ApplicationController
   def show
     @library = Library.find(params[:library_id])
     @model = @library.models.find(params[:id])
+    @groups = @model.parts.group_by{ |i| i.filename.split(/[\ _\-:\.]/)[0] }
   end
 end
