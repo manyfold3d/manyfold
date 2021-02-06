b0f5fac4fc26dee8d16d43b2778adec87ba9eeb2
James Smith <james@floppy.org.uk>
2021-02-06 21:56:31 +0000

    add models controller to show model details

---
+++ app/controllers/models_controller.rb
@@ -0,0 +1,8 @@
+class ModelsController < ApplicationController
+
+
+  def show
+    @library = Library.find(params[:library_id])
+    @model = @library.models.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :libraries, only: [:index, :show]
+  resources :libraries, only: [:index, :show] do
+    resources :models, only: [:show]
+  end
 end
