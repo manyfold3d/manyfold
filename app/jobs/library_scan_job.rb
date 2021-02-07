c0d42d0696a07f73dac5d164e38c88f745951043
James Smith <james@floppy.org.uk>
2021-02-07 23:34:00 +0000

    handle thinigverse download folder structure

--- app/jobs/library_scan_job.rb
+++ app/jobs/library_scan_job.rb
@@ -3,12 +3,16 @@ class LibraryScanJob < ApplicationJob
 
   def perform(library)
     # For each directory in the library, create a model
-    all_3d_files = Dir.glob(File.join(library.path,'**','*.stl'))
-    model_folders = all_3d_files.map{ |f| File.dirname(f) }.uniq
+    all_3d_files = Dir.glob(File.join(library.path, "**", "*.stl"))
+    model_folders = all_3d_files.map { |f| File.dirname(f) }.uniq
     model_folders.each do |path|
-      relative_path =  path.gsub(library.path,"")
+      title = relative_path = path.gsub(library.path, "")
+      if title.ends_with?("/files")
+        title = title.gsub("/files", "")
+      end
+      title = File.basename(title)
       next if relative_path.blank? # For now, ignore files in the root
-      model = library.models.find_or_create_by(name: File.basename(relative_path).humanize.titleize, path: relative_path)
+      model = library.models.find_or_create_by(name: title.humanize.titleize, path: relative_path)
       ModelScanJob.perform_later(model)
     end
   end
--- spec/jobs/library_scan_job_spec.rb
+++ spec/jobs/library_scan_job_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe LibraryScanJob, type: :job do
   end
 
   it "can scan a library directory" do
-    expect { LibraryScanJob.perform_now(library) }.to change { library.models.count }.to(2)
-    expect(library.models.map(&:name)).to match_array ["Model One", "Model Two"]
-    expect(library.models.map(&:path)).to match_array ["/model_one", "/subfolder/model_two"]
+    expect { LibraryScanJob.perform_now(library) }.to change { library.models.count }.to(3)
+    expect(library.models.map(&:name)).to match_array ["Model One", "Model Two", "Thingiverse Model"]
+    expect(library.models.map(&:path)).to match_array ["/model_one", "/subfolder/model_two", "/thingiverse_model/files"]
   end
 
   it "queues up model scans" do
-    expect { LibraryScanJob.perform_now(library) }.to have_enqueued_job(ModelScanJob).twice
+    expect { LibraryScanJob.perform_now(library) }.to have_enqueued_job(ModelScanJob).exactly(3).times
   end
 end
