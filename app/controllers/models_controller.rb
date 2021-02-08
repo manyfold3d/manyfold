3c1db203090399212988885f9df4b5aae65f365c
James Smith <james@floppy.org.uk>
2021-02-08 13:53:24 +0000

    DRY up part rendering

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -3,5 +3,11 @@ class ModelsController < ApplicationController
     @library = Library.find(params[:library_id])
     @model = @library.models.find(params[:id])
     @groups = @model.parts.group_by { |i| i.filename.split(/[\ _\-:.]/)[0] }
+    @ungrouped = []
+    @groups.each_pair do |group, parts|
+      if parts.count == 1
+        @ungrouped << @groups.delete(group)[0]
+      end
+    end
   end
 end
