2b4e48b25422b3dd5560d9434c1660dffef1d2f9
James Smith <james@floppy.org.uk>
2021-02-08 14:36:53 +0000

    move model grouping into helper

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -2,12 +2,6 @@ class ModelsController < ApplicationController
   def show
     @library = Library.find(params[:library_id])
     @model = @library.models.find(params[:id])
-    @groups = @model.parts.group_by { |i| i.filename.split(/[\ _\-:.]/)[0] }
-    @ungrouped = []
-    @groups.each_pair do |group, parts|
-      if parts.count == 1
-        @ungrouped << @groups.delete(group)[0]
-      end
-    end
+    @groups = helpers.group(@model.parts)
   end
 end
--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -1,2 +1,11 @@
 module ModelsHelper
+  def group(parts)
+    groups = parts.group_by { |i| i.filename.split(/[\ _\-:.]/)[0] }
+    groups[nil] = []
+    groups.each_pair do |group, parts|
+      if parts.count == 1
+        groups[nil] << groups.delete(group)[0]
+      end
+    end
+  end
 end
