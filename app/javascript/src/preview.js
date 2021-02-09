dae88a8512389981b18c888eb6864a42a982e72f
James Smith <james@floppy.org.uk>
2021-02-09 11:51:03 +0000

    autocentre objects in view

--- app/javascript/src/preview.js
+++ app/javascript/src/preview.js
@@ -5,7 +5,7 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 export function preview(canvas) {
 
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
+  var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
   camera.position.z = 50;
 
   var renderer = new THREE.WebGLRenderer({canvas, alpha: true});
@@ -13,7 +13,7 @@ export function preview(canvas) {
   const objects = new THREE.Group();
   scene.add(objects);
 
-  const light = new THREE.AmbientLight(0x404040); // soft white light
+  const light = new THREE.AmbientLight(0x7f7f7f); // soft white light
   scene.add(light);
 
   const directionalLight = new THREE.DirectionalLight({position: new THREE.Vector3( 0, 1, 1 )});
@@ -36,7 +36,17 @@ export function preview(canvas) {
       geometry = model.geometry;
     else if (canvas.dataset.format === "stl")
       geometry = model;
-    objects.add( new THREE.Mesh(geometry, material) );
+    const mesh = new THREE.Mesh(geometry, material);
+    const bbox = new THREE.Box3().setFromObject(mesh);
+    const bsphere = new THREE.Sphere();
+    bbox.getBoundingSphere(bsphere);
+    camera.position.z = bsphere.radius * 1.2;
+    camera.position.y = bsphere.radius * 0.5;
+    camera.lookAt(0, 0, 0);
+    const centre = new THREE.Vector3();
+    bbox.getCenter(centre);
+    mesh.position.set(-centre.x, -centre.y, -centre.z);
+    objects.add( mesh );
   }, undefined, function ( error ) {
     console.error( error );
   } );
@@ -44,7 +54,8 @@ export function preview(canvas) {
   var animate = function() {
     requestAnimationFrame(animate);
 
-    objects.rotation.y += 0.01;
+    objects.rotation.z += 0.01;
+    objects.rotation.x = -1.57;
 
     renderer.render(scene, camera);
   };
