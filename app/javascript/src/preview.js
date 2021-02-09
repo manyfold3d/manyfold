e24b9796beed14c4f99839e604856a07d215d0fd
James Smith <james@floppy.org.uk>
2021-02-09 18:33:08 +0000

    lint js

--- app/javascript/src/preview.js
+++ app/javascript/src/preview.js
@@ -19,20 +19,20 @@ export function preview (canvas) {
   let loader = null
   if (canvas.dataset.format === 'obj') { loader = new OBJLoader() } else if (canvas.dataset.format === 'stl') { loader = new STLLoader() }
 
-  const gridHelper = new THREE.GridHelper(260,26,"magenta","cyan");
+  const gridHelper = new THREE.GridHelper(260, 26, 'magenta', 'cyan')
   objects.add(gridHelper)
 
-  let geometry = null;
+  let geometry = null
   loader.load(canvas.dataset.previewUrl, function (model) {
     if (canvas.dataset.format === 'obj') { geometry = model.geometry } else if (canvas.dataset.format === 'stl') { geometry = model }
     // Create mesh and transform to screen coords from print
-    const coord_system_transform = new THREE.Matrix4();
-    coord_system_transform.set(
+    const coordSystemTransform = new THREE.Matrix4()
+    coordSystemTransform.set(
       1, 0, 0, 0, // x -> x
       0, 0, 1, 0, // z -> y
       0, -1, 0, 0, // y -> -z
-      0, 0, 0, 1);
-    const mesh = new THREE.Mesh(geometry.applyMatrix4(coord_system_transform), material)
+      0, 0, 0, 1)
+    const mesh = new THREE.Mesh(geometry.applyMatrix4(coordSystemTransform), material)
     // Calculate bounding volumes
     const bbox = new THREE.Box3().setFromObject(mesh)
     const centre = new THREE.Vector3()
@@ -43,7 +43,7 @@ export function preview (canvas) {
     // Configure camera
     camera.position.z = bsphere.radius * 2.3
     camera.position.y = bsphere.radius * 0.75
-    camera.lookAt(0,modelheight/2,0)
+    camera.lookAt(0, modelheight / 2, 0)
     // Centre the model
     mesh.position.set(-centre.x, -bbox.min.y, -centre.z)
     objects.add(mesh)
@@ -56,8 +56,7 @@ export function preview (canvas) {
       objects.rotation.y += 0.01
       renderer.render(scene, camera)
       window.requestAnimationFrame(animate)
-    }
-    else {
+    } else {
       gridHelper.geometry.dispose()
       material.dispose()
       geometry.dispose()
@@ -70,7 +69,7 @@ export function preview (canvas) {
 
 document.addEventListener('turbolinks:load', () => {
   document.querySelectorAll('canvas[data-preview]').forEach((canvas) => {
-    canvas.height = canvas.width;
+    canvas.height = canvas.width
     preview(canvas)
   })
 })
