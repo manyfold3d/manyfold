7345a599b582170bc9bd0984f526fa649940ea24
James Smith <james@floppy.org.uk>
2021-02-09 21:52:20 +0000

    destroy dependent models

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,5 @@
 class Library < ApplicationRecord
-  has_many :models
+  has_many :models, dependent: :destroy
   validates :path, presence: true, uniqueness: true
 
   def name
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,6 @@
 class Model < ApplicationRecord
   belongs_to :library
-  has_many :parts
+  has_many :parts, dependent: :destroy
   validates :name, presence: true
   validates :path, presence: true, uniqueness: {scope: :library}
 end
