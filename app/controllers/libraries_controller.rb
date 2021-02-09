dc62d68f0bdc36a915e420c2e0a332ced219a476
James Smith <james@floppy.org.uk>
2021-02-09 21:52:42 +0000

    create new libraries and rescan existing ones

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,9 +1,37 @@
 class LibrariesController < ApplicationController
   def index
-    @libraries = Library.all
+    if Library.count === 0
+      redirect_to new_library_path
+    else
+      redirect_to Library.first
+    end
   end
 
   def show
     @library = Library.find(params[:id])
   end
+
+  def new
+    @library = Library.new
+  end
+
+  def create
+    @library = Library.create(library_params)
+    LibraryScanJob.perform_later(@library)
+    redirect_to @library
+  end
+
+  def update
+    @library = Library.find(params[:id])
+    LibraryScanJob.perform_later(@library)
+    redirect_to @library
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:path)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "libraries#index"
-  resources :libraries, only: [:index, :show] do
+  resources :libraries, only: [:index, :show, :new, :create, :update] do
     resources :models, only: [:show] do
       resources :parts, only: [:show]
     end
