638a03ff509eef8a7d1c89c7137e62165daefdde
James Smith <james@floppy.org.uk>
2021-02-09 20:54:39 +0000

    root page is the library list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "libraries#index"
   resources :libraries, only: [:index, :show] do
     resources :models, only: [:show] do
       resources :parts, only: [:show]
