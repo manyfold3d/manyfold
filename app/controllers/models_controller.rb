92969cb8f5d54695871b9850400ea49034a8d981
James Smith <james@floppy.org.uk>
2021-02-23 00:38:48 +0000

    add button to set part as preview

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -4,4 +4,19 @@ class ModelsController < ApplicationController
     @model = @library.models.find(params[:id])
     @groups = helpers.group(@model.parts)
   end
+
+  def update
+    @library = Library.find(params[:library_id])
+    @model = @library.models.find(params[:id])
+    @model.update(model_params)
+    redirect_to [@library, @model]
+  end
+
+  private
+
+  def model_params
+    params.require(:model).permit([
+      :preview_part_id
+    ])
+  end
 end
