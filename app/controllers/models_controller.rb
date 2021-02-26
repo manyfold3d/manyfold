688bc60af5ceddc2b58748520f049893ddce7670
James Smith <james@floppy.org.uk>
2021-02-26 12:07:27 +0000

    DRY up model controller

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,13 +1,15 @@
 class ModelsController < ApplicationController
+  before_action :get_library
+  before_action :get_model
+
   def show
-    @library = Library.find(params[:library_id])
-    @model = @library.models.find(params[:id])
     @groups = helpers.group(@model.parts)
   end
 
+  def edit
+  end
+
   def update
-    @library = Library.find(params[:library_id])
-    @model = @library.models.find(params[:id])
     @model.update(model_params)
     redirect_to [@library, @model]
   end
@@ -18,5 +20,13 @@ class ModelsController < ApplicationController
     params.require(:model).permit([
       :preview_part_id
     ])
+  end
+
+  def get_library
+    @library = Library.find(params[:library_id])
+  end
+
+  def get_model
+    @model = @library.models.find(params[:id])
   end
 end
