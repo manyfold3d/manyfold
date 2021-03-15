07981166e3fd3ef682d658e18a28c9cba9a16ff1
James Smith <james@floppy.org.uk>
2021-03-15 22:51:05 +0000

    add database_cleaner to remove test interactions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 require "support/factory_bot"
+require "support/database_cleaner"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
