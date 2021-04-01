dc6c0a6fd1ede894d771fd7db5bce4f2160b74fd
James Smith <james@floppy.org.uk>
2021-04-01 22:05:18 +0100

    remove setting of cache_store
    This will cause problems with stumulus reflex, but I don't care yet.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.session_store :cache_store
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
