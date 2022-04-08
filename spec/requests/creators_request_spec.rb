f96203ef474e46252d9039ae0448e7d3f4a0a041
Matthew Page <matthewnpage@gmail.com>
2022-04-08 15:51:17 -0700

    add pagination to creators and fix tests for libraries

--- spec/requests/creators_request_spec.rb
+++ spec/requests/creators_request_spec.rb
@@ -1,4 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "Creators", type: :request do
+  before :all do
+    11.times do
+      FactoryBot.create(:creator) do |creator|
+          FactoryBot.create_list(:link, 1, linkable: creator)
+          FactoryBot.create_list(:model, 1, creator: creator)
+      end
+    end
+  end
+  describe "GET /creators?page=2" do
+    it "returns paginated creators" do
+      allow(Rails.application.config).to receive(:paginate_creators).and_return(true)
+      get "/creators?page=2"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to match(/pagination/)
+    end
+  end
 end
