3f73298014e2b79ab9e2928b301cf6bd271f5f18
Matthew Page <matthewnpage@gmail.com>
2022-04-15 11:15:11 -0700

    tslint fix

--- app/javascript/src/bulk_edit.ts
+++ app/javascript/src/bulk_edit.ts
@@ -1,14 +1,14 @@
-function handleSelectAllChange(event) {
-  event.preventDefault();
-  document.querySelectorAll('[data-bulk-item]').forEach((cb) => {
-    cb.checked = !cb.checked
-  });
+function handleSelectAllChange (event): void {
+  event.preventDefault()
+  document.querySelectorAll('[data-bulk-item]').forEach((cb): boolean => {
+    cb.checked = !(cb.checked as boolean)
+  })
 }
 
 document.addEventListener('turbolinks:load', () => {
-  const bulkSelector = document.querySelector('input[name="bulk_select_all"]');
-  if (bulkSelector) {
-    bulkSelector.removeEventListener("change", handleSelectAllChange);
-    bulkSelector.addEventListener("change", handleSelectAllChange);
+  const bulkSelector = document.querySelector('input[name="bulk_select_all"]')
+  if (bulkSelector != null) {
+    bulkSelector.removeEventListener('change', handleSelectAllChange)
+    bulkSelector.addEventListener('change', handleSelectAllChange)
   }
 })
