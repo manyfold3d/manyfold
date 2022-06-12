66ed3960ceb25e24a647de00278e510059dab3c1
James Smith <james@floppy.org.uk>
2022-06-13 00:10:03 +0100

    move webpack config somewhere tidy

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
-    path: path.resolve(__dirname, 'app/assets/builds')
+    path: path.resolve(__dirname, '..', '..', 'app/assets/builds')
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
