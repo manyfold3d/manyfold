147b4122d62cee3a4d181d24f1296b30317e3587
James Smith <james@floppy.org.uk>
2022-06-14 22:54:47 +0100

    require login, but automatically log in if there is a single user with no password
    this is so that in the default single-user case, it all happens behind the scenes and the user doesn't need to know about passwords, etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :auto_login_single_user
+  before_action :authenticate_user!
+
+  def auto_login_single_user
+    # If there is a single user with no password set,
+    # then log in automatically as that user.
+    if User.count == 1 && User.first.encrypted_password == ""
+      sign_in(:user, User.first)
+    end
+  end
 end
