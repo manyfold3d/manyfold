0820022a0061df42f10f2a4f511da855ad79e936
James Smith <james@floppy.org.uk>
2022-06-14 21:54:01 +0100

    install devise and add basic generated configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "van_dam_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: "localhost", port: 5000}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
