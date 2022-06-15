7d7f346fe9520cb3e14379a77af2ca1bdfe40aff
James Smith <james@floppy.org.uk>
2022-06-15 08:46:36 +0100

    create default user for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
+  # Create the default admin user before tests
+  config.before(:suite) do
+    if User.count == 0
+      User.create(
+        username: "admin",
+        email: "nobody@example.com",
+        admin: true
+      )
+    end
+  end
 end
