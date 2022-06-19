3153a7d366d8439c5978ace1df384bbede4b2459
James Smith <james@floppy.org.uk>
2022-06-19 19:59:20 +0100

    add tag settings to settings page

--- app/models/site_settings.rb
+++ app/models/site_settings.rb
@@ -4,5 +4,5 @@ class SiteSettings < RailsSettings::Base
 
   field :model_tags_filter_stop_words, type: :boolean, default: true
   field :model_tags_stop_words_locale, type: :string, default: "en"
-  field :model_tags_custom_stop_words, type: :array, default: %w[stl]
+  field :model_tags_custom_stop_words, type: :array, default: Rails.configuration.formats.flatten(2).select { |x| x.is_a?(String) }
 end
