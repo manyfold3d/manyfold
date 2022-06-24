93bccff7421e7b5878e9bda182628ba67a177d50
James Smith <james@floppy.org.uk>
2022-06-24 23:58:07 +0100

    style log file display a little

--- app/admin/log.rb
+++ app/admin/log.rb
@@ -1,9 +1,12 @@
 # app/admin/calendar.rb
 ActiveAdmin.register_page "Log" do
   content do
-    pre do
-      File.readlines(Rails.root.join("log/#{Rails.env}.log")).split("\n") do |line|
-        text_node line
+    h2 "log/#{Rails.env}.log"
+    div style: "padding: 0px 1em; background: #eee; border: 1px solid black; width: 90vw; overflow-x: scroll" do
+      pre do
+        File.readlines(Rails.root.join("log/#{Rails.env}.log")).split("\n") do |line|
+          text_node line
+        end
       end
     end
   end
