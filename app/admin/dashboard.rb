aa2629a9d7dc3d954960173411b8467bf0f4a835
James Smith <james@floppy.org.uk>
2022-06-24 15:47:30 +0100

    display tasks
    using Delayed::Job code from https://pablofernandez.tech/2016/08/30/displaying-delayedjobs-jobs-in-active-admin/

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -8,8 +8,17 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Models" do
           ul do
-            Model.all.order(:created_at).limit(50).map do |model|
+            Model.all.order(:created_at).limit(20).map do |model|
               li link_to(model.name, admin_model_path(model))
+            end
+          end
+        end
+      end
+      column do
+        panel "Task Queue" do
+          ul do
+            Delayed::Job.all.order(:created_at).limit(20).map do |job|
+              li link_to(job.id, admin_task_path(job))
             end
           end
         end
---
+++ app/admin/delayed_job.rb
@@ -0,0 +1,121 @@
+ActiveAdmin.register Delayed::Job, as: "Task" do
+  actions :all, except: [:new]
+
+  collection_action :run_all, method: :post do
+    successes, failures = Delayed::Worker.new.work_off
+    if failures > 0
+      flash[:alert] = "#{failures} failed Tasks when running them."
+    end
+    if successes > 0
+      flash[:notice] = "#{successes} Tasks run successfully"
+    end
+    if failures == 0 && successes == 0
+      flash[:notice] = "Yawn... no Tasks run"
+    end
+    redirect_to admin_tasks_url
+    I18n.locale = :en # Running tasks can mess up the locale.
+  end
+
+  if Rails.env.development?
+    collection_action :delete_all, method: :post do
+      n = Delayed::Job.delete_all
+      redirect_to admin_tasks_url, notice: "#{n} tasks deleted."
+    end
+  end
+
+  collection_action :mark_all_for_re_run, method: :post do
+    n = Delayed::Job.update_all("run_at = created_at")
+    redirect_to admin_tasks_url, notice: "Marked all tasks (#{n}) for re-running."
+  end
+
+  member_action :run, method: :post do
+    task = Delayed::Job.find(params[:id])
+    begin
+      task.invoke_job
+      task.destroy
+      redirect_to admin_tasks_url, notice: "1 Task run, apparently successfully, but who knows!"
+    rescue => e
+      redirect_to admin_tasks_url, alert: "Failed to run a Task: #{e}"
+    end
+    I18n.locale = :en # Running Tasks can mess up the locale.
+  end
+
+  action_item :run do
+    links = link_to("Run All Tasks", run_all_admin_tasks_url, method: :post)
+    links += (" " + link_to("Mark all for re-run", mark_all_for_re_run_admin_tasks_url, method: :post)).html_safe
+    links += (" " + link_to("Delete All Tasks", delete_all_admin_tasks_url, method: :post)).html_safe if Rails.env.development?
+    links
+  end
+
+  index do
+    selectable_column
+    id_column
+    column "P", :priority
+    column "A", :attempts
+    column("Error", :last_error, sortable: :last_error) { |post| post.last_error.present? ? post.last_error.split('\n').first : "" }
+    column(:created_at, sortable: :created_at) { |job| job.created_at.iso8601.tr("T", " ") }
+    column(:run_at, sortable: :run_at) { |post| post.run_at.present? ? post.run_at.iso8601.tr("T", " ") : nil }
+    column :queue
+    column("Running", sortable: :locked_at) { |dj| dj.locked_at.present? ? "#{(Time.now - dj.locked_at).round(1)}s by #{dj.locked_by}" : "" }
+    actions
+  end
+
+  show title: ->(dj) { "Task #{dj.id}" } do |task|
+    panel "Task" do
+      attributes_table_for task do
+        row :id
+        row :priority
+        row :attempts
+        row :queue
+        row :run_at
+        row :locked_at
+        row :failed_at
+        row :locked_by
+        row :created_at
+        row :updated_at
+      end
+    end
+
+    panel "Handler" do
+      pre task.handler
+    rescue => e
+      div "Couldn't render handler: #{e.message}"
+    end
+
+    panel "Last error" do
+      pre task.last_error
+    rescue => e
+      div "Couldn't render last error: #{e.message}"
+    end
+  end
+
+  form do |f|
+    f.inputs("Task") do
+      f.input :id, input_html: {readonly: true}
+      f.input :priority
+      f.input :attempts
+      f.input :queue
+      f.input :created_at, input_html: {readonly: true}, as: :string
+      f.input :updated_at, input_html: {readonly: true}, as: :string
+      f.input :run_at, input_html: {readonly: true}, as: :string
+      f.input :locked_at, input_html: {readonly: true}, as: :string
+      f.input :failed_at, input_html: {readonly: true}, as: :string
+      f.input :locked_by, input_html: {readonly: true}
+    end
+
+    f.buttons
+  end
+
+  controller do
+    def update
+      @task = Delayed::Job.find(params[:id])
+      @task.assign_attributes(params[:task], without_protection: true)
+      if @task.save
+        redirect_to admin_tasks_url, notice: "Task #{@task} saved successfully"
+      else
+        render :edit
+      end
+      I18n.locale = :en # Running Tasks can mess up the locale.
+    end
+  end
+end
