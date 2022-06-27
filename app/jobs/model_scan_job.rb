1154ef5aff726b2009bf4d551e5998650d060b44
James Smith <james@floppy.org.uk>
2022-06-27 23:43:12 +0100

    don't try to use empty models after deletion

--- app/jobs/model_scan_job.rb
+++ app/jobs/model_scan_job.rb
@@ -39,11 +39,11 @@ class ModelScanJob < ApplicationJob
     model.model_files.select { |f|
       !File.exist?(File.join(model_path, f.filename))
     }.each(&:destroy)
-    # If this model has no files, self destruct
-    model.destroy if model.model_files.reload.count == 0
     # Set tags and default files
     model.model_files.reload
     model.preview_file = model.model_files.first unless model.preview_file
     model.autogenerate_tags_from_path! if model.tags.empty?
+    # If this model has no files, self destruct
+    model.destroy if model.model_files.reload.count == 0
   end
 end
