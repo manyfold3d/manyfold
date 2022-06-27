638d1fb5e3e2b8d8ad41cf1d322b2df0fc61c737
James Smith <james@floppy.org.uk>
2022-06-27 15:18:35 +0100

    don't retag if the model already has tags

--- app/jobs/model_scan_job.rb
+++ app/jobs/model_scan_job.rb
@@ -33,6 +33,6 @@ class ModelScanJob < ApplicationJob
     # Set tags and default files
     model.model_files.reload
     model.preview_file = model.model_files.first unless model.preview_file
-    model.autogenerate_tags_from_path!
+    model.autogenerate_tags_from_path! if model.tags.empty?
   end
 end
