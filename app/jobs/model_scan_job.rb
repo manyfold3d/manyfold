4fb596da21d232369c89b1ec4d76dd4253dfedad
James Smith <james@floppy.org.uk>
2022-06-27 15:17:54 +0100

    preserve preview file setting if already set

--- app/jobs/model_scan_job.rb
+++ app/jobs/model_scan_job.rb
@@ -32,7 +32,7 @@ class ModelScanJob < ApplicationJob
     }.each(&:destroy)
     # Set tags and default files
     model.model_files.reload
-    model.preview_file = model.model_files.first
+    model.preview_file = model.model_files.first unless model.preview_file
     model.autogenerate_tags_from_path!
   end
 end
