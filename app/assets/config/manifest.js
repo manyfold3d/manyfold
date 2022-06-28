aa62c4290fe7bb2c316e5df4eaa0735eea6fd52d
James Smith <james@floppy.org.uk>
2022-06-28 20:11:13 +0100

    run importmaps installer

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../../../node_modules/bootstrap-icons/icons .svg
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
