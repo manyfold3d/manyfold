ce20232980820147581b8b717de7ee9deeece2f8
James Smith <james@floppy.org.uk>
2022-06-28 20:14:26 +0100

    Move custom js code from packs to new importmap main file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
+
+import 'controllers'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'src/preview'
+import 'src/bulk_edit'
+import 'src/model_edit'
+
+import '@nathanvda/cocoon'
+import '@selectize/selectize/dist/js/standalone/selectize.min.js'
+import '@selectize/selectize/dist/css/selectize.bootstrap5.css'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
