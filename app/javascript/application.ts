f559a740312f5b17167f7b77b062969448dab02c
James Smith <james@floppy.org.uk>
2022-08-01 22:31:56 +0100

    include jQuery and selectize from HTML, outside esbuild

---
+++ app/javascript/application.ts
@@ -0,0 +1,16 @@
+// Entry point for the build script in your package.json
+
+import Rails from '@rails/ujs'
+
+import 'bootstrap'
+
+// JQuery is imported in the main layout just for these
+import '@nathanvda/cocoon'
+// I can't make this work, so it's included in the layout for now
+// import '@selectize/selectize/dist/js/standalone/selectize.js'
+
+import 'src/preview'
+import 'src/bulk_edit'
+import 'src/model_edit'
+
+Rails.start()
