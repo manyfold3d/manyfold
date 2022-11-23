e96bd06a50c7330cb253f984369a66af936c4f83
James Smith <james@floppy.org.uk>
2022-11-23 15:50:53 +0000

    add basic collection list and view, with links to models

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,9 @@
 class CollectionsController < ApplicationController
   def index
+    @collections = ActsAsTaggableOn::Tag.for_context(:collections)
   end
 
   def show
+    @collection = ActsAsTaggableOn::Tag.for_context(:collections).find(params[:id])
   end
 end
