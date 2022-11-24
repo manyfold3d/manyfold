2ac0d5ae527f4c0d51ed32117ef565aa1b970ab8
James Smith <james@floppy.org.uk>
2022-11-24 00:53:10 +0000

    show a proper model for each collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,5 +5,19 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = ActsAsTaggableOn::Tag.for_context(:collections).find(params[:id])
+    @models = Model.tagged_with(@collection, context: :collection)
+
+    # Ordering
+    @models = case session["order"]
+    when "recent"
+      @models.order(created_at: :desc)
+    else
+      @models.order(name: :asc)
+    end
+
+    if current_user.pagination_settings["models"]
+      page = params[:page] || 1
+      @models = @models.page(page).per(current_user.pagination_settings["per_page"])
+    end
   end
 end
