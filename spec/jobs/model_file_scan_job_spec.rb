dce70054a5eecd47ed1ab351ba5bb1396128dc7b
James Smith <james@floppy.org.uk>
2022-12-12 17:35:13 +0000

    lint: use be instead of eq

--- spec/jobs/model_file_scan_job_spec.rb
+++ spec/jobs/model_file_scan_job_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe ModelFileScanJob, type: :job do
     model_file = create(:model_file, filename: "file1_supported.stl")
     ModelFileScanJob.perform_now(model_file)
     model_file.reload
-    expect(model_file.presupported).to eq true
+    expect(model_file.presupported).to be true
   end
 
   it "detects if file is unsupported" do
     model_file = create(:model_file)
     ModelFileScanJob.perform_now(model_file)
     model_file.reload
-    expect(model_file.presupported).to eq false
+    expect(model_file.presupported).to be false
   end
 end
