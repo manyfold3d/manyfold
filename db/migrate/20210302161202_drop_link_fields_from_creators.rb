8d43df1771dd19d36b3e114cdbcdda64ce605cf2
James Smith <james@floppy.org.uk>
2022-12-12 17:25:57 +0000

    lint: fix non-reversible migration

--- db/migrate/20210302161202_drop_link_fields_from_creators.rb
+++ db/migrate/20210302161202_drop_link_fields_from_creators.rb
@@ -1,8 +1,8 @@
 class DropLinkFieldsFromCreators < ActiveRecord::Migration[6.1]
   def change
-    remove_column :creators, :thingiverse_user
-    remove_column :creators, :cults3d_user
-    remove_column :creators, :mmf_user
-    remove_column :creators, :cgtrader_user
+    remove_column :creators, :thingiverse_user, :string
+    remove_column :creators, :cults3d_user, :string
+    remove_column :creators, :mmf_user, :string
+    remove_column :creators, :cgtrader_user, :string
   end
 end
