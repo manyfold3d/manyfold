b4a1fdf91939c4237658d2332c25d6ef035b8909
James Smith <james@floppy.org.uk>
2022-12-12 17:36:15 +0000

    lint: use create list instead of x.times

--- spec/requests/creators_request_spec.rb
+++ spec/requests/creators_request_spec.rb
@@ -2,13 +2,12 @@ require "rails_helper"
 
 RSpec.describe "Creators", type: :request do
   before :all do
-    13.times do
-      FactoryBot.create(:creator) do |creator|
-        FactoryBot.create_list(:link, 1, linkable: creator)
-        FactoryBot.create_list(:model, 1, creator: creator)
-      end
+    FactoryBot.create_list(:creator, 13) do |creator|
+      FactoryBot.create_list(:link, 1, linkable: creator)
+      FactoryBot.create_list(:model, 1, creator: creator)
     end
   end
+
   describe "GET /creators?page=2" do
     it "returns paginated creators" do
       get "/creators?page=2"
