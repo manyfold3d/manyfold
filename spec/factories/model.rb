e1701e5374540ffd83476445684ab0738b8f4bd9
James Smith <james@floppy.org.uk>
2022-12-12 17:34:51 +0000

    lint: brackets in factorybot

--- spec/factories/model.rb
+++ spec/factories/model.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :model do
     name { Faker::Creature::Animal.name }
     path { Faker::File.dir }
-    library { build :library }
+    library { build(:library) }
   end
 end
