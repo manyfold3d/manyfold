2efde55393535c442619d8fb48c826ba0b41224a
James Smith <james@floppy.org.uk>
2022-12-12 16:40:30 +0000

    lint: prefer Rails.root.join

--- app/admin/log.rb
+++ app/admin/log.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page "Log" do
     h2 "log/#{Rails.env}.log"
     div style: "padding: 0px 1em; background: #eee; border: 1px solid black; width: 90vw; overflow-x: scroll" do
       pre do
-        File.readlines(Rails.root.join("log/#{Rails.env}.log")).split("\n") do |line|
+        Rails.root.join("log/#{Rails.env}.log").readlines.split("\n") do |line|
           text_node line
         end
       end
