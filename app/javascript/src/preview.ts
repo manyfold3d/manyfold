179fe7cef8e16be8606b9ba56934cc95f76a5034
James Smith <james@floppy.org.uk>
2022-12-14 19:27:31 +0000

    position lights at the corners of the grid

--- app/javascript/src/preview.ts
+++ app/javascript/src/preview.ts
@@ -71,10 +71,10 @@ class ObjectPreview {
     this.hemiLight = new THREE.HemisphereLight(0xffffff, 0x404040)
     this.scene.add(this.hemiLight)
     const light = new THREE.PointLight(0xffffff, 0.25)
-    light.position.set(250, 50, 100)
+    light.position.set(this.gridSizeX, 50, this.gridSizeZ)
     this.scene.add(light)
     const light2 = new THREE.PointLight(0xffffff, 0.25)
-    light2.position.set(-250, 50, 100)
+    light2.position.set(-this.gridSizeX, 50, this.gridSizeZ)
     this.scene.add(light2)
   }
 
