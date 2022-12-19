4526c403d0104b88235ff9eacfc1274b4b92fcce
James Smith <james@floppy.org.uk>
2022-12-19 23:10:55 +0000

    handle missing printed field in data migration
    This will happen if db:setup is used rather than a piece-by-piece migration

--- db/data/20221220223040_move_printed_to_favorites.rb
+++ db/data/20221220223040_move_printed_to_favorites.rb
@@ -2,14 +2,14 @@
 
 class MovePrintedToFavorites < ActiveRecord::Migration[7.0]
   def up
-    # If there's more than one user, we can't really do anything so abort
-    if User.count != 1
-      raise ActiveRecord::ConfigurationError
-    else
-      user = User.first
-      ModelFile.where(printed: true).each do |file|
-        user.favorite(file, scope: :printed)
-      end
+    # If there's no "printed" field, there's nothing to move
+    return if !ModelFile.has_attribute?(:printed)
+    # Move the data over to the first user found
+    # If there's more than one, this might be a bad choice,
+    # but it's better than nothing
+    user = User.first
+    ModelFile.where(printed: true).each do |file|
+      user.favorite(file, scope: :printed)
     end
   end
 
