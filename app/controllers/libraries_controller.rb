e0655ddd9f41d8de00759156ca24afe59acaeaba
Don Eisele <ksuquix@gmail.com>
2023-01-14 17:44:38 -0600

    Backend for searching on q=...

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -40,6 +40,13 @@ class LibrariesController < ApplicationController
       @models = @models.tagged_with(@collection, context: :collection) if @collection
     end
 
+    # keyword search filter
+    if params[:q]
+      field = Model.arel_table[:name]
+      @models = @models.where("tags.name LIKE ?","%#{params[:q]}%").or(@models.where(field.matches("%#{params[:q]}%"))).
+      joins("INNER JOIN taggings ON taggings.taggable_id=models.id AND taggings.taggable_type = 'Model' INNER JOIN tags ON tags.id = taggings.tag_id").distinct
+    end
+
     @commontags = ActsAsTaggableOn::Tag.joins(:taggings).where(taggings: {taggable: @models.except(:limit, :offset)})
 
   end
