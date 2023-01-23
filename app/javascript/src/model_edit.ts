ac00f65359fa8435ed7a2802f05eee1047090c1c
Don Eisele <ksuquix@gmail.com>
2023-01-23 00:18:05 -0600

    typescript complaints

--- app/javascript/src/model_edit.ts
+++ app/javascript/src/model_edit.ts
@@ -14,10 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
   $('[contenteditable=true]').focus(function () {
     $(this).data('initialText', $(this).text().trim())
   })
-  $('[contenteditable=true]').blur(function () {
+  $('[contenteditable=true]').blur(async function () {
     if ($(this).data('initialText') !== $(this).text().trim()) {
       console.log($(this).data('field'), ' changed: ', $(this).text().trim())
-      const postvar = {}
+      const postvar: any = {}
       postvar[$(this).data('field')] = $(this).text().trim()
       postvar._method = 'patch'
       postvar.authenticity_token = $('meta[name="csrf-token"]').attr('content')
