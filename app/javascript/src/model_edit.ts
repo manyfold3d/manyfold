d905036e24cc4341afce2d95c2e809e502520d6d
Don Eisele <ksuquix@gmail.com>
2023-01-22 21:41:10 -0600

    Editable names by clicking on name without going into edit

--- app/javascript/src/model_edit.ts
+++ app/javascript/src/model_edit.ts
@@ -9,5 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
       })
       window.tagInputs.push(tagInput)
     }
-  )
-})
+  );
+  // Editable names (and other fields):
+  $('[contenteditable=true]').focus(function() {
+    $(this).data("initialText", $(this).text().trim());
+  });
+  $('[contenteditable=true]').blur(function() {
+      if ($(this).data("initialText") !== $(this).text().trim()) {
+          console.log($(this).data("field")+" changed: "+$(this).text().trim());
+          var postvar = {};
+          postvar[$(this).data("field")] = $(this).text().trim();
+          postvar["_method"] = "patch";
+          postvar["authenticity_token"] = $('meta[name="csrf-token"]').attr('content');
+          $.post($(this).data("path"),postvar, function(result) {console.log(result.status)});
+      }
+  });
+});
