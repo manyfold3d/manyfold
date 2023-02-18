572e155628602ede640ab1fa6b80a940f5e52b09
James Smith <james@floppy.org.uk>
2023-02-18 17:42:43 +0000

    use extended non-standard MIME types for all models

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -6,13 +6,11 @@
 Mime::Type.register "model/stl", :stl
 Mime::Type.register "model/obj", :obj
 Mime::Type.register "model/3mf", :threemf, [], ["3mf"]
-Mime::Type.register "text/plain", :ply
-Mime::Type.register "application/octet-stream", :blend
-Mime::Type.register "application/octet-stream", :mix
-Mime::Type.register "application/octet-stream", :abc
-Mime::Type.register "model/step", :stp
-Mime::Type.register "application/octet-stream", :lys
-Mime::Type.register "application/octet-stream", :lyt
-Mime::Type.register "application/octet-stream", :chitubox
-
+Mime::Type.register "model/x-ply", :ply
+Mime::Type.register "model/x-blender", :blend
+Mime::Type.register "model/x-meshmixer", :mix
+Mime::Type.register "model/x-alembic", :abc
+Mime::Type.register "model/step", :step, [], ["stp"]
+Mime::Type.register "model/x-lychee", :lychee, [], ["lys", "lyt"]
+Mime::Type.register "model/x-chitubox", :chitubox, [], ["ctb"]
 Mime::Type.register "image/webp", :webp
