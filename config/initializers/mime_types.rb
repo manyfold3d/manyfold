f1be8731e0ce8e0f26c1b62d338fdb37e8928f94
James Smith <james@floppy.org.uk>
2023-02-18 16:39:06 +0000

    update MIMIE types for 3MF and STEP files

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,12 +5,12 @@
 
 Mime::Type.register "model/stl", :stl
 Mime::Type.register "model/obj", :obj
-Mime::Type.register "application/vnd.ms-package.3dmanufacturing-3dmodel+xml", :threemf, [], ["3mf"]
+Mime::Type.register "model/3mf", :threemf, [], ["3mf"]
 Mime::Type.register "text/plain", :ply
 Mime::Type.register "application/octet-stream", :blend
 Mime::Type.register "application/octet-stream", :mix
 Mime::Type.register "application/octet-stream", :abc
-Mime::Type.register "application/octet-stream", :stp
+Mime::Type.register "model/step", :stp
 Mime::Type.register "application/octet-stream", :lys
 Mime::Type.register "application/octet-stream", :lyt
 Mime::Type.register "application/octet-stream", :chitubox
