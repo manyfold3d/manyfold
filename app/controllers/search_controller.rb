c386cc500a6f05abf71c307b90dc6523c0521885
James Smith <james@floppy.org.uk>
2023-02-20 23:56:35 +0000

    change frontpage search to use new unified models list
    resolves #993

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,7 @@ class SearchController < ApplicationController
   before_action :check_for_first_use
 
   def index
-    @query = params[:q]
-    if @query
-      field = Model.arel_table[:name]
-      @results = Model.where(field.matches("%#{@query}%"))
-      @results += Model.tagged_with(@query)
-      @results.uniq!
-    else
-      @recent = Model.recent.limit(20)
-    end
+    @recent = Model.recent.limit(20)
   end
 
   private
