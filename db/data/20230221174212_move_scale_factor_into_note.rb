a4251237434562dc4e0a8e1810ecc6b03d886dd9
Don Eisele <ksuquix@gmail.com>
2023-02-23 00:45:50 -0600

    if scale_factor doesn't exist on objects, don't break when removing it

--- db/data/20230221174212_move_scale_factor_into_note.rb
+++ db/data/20230221174212_move_scale_factor_into_note.rb
@@ -3,7 +3,7 @@
 class MoveScaleFactorIntoNote < ActiveRecord::Migration[7.0]
   def up
     Model.all.each do |model|
-      if model.scale_factor != 100
+      if defined?(model.scale_factor) && (model.scale_factor != 100)
         model.update!(notes: [
           model.notes,
           "Scale factor: #{model.scale_factor}%"
