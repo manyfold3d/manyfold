95c4ae5831cf0077a379e6e040874849ed31e12b
Michael <8921172+mcsgroi@users.noreply.github.com>
2023-03-06 21:21:04 -0600

    Add upload and delete options to UI
    - Abstract unzip functionality out of file saving method

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -24,16 +24,20 @@ class UploadsController < ApplicationController
       file_name = File.basename(file_name_with_zip, File.extname(file_name_with_zip))
       dest_folder_name = library_path + file_name
       if !Dir.exist?(dest_folder_name) then
-        Dir.mkdir(dest_folder_name);
-
-        Zip::File.open(datafile) do |zipfile|
-          zipfile.each do |f|
-                f_path = File.join(dest_folder_name, f.name)
-                FileUtils.mkdir_p(File.dirname(f_path))
-                zipfile.extract(f, f_path) unless File.exist?(f_path)
-            end
-        end
+        self.unzip(dest_folder_name, datafile)
       end
     }
+  end
+  
+  def unzip(dest_folder_name, datafile)
+    Dir.mkdir(dest_folder_name)
+
+    Zip::File.open(datafile) do |zipfile|
+      zipfile.each do |f|
+            f_path = File.join(dest_folder_name, f.name)
+            FileUtils.mkdir_p(File.dirname(f_path))
+            zipfile.extract(f, f_path) unless File.exist?(f_path)
+        end
+    end
   end
 end
