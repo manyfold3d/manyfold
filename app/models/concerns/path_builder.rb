b5e5dd1de22b455eea91a7d9e1072a7fe07899b0
James Smith <james@floppy.org.uk>
2023-03-08 00:51:43 +0000

    combine all path elements into one template string and enable literal text inclusion

--- app/models/concerns/path_builder.rb
+++ app/models/concerns/path_builder.rb
@@ -2,19 +2,25 @@ module PathBuilder
   extend ActiveSupport::Concern
 
   def formatted_path
-    formatted_path_out = []
-    SiteSettings.model_path_prefix_template.split("/").each { |p|
-      case p
+    SiteSettings.model_path_prefix_template.gsub(/{.+?}/) do |token|
+      case token
       when "{tags}"
-        formatted_path_out.push(tags.order(taggings_count: :desc).map(&:to_s).map(&:parameterize))
+        (tags.count > 0) ?
+          File.join(tags.order(taggings_count: :desc).map(&:to_s).map(&:parameterize)) :
+          "@untagged"
       when "{creator}"
-        formatted_path_out.push(creator ? creator.name : "unset-creator")
+        creator&.name || "unset-creator"
       when "{collection}"
-        formatted_path_out.push((collections.count > 0) ? collections.map { |c| c.name } : "unset-collection")
+        (collections.count > 0) ?
+          collections.map(&:name).join(",") :
+          "unset-collection"
+      when "{modelName}"
+        name.parameterize
+      when "{modelId}"
+        id.to_s
       else
-        formatted_path_out.push("bad-formatted-path-element")
+        token
       end
-    }
-    File.join("", formatted_path_out, name.parameterize) + (SiteSettings.model_path_suffix_model_id ? "##{id}" : "")
+    end
   end
 end
