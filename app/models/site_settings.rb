2ab153ca8a29867c6d5d820410348c6fab2b1284
James Smith <james@floppy.org.uk>
2023-03-08 22:58:03 +0000

    update default path template

--- app/models/site_settings.rb
+++ app/models/site_settings.rb
@@ -11,7 +11,7 @@ class SiteSettings < RailsSettings::Base
   field :model_tags_stop_words_locale, type: :string, default: "en"
   field :model_tags_custom_stop_words, type: :array, default: (SupportedMimeTypes.image_extensions + SupportedMimeTypes.model_extensions)
   field :model_tags_auto_tag_new, type: :string, default: "!new"
-  field :model_path_prefix_template, type: :string, default: "tags"
+  field :model_path_prefix_template, type: :string, default: "{tags}/{modelName}{modelId}"
   field :model_tags_tag_model_path_prefix, type: :boolean, default: true
   field :model_path_suffix_model_id, type: :boolean, default: true
 end
