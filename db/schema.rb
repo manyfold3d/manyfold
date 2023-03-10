f3da82a09c44ef15fc67f2adea89c36a527a929a
James Smith <james@floppy.org.uk>
2023-03-10 23:05:57 +0000

    remove incorrect fields from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -128,8 +128,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_215826) do
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.integer "taggings_count", default: 0
-    t.text "notes"
-    t.text "caption"
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
