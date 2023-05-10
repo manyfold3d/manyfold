d23ab4778ea02b7439b185bb739cb3babd9d9582
James Smith <james@floppy.org.uk>
2023-05-10 17:19:41 +0100

    lint: add not null constraint to boolean columns

--- db/migrate/20210210153744_add_fields_to_parts.rb
+++ db/migrate/20210210153744_add_fields_to_parts.rb
@@ -1,6 +1,6 @@
 class AddFieldsToParts < ActiveRecord::Migration[6.1]
   def change
-    add_column :parts, :presupported, :boolean, default: false
-    add_column :parts, :printed, :boolean, default: false
+    add_column :parts, :presupported, :boolean, default: false, null: false
+    add_column :parts, :printed, :boolean, default: false, null: false
   end
 end
