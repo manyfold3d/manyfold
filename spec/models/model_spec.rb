0539801588b1cfec9de2b77e58aec8c69706359f
James Smith <james@floppy.org.uk>
2023-06-10 22:42:01 +0100

    write tests for moving models around on disk

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -24,21 +24,21 @@ RSpec.describe Model do
   context "with a library on disk" do
     before do
       allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?).with("/library1").and_return(true)
-      allow(File).to receive(:exist?).with("/library2").and_return(true)
+      allow(File).to receive(:exist?).with("/original_library").and_return(true)
+      allow(File).to receive(:exist?).with("/new_library").and_return(true)
     end
 
     it "must have a unique path within its library" do
-      library = create(:library, path: "/library1")
+      library = create(:library, path: "/original_library")
       create(:model, library: library, path: "model")
       expect(build(:model, library: library, path: "model")).not_to be_valid
     end
 
     it "can have the same path as a model in a different library" do
-      library1 = create(:library, path: "/library1")
-      create(:model, library: library1, path: "model")
-      library2 = create(:library, path: "/library2")
-      expect(build(:model, library: library2, path: "model")).to be_valid
+      original_library = create(:library, path: "/original_library")
+      create(:model, library: original_library, path: "model")
+      new_library = create(:library, path: "/new_library")
+      expect(build(:model, library: new_library, path: "model")).to be_valid
     end
   end
 
@@ -95,6 +95,82 @@ RSpec.describe Model do
     it "correctly flags up contained models" do
       expect(parent.contains_other_models?).to be true
       expect(child.contains_other_models?).to be false
+    end
+  end
+
+  context "being organised" do
+    around do |ex|
+      Dir.mktmpdir do |library_path|
+        @library_path = library_path
+        ex.run
+      end
+    end
+
+    let(:library) { create(:library, path: @library_path) }
+    let!(:model) {
+      FileUtils.mkdir_p(File.join(library.path, "original"))
+      create(:model, library: library, name: "test model", path: "original")
+    }
+
+    it "moves model folder" do
+      expect { model.update! organize: true }.not_to raise_error
+      expect(Dir.exist?(File.join(library.path, "original"))).to be false
+      expect(Dir.exist?(File.join(library.path, "@untagged", "test-model#1"))).to be true
+    end
+
+    it "has a validation error if the destination path already exists, and does not move anything" do
+      FileUtils.mkdir_p(File.join(library.path, "@untagged/test-model#1"))
+      expect { model.update! organize: true }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(model.errors.full_messages).to include("Path already exists")
+      expect(Dir.exist?(File.join(library.path, "original"))).to be true
+    end
+
+    it "has a validation error if the model has submodels, and does not move anything" do
+      create(:model, library: library, name: "sub model", path: "original/submodel")
+      expect { model.update! organize: true }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(model.errors.full_messages).to include("Path can't be changed, model contains other models")
+      expect(Dir.exist?(File.join(library.path, "original"))).to be true
+      expect(Dir.exist?(File.join(library.path, "@untagged", "test-model#1"))).to be false
+    end
+  end
+
+  context "changing library" do
+    around do |ex|
+      Dir.mktmpdir do |library_path|
+        Dir.mkdir File.join(library_path, "original_library")
+        Dir.mkdir File.join(library_path, "new_library")
+        @library_path = library_path
+        ex.run
+      end
+    end
+
+    let(:original_library) { create(:library, path: File.join(@library_path, "original_library")) }
+    let(:new_library) { create(:library, path: File.join(@library_path, "new_library")) }
+    let!(:model) {
+      FileUtils.mkdir_p(File.join(original_library.path, "model"))
+      create(:model, library: original_library, name: "test model", path: "model")
+    }
+    let(:submodel) { create(:model, library: original_library, name: "sub model", path: "model/submodel") }
+
+    it "moves model folder" do
+      expect { model.update! library: new_library }.not_to raise_error
+      expect(Dir.exist?(File.join(original_library.path, "model"))).to be false
+      expect(Dir.exist?(File.join(new_library.path, "model"))).to be true
+    end
+
+    it "has a validation error if the destination path already exists, and does not move folder" do
+      FileUtils.mkdir_p(File.join(new_library.path, "model"))
+      expect { model.update! library: new_library }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(model.errors.full_messages).to include("Path already exists")
+      expect(Dir.exist?(File.join(original_library.path, "model"))).to be true
+    end
+
+    it "has a validation error if the model has submodels, and does not move anything" do
+      create(:model, library: original_library, name: "sub model", path: "model/submodel")
+      expect { model.update! library: new_library }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(model.errors.full_messages).to include("Library can't be changed, model contains other models")
+      expect(Dir.exist?(File.join(original_library.path, "model"))).to be true
+      expect(Dir.exist?(File.join(new_library.path, "model"))).to be false
     end
   end
 end
