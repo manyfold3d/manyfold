d71eba3a6c4c9a0a7ed068fe16818be7c946b46b
James Smith <james@floppy.org.uk>
2023-06-18 12:13:10 +0100

    refactor path parser further to reduce complexity

--- app/models/concerns/path_parser.rb
+++ app/models/concerns/path_parser.rb
@@ -9,63 +9,81 @@ module PathParser
   def parse_metadata_from_path!
     return unless SiteSettings.model_path_template
     components = extract_path_components
-    if components[:tags].present?
-      tag_list.add(remove_stop_words(components[:tags]))
-    end
-    self.creator = find_or_create_from_path_component(Creator, components[:creator]) if components[:creator]
-    self.collection = find_or_create_from_path_component(Collection, components[:collection]) if components[:collection]
-    if components[:model_name]
-      self.name = to_human_name(components[:model_name])
-    end
+    parse_tags(components[:tags])
+    parse_creator(components[:creator])
+    parse_collection(components[:collection])
+    parse_name(components[:model_name])
     save!
   end
-end
 
-def path_parse_pattern
-  Regexp.new("^/?.*?" +
-    SiteSettings.model_path_template.gsub(/{.+?}/) { |token|
-      case token
-      when "{tags}"
-        "(?<tags>[[:print:]]*)"
-      when "{creator}"
-        "(?<creator>[[:print:]&&[^/]]*?)"
-      when "{collection}"
-        "(?<collection>[[:print:]&&[^/]]*?)"
-      when "{modelName}"
-        "(?<model_name>[[:print:]&&[^/]]*?)"
-      when "{modelId}"
-        "(?<model_id>#[[:digit:]]+)?"
-      else
-        "[[:print:]&&[^/]]*"
-      end
-    } + "$")
-end
+  private
 
-def extract_path_components
-  components = path.match(path_parse_pattern)&.named_captures&.symbolize_keys
-  return {} if components.nil?
-  components.merge({
-    tags: components[:tags]&.split("/")&.compact_blank,
-    model_id: nil # discard ID, never gonna use it in parsing
-  }).compact
-end
+  def parse_creator(component)
+    return unless component
+    self.creator = find_or_create_from_path_component(Creator, component)
+  end
 
-def remove_stop_words(words)
-  return words if !SiteSettings.model_tags_filter_stop_words
-  stopword_filter = Stopwords::Snowball::Filter.new(
-    SiteSettings.model_tags_stop_words_locale,
-    SiteSettings.model_tags_custom_stop_words
-  )
-  stopword_filter.filter(words)
-end
+  def parse_collection(component)
+    return unless component
+    self.collection = find_or_create_from_path_component(Collection, component)
+  end
 
-def find_or_create_from_path_component(klass, path_component)
-  klass.find_by(slug: path_component) ||
-    klass.create_with(slug: path_component.parameterize).find_or_create_by(
-      name: to_human_name(path_component)
+  def parse_name(component)
+    return unless component
+    self.name = to_human_name(component)
+  end
+
+  def parse_tags(component)
+    return unless component
+    tag_list.add(remove_stop_words(component))
+  end
+
+  def path_parse_pattern
+    Regexp.new("^/?.*?" +
+      SiteSettings.model_path_template.gsub(/{.+?}/) { |token|
+        case token
+        when "{tags}"
+          "(?<tags>[[:print:]]*)"
+        when "{creator}"
+          "(?<creator>[[:print:]&&[^/]]*?)"
+        when "{collection}"
+          "(?<collection>[[:print:]&&[^/]]*?)"
+        when "{modelName}"
+          "(?<model_name>[[:print:]&&[^/]]*?)"
+        when "{modelId}"
+          "(?<model_id>#[[:digit:]]+)?"
+        else
+          "[[:print:]&&[^/]]*"
+        end
+      } + "$")
+  end
+
+  def extract_path_components
+    components = path.match(path_parse_pattern)&.named_captures&.symbolize_keys
+    return {} if components.nil?
+    components.merge({
+      tags: components[:tags]&.split("/")&.compact_blank,
+      model_id: nil # discard ID, never gonna use it in parsing
+    }).compact
+  end
+
+  def remove_stop_words(words)
+    return words if !SiteSettings.model_tags_filter_stop_words
+    stopword_filter = Stopwords::Snowball::Filter.new(
+      SiteSettings.model_tags_stop_words_locale,
+      SiteSettings.model_tags_custom_stop_words
     )
-end
+    stopword_filter.filter(words)
+  end
 
-def to_human_name(str)
-  str.humanize.tr("+", " ").titleize
+  def find_or_create_from_path_component(klass, path_component)
+    klass.find_by(slug: path_component) ||
+      klass.create_with(slug: path_component.parameterize).find_or_create_by(
+        name: to_human_name(path_component)
+      )
+  end
+
+  def to_human_name(str)
+    str.humanize.tr("+", " ").titleize
+  end
 end
