cdfaa01b2247a23f8772cc043ba95ba39c73a204
James Smith <james@floppy.org.uk>
2023-06-19 19:56:37 +0100

    show detected problems in lists/titles
    resolves #1281

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -7,4 +7,11 @@ module ModelsHelper
     end
     groups.merge(nil => ungrouped)
   end
+
+  def status_badges(model)
+    badges = []
+    badges << content_tag(:span, "new", class: "badge rounded-pill bg-info") if model.tag_list.include? SiteSettings.model_tags_auto_tag_new
+    badges << content_tag(:span, icon("exclamation-triangle-fill", "Problem"), class: "text-warning align-middle") unless model.problems.empty?
+    content_tag :span, safe_join(badges), class: "status-badges"
+  end
 end
