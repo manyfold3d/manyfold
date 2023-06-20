186096b58004b5eca7e92e763248dcf630706c53
James Smith <james@floppy.org.uk>
2023-06-20 21:54:44 +0100

    add helper for creating or clearing Problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -8,4 +8,12 @@ class Problem < ApplicationRecord
     :empty,
     :destination_exists # No longer used, but kept for compatibility
   ]
+
+  def self.create_or_clear(problematic, cat, present)
+    if present
+      problematic.problems.create(category: cat)
+    else
+      problematic.problems.where(category: cat).destroy_all
+    end
+  end
 end
