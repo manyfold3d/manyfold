eea53aa8343dc772d4e94ff6572783d7e339b92e
James Smith <james@floppy.org.uk>
2023-06-21 09:51:58 +0100

    queue integroty check after merge

--- app/models/model.rb
+++ app/models/model.rb
@@ -48,6 +48,9 @@ class Model < ApplicationRecord
         model: target
       )
     end
+    # Queue up integrity check on target
+    # to update any known problems
+    Scan::CheckModelIntegrityJob.perform_later(target)
     reload
     destroy
   end
