123f5c4c4d98b484fba74a7b5475ad16f88de330
James Smith <james@floppy.org.uk>
2023-06-23 17:43:06 +0100

    Refactor library scan specs to test things more individually

--- spec/jobs/library_scan_job_spec.rb
+++ spec/jobs/library_scan_job_spec.rb
@@ -6,18 +6,12 @@ RSpec.describe LibraryScanJob do
     ActiveJob::Base.queue_adapter = :test
   end
 
-  context "with a variety of files" do
+  context "with files in various folders" do
     around do |ex|
       MockDirectory.create([
         "model_one/part_1.obj",
         "model_one/part_2.obj",
-        "model_one/nested_model/part_one.stl",
-        "subfolder/model_two/part_one.stl",
-        "thingiverse_model/files/part_one.stl",
-        "thingiverse_model/images/card_preview_DISPLAY.png",
-        "thingiverse_model/images/ignore.stl",
-        "thingiverse_model/LICENSE.txt",
-        "thingiverse_model/README.txt"
+        "subfolder/model_two/part_one.stl"
       ]) do |path|
         @library_path = path
         ex.run
@@ -29,19 +23,40 @@ RSpec.describe LibraryScanJob do
     # rubocop:enable RSpec/InstanceVariable
 
     it "can scan a library directory" do
-      expect { described_class.perform_now(library) }.to change { library.models.count }.to(4)
-      expect(library.models.map(&:name)).to contain_exactly("Model One", "Model Two", "Nested Model", "Thingiverse Model")
-      expect(library.models.map(&:path)).to contain_exactly("model_one", "subfolder/model_two", "model_one/nested_model", "thingiverse_model")
+      expect { described_class.perform_now(library) }.to change { library.models.count }.to(2)
+      expect(library.models.map(&:path)).to contain_exactly("model_one", "subfolder/model_two")
     end
 
     it "queues up model scans" do
-      expect { described_class.perform_now(library) }.to have_enqueued_job(ModelScanJob).exactly(4).times
+      expect { described_class.perform_now(library) }.to have_enqueued_job(ModelScanJob).exactly(2).times
     end
 
     it "only scans models with changes on rescan" do
       model_one = create(:model, path: "model_one", library: library)
       ModelScanJob.perform_now(model_one)
-      expect { described_class.perform_now(library) }.to have_enqueued_job(ModelScanJob).exactly(3).times
+      expect { described_class.perform_now(library) }.to have_enqueued_job(ModelScanJob).exactly(1).times
+    end
+  end
+
+  context "with a thingiverse-style model folder" do
+    around do |ex|
+      MockDirectory.create([
+        "thingiverse_model/files/part_one.stl",
+        "thingiverse_model/images/preview.png",
+        "thingiverse_model/README.txt"
+      ]) do |path|
+        @library_path = path
+        ex.run
+      end
+    end
+
+    # rubocop:disable RSpec/InstanceVariable
+    let(:library) { create(:library, path: @library_path) }
+    # rubocop:enable RSpec/InstanceVariable
+
+    it "understands that it's a single model" do
+      expect { described_class.perform_now(library) }.to change { library.models.count }.to(1)
+      expect(library.models.map(&:path)).to contain_exactly("thingiverse_model")
     end
   end
 
