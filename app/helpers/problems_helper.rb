e0a55cd44aca337176c4b370bf6cc813ff360e57
James Smith <james@floppy.org.uk>
2023-07-12 17:35:11 +0100

    change row and icon colour depending on severity of problem

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,2 +1,18 @@
 module ProblemsHelper
+  def problem_severity(problem)
+    case problem.category.to_sym
+    when :missing
+      "danger"
+    when :nesting
+      "warning"
+    else
+      "info"
+    end
+  end
+
+  def max_problem_severity
+    return "danger" if Problem.where(category: :missing).count > 0
+    return "warning" if Problem.where(category: :nesting).count > 0
+    "info"
+  end
 end
