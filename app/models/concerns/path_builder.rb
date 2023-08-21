b6a68e257310825d66e2b038c780ff5fa77167ed
James Smith <james@floppy.org.uk>
2023-08-21 14:50:40 +0100

    fix lint errors from updated cops

--- app/models/concerns/path_builder.rb
+++ app/models/concerns/path_builder.rb
@@ -6,7 +6,7 @@ module PathBuilder
       case token
       when "{tags}"
         (tags.count > 0) ?
-          File.join(tags.order(taggings_count: :desc).map(&:to_s).map(&:parameterize)) :
+          File.join(tags.order(taggings_count: :desc).map { |x| x.to_s.parameterize }) :
           "@untagged"
       when "{creator}"
         path_component(creator) || "@unattributed"
