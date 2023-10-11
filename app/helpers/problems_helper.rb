c4707038b1e8945f6730182a4bcd4f098c2c4496
James Smith <james@floppy.org.uk>
2023-10-11 22:30:45 +0100

    refactor problem_severity method

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,13 +1,12 @@
 module ProblemsHelper
   def problem_severity(problem)
-    case problem.category.to_sym
-    when :missing
-      "danger"
-    when :nesting, :duplicate
-      "warning"
-    else
-      "info"
-    end
+    {
+      missing: "danger",
+      empty: "info",
+      nesting: "warning",
+      inefficient: "info",
+      duplicate: "warning"
+    }[problem.category.to_sym] || "info"
   end
 
   def max_problem_severity
