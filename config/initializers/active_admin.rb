f0ef7af51e11bb188032002dcb3865457a7dadec
James Smith <james@floppy.org.uk>
2024-01-26 13:05:35 +0000

    disable advanced admin in demo mode

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -58,24 +58,13 @@ ActiveAdmin.setup do |config|
 
   # == User Authorization
   #
-  # Active Admin will automatically call an authorization
-  # method in a before filter of all controller actions to
-  # ensure that there is a user with proper rights. You can use
-  # CanCanAdapter or make your own. Please refer to documentation.
-  # config.authorization_adapter = ActiveAdmin::CanCanAdapter
-
-  # In case you prefer Pundit over other solutions you can here pass
-  # the name of default policy class. This policy will be used in every
-  # case when Pundit is unable to find suitable policy.
-  # config.pundit_default_policy = "MyDefaultPunditPolicy"
+  config.authorization_adapter = ActiveAdmin::PunditAdapter
+  config.pundit_default_policy = "ActiveAdminPolicy"
 
   # If you wish to maintain a separate set of Pundit policies for admin
   # resources, you may set a namespace here that Pundit will search
   # within when looking for a resource's policy.
   # config.pundit_policy_namespace = :admin
-
-  # You can customize your CanCan Ability class name here.
-  # config.cancan_ability_class = "Ability"
 
   # You can specify a method to be called on unauthorized access.
   # This is necessary in order to prevent a redirect loop which happens
