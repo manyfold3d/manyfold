7ca8f49c2ae6ea14c68f226c80ce95d9119fa871
James Smith <james@floppy.org.uk>
2024-01-30 09:41:04 +0000

    fix missing scope in activeadmin pundit policy
    resolves #1777

--- app/policies/active_admin_policy.rb
+++ app/policies/active_admin_policy.rb
@@ -24,4 +24,21 @@ class ActiveAdminPolicy < ApplicationPolicy
   def destroy?
     true
   end
+
+  def destroy_all?
+    true
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
 end
