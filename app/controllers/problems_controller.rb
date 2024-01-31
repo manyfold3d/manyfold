7e252bfeb61c2b56f941ca543bad4c1df05a9e29
James Smith <james@floppy.org.uk>
2024-01-31 14:45:29 +0000

    add links to ignore problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -3,4 +3,20 @@ class ProblemsController < ApplicationController
     page = params[:page] || 1
     @problems = Problem.visible(current_user.problem_settings).page(page).per(50).order([:category, :problematic_type])
   end
+
+  def update
+    @problem = Problem.find(params[:id])
+    @problem.update!(permitted_params)
+    flash[:notice] = t(".ignored", name: @problem.problematic.name,
+      message: t("problems.%{type}_%{category}.title" % {type: @problem.problematic_type.underscore, category: @problem.category}))
+    redirect_to problems_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:problem).permit([
+      :ignored
+    ])
+  end
 end
