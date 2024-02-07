f7345e3a4a9b844a85ac4704a66990f57902f69c
James Smith <james@floppy.org.uk>
2024-02-07 20:15:00 +0000

    disable gettext output

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,6 +1,7 @@
 if Rails.env.development?
   TranslationIO.configure do |config|
     config.api_key = ENV["TRANSLATION_IO_API_KEY"]
+    config.disable_gettext = true
     config.source_locale = "en"
     config.target_locales = ["de"]
   end
