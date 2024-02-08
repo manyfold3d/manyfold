d1a0573cf1d5e852056a18a049648f33a5fd6f30
James Smith <james@floppy.org.uk>
2024-02-08 10:41:26 +0000

    ignore built-in errors keys

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -2,6 +2,9 @@ if Rails.env.development?
   TranslationIO.configure do |config|
     config.api_key = ENV["TRANSLATION_IO_API_KEY"]
     config.disable_gettext = true
+    config.ignored_key_prefixes = [
+      "errors" # built-in errors that get picked up
+    ]
     config.source_locale = "en"
     config.target_locales = ["de"]
   end
