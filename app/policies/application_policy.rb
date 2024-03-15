d2f97dcca54d7e6ce3c7de0514c39ba7e6f79435
James Smith <james@floppy.org.uk>
2024-03-15 15:42:54 +0000

    make default policy be allowed for logged-in admins, but not for anyone else
    this lets us cover a good range of current defaults, but upgrade access in a fail-safe way.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -9,15 +9,15 @@ class ApplicationPolicy
   end
 
   def index?
-    false
+    user&.admin?
   end
 
   def show?
-    false
+    user&.admin?
   end
 
   def create?
-    false
+    user&.admin?
   end
 
   def new?
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    user&.admin?
   end
 
   def edit?
@@ -33,7 +33,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    user&.admin?
   end
 
   class Scope
@@ -43,7 +43,7 @@ class ApplicationPolicy
     end
 
     def resolve
-      raise NotImplementedError, "You must define #resolve in #{self.class}"
+      scope.all
     end
 
     private
