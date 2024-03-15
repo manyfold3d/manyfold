6509c5ccf3a279c43827566f3d0bb1680ce3d35f
James Smith <james@floppy.org.uk>
2024-03-15 15:36:09 +0000

    add missing policies for admin mode

---
+++ app/policies/acts_as_taggable_on/tag_policy.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class ActsAsTaggableOn::TagPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
