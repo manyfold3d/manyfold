dc6ef6b4eecc9254ffaf3323b10a3a6195a0ed9c
James Smith <james@floppy.org.uk>
2024-03-19 09:42:39 +0000

    enable cancel method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,9 +40,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def cancel
+    authorize :"users/registrations"
+    super
+  end
 
   protected
 
