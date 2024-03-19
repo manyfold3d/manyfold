5b3226d032fea21c9c52b583719d36b25b65b44f
James Smith <james@floppy.org.uk>
2024-03-19 10:47:24 +0000

    remove redundant policy methods from RegistrationsPolicy

--- app/policies/users/registrations_policy.rb
+++ app/policies/users/registrations_policy.rb
@@ -1,19 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsPolicy < ApplicationPolicy
-  def create?
-    Flipper.enabled?(:multiuser) &&
-      SiteSettings.registration_enabled
-  end
-
-  def update?
-    user == record
-  end
-
-  def destroy?
-    Flipper.enabled?(:multiuser) && (user == record)
-  end
-
   def cancel?
     Flipper.enabled?(:multiuser)
   end
