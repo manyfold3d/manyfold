bf1d32f74b9c2ead2879768f823f12c5391eb468
James Smith <james@floppy.org.uk>
2024-03-20 22:42:29 +0000

    don't bother disabling user admin in single user mode
    doesn't really matter

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,40 +1,34 @@
-begin
-  if Flipper.enabled? :multiuser
-    ActiveAdmin.register User do
-      permit_params :email, :password, :password_confirmation, :username
+ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation, :username
 
-      controller do
-        defaults finder: :find_by_username
-      end
-
-      index do
-        selectable_column
-        id_column
-        column :username
-        column :email
-        column :current_sign_in_at
-        column :sign_in_count
-        column :created_at
-        actions
-      end
-
-      filter :username
-      filter :email
-      filter :current_sign_in_at
-      filter :sign_in_count
-      filter :created_at
-
-      form do |f|
-        f.inputs do
-          f.input :username
-          f.input :email
-          f.input :password
-          f.input :password_confirmation
-        end
-        f.actions
-      end
-    end
+  controller do
+    defaults finder: :find_by_username
   end
-rescue ActiveRecord::StatementInvalid
-  # If we've not migrated Flipper yet, we'll get an exception, which we can swallow
+
+  index do
+    selectable_column
+    id_column
+    column :username
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :username
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :username
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
