8043cbd7309b6330e9b827111ee98ce03c7d6544
James Smith <james@floppy.org.uk>
2024-03-20 16:39:49 +0000

    test that users can have roles

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Role do
-  it "needs testing"
+  let(:user) { create(:user) }
+
+  it "can be given to a user" do
+    user.add_role :administrator
+    expect(user.is_administrator?).to be true
+  end
 end
