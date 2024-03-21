964e13b27416f8fa11774642b63e90aa4a1f0629
James Smith <james@floppy.org.uk>
2024-03-21 22:48:51 +0000

    set up granular access policy for Models

--- spec/requests/models_spec.rb
+++ spec/requests/models_spec.rb
@@ -30,15 +30,19 @@ RSpec.describe "Models" do
       end
     end
 
-    describe "GET /libraries/:library_id/models/:id/edit", :as_editor do
-      it "shows edit page for file" do
+    describe "GET /libraries/:library_id/models/:id/edit" do
+      it "shows edit page for file", :as_editor do
         get "/libraries/#{library.id}/models/#{library.models.first.id}/edit"
         expect(response).to have_http_status(:success)
       end
+
+      it "is denied to non-editors", :as_contributor do
+        expect { get "/libraries/#{library.id}/models/#{library.models.first.id}/edit" }.to raise_error(Pundit::NotAuthorizedError)
+      end
     end
 
-    describe "PUT /libraries/:library_id/models/:id", :as_editor do
-      it "adds tags to a model" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+    describe "PUT /libraries/:library_id/models/:id" do
+      it "adds tags to a model", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
         put "/libraries/#{library.id}/models/#{library.models.first.id}", params: {model: {tag_list: ["a", "b", "c"]}}
         expect(response).to have_http_status(:redirect)
         tags = library.models.first.tag_list
@@ -48,7 +52,7 @@ RSpec.describe "Models" do
         expect(tags[2]).to eq "c"
       end
 
-      it "removes tags from a model" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+      it "removes tags from a model", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
         first = library.models.first
         first.tag_list = "a, b, c"
         first.save
@@ -61,7 +65,7 @@ RSpec.describe "Models" do
         expect(tags[1]).to eq "b"
       end
 
-      it "both adds and removes tags from a model" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+      it "both adds and removes tags from a model", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
         first = library.models.first
         first.tag_list = "a, b, c"
         first.save
@@ -74,24 +78,36 @@ RSpec.describe "Models" do
         expect(tags[1]).to eq "b"
         expect(tags[2]).to eq "d"
       end
+
+      it "is denied to non-editors", :as_contributor do
+        expect { put "/libraries/#{library.id}/models/#{library.models.first.id}" }.to raise_error(Pundit::NotAuthorizedError)
+      end
     end
 
-    describe "DELETE /libraries/:library_id/models/:id", :as_editor do # rubocop:todo RSpec/RepeatedExampleGroupBody
-      it "redirects to model list after deletion" do
+    describe "DELETE /libraries/:library_id/models/:id" do # rubocop:todo RSpec/RepeatedExampleGroupBody
+      it "redirects to model list after deletion", :as_editor do
         delete "/libraries/#{library.id}/models/#{library.models.first.id}"
         expect(response).to redirect_to("/libraries/#{library.id}")
       end
-    end
 
-    describe "GET /models/edit", :as_editor do # rubocop:todo RSpec/RepeatedExampleGroupBody
-      it "shows bulk edit page" do
-        get "/models/edit"
-        expect(response).to have_http_status(:success)
+      it "is denied to non-editors", :as_contributor do
+        expect { delete "/libraries/#{library.id}/models/#{library.models.first.id}" }.to raise_error(Pundit::NotAuthorizedError)
       end
     end
 
-    describe "PATCH /models/edit", :as_editor do
-      it "updates models creator" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+    describe "GET /models/edit" do # rubocop:todo RSpec/RepeatedExampleGroupBody
+      it "shows bulk edit page", :as_editor do
+        get "/models/edit"
+        expect(response).to have_http_status(:success)
+      end
+
+      it "is denied to non-editors", :as_contributor do
+        expect { get "/models/edit" }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    describe "PATCH /models/edit" do
+      it "updates models creator", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
         models = library.models.take(2)
         update = {}
         update[models[0].id] = 1
@@ -105,8 +121,8 @@ RSpec.describe "Models" do
         expect(models[1].creator_id).to eq creator.id
       end
 
-      update = {}
-      it "adds tags to models" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+      it "adds tags to models", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+        update = {}
         library.models.take(2).each do |model|
           update[model.id] = 1
         end
@@ -119,7 +135,7 @@ RSpec.describe "Models" do
         end
       end
 
-      it "removes tags from models" do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
+      it "removes tags from models", :as_editor do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
         update = {}
         library.models.take(2).each do |model|
           model.tag_list = "a, b, c"
@@ -134,6 +150,11 @@ RSpec.describe "Models" do
           expect(model.tag_list).to eq ["c"]
         end
       end
+
+      it "is denied to non-editors", :as_contributor do
+        update = {}
+        expect { patch "/models/update", params: {models: update, remove_tags: ["a", "b"]} }.to raise_error(Pundit::NotAuthorizedError)
+      end
     end
 
     describe "GET /models", :as_viewer do
@@ -144,10 +165,14 @@ RSpec.describe "Models" do
       end
     end
 
-    describe "POST /libraries/:library_id/models/:id/merge", :as_editor do
-      it "gives a bad request response if no merge parameter is provided" do
+    describe "POST /libraries/:library_id/models/:id/merge" do
+      it "gives a bad request response if no merge parameter is provided", :as_editor do
         post "/libraries/#{library.id}/models/#{library.models.first.id}/merge"
         expect(response).to have_http_status(:bad_request)
+      end
+
+      it "is denied to non-editors", :as_contributor do
+        expect { post "/libraries/#{library.id}/models/#{library.models.first.id}/merge" }.to raise_error(Pundit::NotAuthorizedError)
       end
     end
   end
