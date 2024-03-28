e5c3e865fb0d6c695909c8af54da96fba769a503
James Smith <james@floppy.org.uk>
2024-03-28 11:49:41 +0000

    working manifold analysis for Mittsu::Geometry

--- app/lib/mesh_analysis.rb
+++ app/lib/mesh_analysis.rb
@@ -1,13 +1,33 @@
 module Mittsu
   class Geometry
     def manifold?
-      false
+      edges = {}
+      # For each face, record its edges in the edge hash
+      @faces.each do |face|
+        update_edge_hash face.a, face.b, edges
+        update_edge_hash face.b, face.c, edges
+        update_edge_hash face.c, face.a, edges
+      end
+      # If there's anything left in the edge hash, then either
+      # we have holes, or we have badly oriented faces
+      edges.empty?
+    end
+
+    private
+
+    # Updates edge hash with the passed vertex indexes
+    # First, the reverse edge is searched for in the hash
+    # If found, it's removed as we've got a match
+    # If not, we record this edge in the hash
+    def update_edge_hash(v1, v2, edges)
+      return if edges.delete "#{v2}->#{v1}"
+      edges["#{v1}->#{v2}"] = true
     end
   end
 
   class BufferGeometry
     def manifold?
-      false
+      true
     end
   end
 end
--- spec/lib/mesh_analysis_spec.rb
+++ spec/lib/mesh_analysis_spec.rb
@@ -3,11 +3,20 @@ require "rails_helper"
 RSpec.describe "MeshAnalysis" do
   it "verifies that meshes are manifold (i.e. do not have holes)" do
     geometry = Mittsu::SphereGeometry.new(2.0, 32, 16)
+    geometry.merge_vertices
     expect(geometry.manifold?).to be true
   end
 
   it "detects meshes that are non-manifold (i.e. do have holes)" do
-    geometry = Mittsu::PlaneBufferGeometry.new(1.0, 1.0)
+    geometry = Mittsu::PlaneGeometry.new(1.0, 1.0)
+    geometry.merge_vertices
     expect(geometry.manifold?).to be false
   end
+
+  it "detects buffergeometry meshes that are non-manifold" do
+    geometry = Mittsu::PlaneBufferGeometry.new(1.0, 1.0)
+    geometry.merge_vertices
+    expect(geometry.manifold?).to be false
+  end
+
 end
