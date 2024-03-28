03070ca5376aaa7ebac785f6b5d0b823a89c864f
James Smith <james@floppy.org.uk>
2024-03-28 08:38:39 +0000

    add manifold? method to mittsu geometry classes

---
+++ app/lib/mesh_analysis.rb
@@ -0,0 +1,13 @@
+module Mittsu
+  class Geometry
+    def manifold?
+      false
+    end
+  end
+
+  class BufferGeometry
+    def manifold?
+      false
+    end
+  end
+end
--- spec/lib/mesh_analysis_spec.rb
+++ spec/lib/mesh_analysis_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "MeshAnalysis" do
-
   it "verifies that meshes are manifold (i.e. do not have holes)" do
     geometry = Mittsu::SphereGeometry.new(2.0, 32, 16)
     expect(geometry.manifold?).to be true
@@ -11,5 +10,4 @@ RSpec.describe "MeshAnalysis" do
     geometry = Mittsu::PlaneBufferGeometry.new(1.0, 1.0)
     expect(geometry.manifold?).to be false
   end
-
 end
