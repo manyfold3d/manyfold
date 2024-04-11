572ee2e7a6fef0d8d68efbfc67b05a6731f19e9b
James Smith <james@floppy.org.uk>
2024-04-11 15:20:41 +0100

    set default mail-from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: ENV.fetch("SMTP_FROM_ADDRESS", "notifications@#{ENV.fetch("PUBLIC_HOSTNAME", "localhost")}")
   layout "mailer"
 end
