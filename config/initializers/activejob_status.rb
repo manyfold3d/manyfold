0833591d14f489074d0e3f96e53f5b242dfc9d3b
James Smith <james@floppy.org.uk>
2024-04-18 13:36:05 +0100

    configure activejob-status for 7 day retention of status and exceptions

--- config/initializers/activejob_status.rb
+++ config/initializers/activejob_status.rb
@@ -1,3 +1,7 @@
 # Sidekiq and Rails communicate via redis, so we should always use that.
 ActiveJob::Status.store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") }
 
+ActiveJob::Status.options = {
+  includes: %i[status exception],
+  expires_in: 7.days.to_i
+}
