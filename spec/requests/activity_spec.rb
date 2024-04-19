d0a1f705253e4c3a79b6657d7d09321bc0079d4a
James Smith <james@floppy.org.uk>
2024-04-19 12:06:04 +0100

    add status update messages to DetectFilesystemChangesJob

---
+++ spec/requests/activity_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Activities", type: :request do
+  context "as admin", :as_administrator do
+    describe "GET /" do
+      it "returns http success" do
+        get "/activity"
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  context "as non-admin", :as_editor do
+    describe "GET /" do
+      it "raises a routing error" do
+        expect { get "/activity" }.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
+
+end
