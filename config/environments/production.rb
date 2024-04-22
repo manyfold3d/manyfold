54ee8d6b3ff4fe9950435a003a7979d1755e94a6
James Smith <james@floppy.org.uk>
2024-04-22 12:54:36 +0100

    turn off static file serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,9 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # Disable serving static files from the `/public` folder since
+  # Thruster handles this.
+  config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
