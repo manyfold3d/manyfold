38634a84af9c1a34a151a4310820a258349a7bcc
James Smith <james@floppy.org.uk>
2024-04-29 11:48:47 +0100

    use different icons for different problem severities, and set severity in icon title text

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -4,13 +4,27 @@ module ProblemsHelper
   end
 
   def max_problem_severity(problems = Problem.all)
+    return nil if problems.empty?
     severities = problems.select(:category).distinct.map { |p| problem_severity(p) }
     severities.max_by { |p| Problem::SEVERITIES.find_index(p) }
   end
 
-  def problem_icon(problems = Problem.all)
-    return "" if problems.empty?
+  def problem_icon(severity)
+    i = case severity
+    when :info
+      "info-circle-fill"
+    when :warning
+      "exclamation-triangle-fill"
+    when :danger
+      "x-octagon-fill"
+    else
+      nil
+    end
+  end
+
+  def problem_icon_tag(problems = Problem.all)
     severity = max_problem_severity(problems)
-    content_tag(:span, icon("exclamation-triangle-fill", Problem.model_name.human(count: 100)), class: "text-#{severity} align-middle")
+    ico = problem_icon(severity)
+    content_tag(:span, icon(ico, t("problems.severities.#{severity}")), class: "text-#{severity} align-middle") if ico
   end
 end
