d9c67cfe192ce68d56884ae14f51b710c787e507
James Smith <james@floppy.org.uk>
2024-05-02 22:08:47 +0100

    set aria-disabled on active indicator

--- app/javascript/src/carousel.ts
+++ app/javascript/src/carousel.ts
@@ -1,44 +1,48 @@
 import * as bootstrap from 'bootstrap'
 
 document.addEventListener('DOMContentLoaded', () => {
-	var paused = false;
-	var manual = false;
-	const myCarouselElement = document.querySelector('#imageCarousel')
-	if (myCarouselElement) {
-		const carousel = new bootstrap.Carousel(myCarouselElement, {
-			interval: 1000,
-			pause: false
-		});
+  let paused = false
+  let manual = false
+  const carouselElement = document.querySelector('#imageCarousel')
+  if (carouselElement != null) {
+    new bootstrap.Carousel(carouselElement, { /* eslint no-new: 0 */
+      interval: 5000,
+      pause: false
+    })
 
-		function togglePause () {
-			manual = true
-			onPause(!paused, true)
-		}
+    function togglePause (): void {
+      manual = true
+      onPause(!paused, true)
+    }
 
-		function onPause (pause, updateState) {
-			if (pause) {
-				$('#imageCarousel').carousel("pause");
-				$('#imageCarouselInner').attr("aria-live", "polite")
-				if (updateState) {
-					paused = true;
-					$('#rotationControlIcon').addClass("bi-play")
-					$('#rotationControlIcon').removeClass("bi-pause")
-				}
-			}
-			else {
-				$('#imageCarousel').carousel("cycle");
-				$('#imageCarouselInner').attr("aria-live", "off")
-				if (updateState) {
-					paused = false;
-					$('#rotationControlIcon').addClass("bi-pause")
-					$('#rotationControlIcon').removeClass("bi-play")
-				}
-			}
-		}
+    function onPause (pause: boolean, updateState: boolean): void {
+      if (pause) {
+        $('#imageCarousel').carousel('pause')
+        $('#imageCarouselInner').attr('aria-live', 'polite')
+        if (updateState) {
+          paused = true
+          $('#rotationControlIcon').addClass('bi-play')
+          $('#rotationControlIcon').removeClass('bi-pause')
+        }
+      } else {
+        $('#imageCarousel').carousel('cycle')
+        $('#imageCarouselInner').attr('aria-live', 'off')
+        if (updateState) {
+          paused = false
+          $('#rotationControlIcon').addClass('bi-pause')
+          $('#rotationControlIcon').removeClass('bi-play')
+        }
+      }
+    }
 
-		$('#rotationControl').click(togglePause);
-		$('#imageCarousel').mouseenter(function () { if (!manual) { onPause(true, false) } });
-		$('#imageCarousel').mouseleave(function () { if (!manual) { onPause(false, false) } });
-		//$('#imageCarousel').focus(function () { manual = true; onPause(true, true) }});
-	}
-});
+    carouselElement.addEventListener('slid.bs.carousel', function (event) {
+      $('.carousel-indicators > button').removeAttr('aria-disabled')
+      $(`.carousel-indicators > button:nth-child(${event.to + 1})`).attr('aria-disabled', 'true')
+    })
+
+    $('#rotationControl').click(togglePause)
+    $('#imageCarousel').mouseenter(function () { if (!manual) { onPause(true, false) } })
+    $('#imageCarousel').mouseleave(function () { if (!manual) { onPause(false, false) } })
+    // $('#imageCarousel').focus(function () { manual = true; onPause(true, true) }});
+  }
+})
