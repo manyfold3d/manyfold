7f1401d9c721604020c7e0f5f2ea879162d54496
James Smith <james@floppy.org.uk>
2024-05-08 16:30:40 +0100

    make event transfer handler type safe

--- app/javascript/src/comlink_event_handler.ts
+++ app/javascript/src/comlink_event_handler.ts
@@ -1,21 +1,33 @@
 import * as Comlink from 'comlink';
 
+type UnifiedEvent = {
+	type: string;
+	altKey? : boolean;
+	ctrlKey? : boolean;
+	metaKey? : boolean;
+	shiftKey? : boolean;
+	button? : number;
+	buttons? : number;
+	clientX? : number;
+	clientY? : number;
+	pageX? : number;
+	pageY? : number;
+	pointerType? : string;
+	deltaX? : number;
+	deltaY? : number;
+	keyCode? : number;
+}
+
 Comlink.transferHandlers.set("EVENT", {
-	canHandle: (obj) => obj instanceof Event,
-	serialize: (ev) => {
-		return [
-			{
-				type: ev.type,
-				target: {
-					id: ev.target.id,
-					classList: [...ev.target.classList],
-				},
-				// Shared
+	canHandle: (obj) : boolean => obj instanceof Event,
+	serialize: (ev : Event) => {
+		var serializedEvent : UnifiedEvent = { type: ev.type }
+		if (ev instanceof PointerEvent)
+			serializedEvent = {
 				altKey: ev.altKey,
 				ctrlKey: ev.ctrlKey,
 				metaKey: ev.metaKey,
 				shiftKey: ev.shiftKey,
-				// MouseEvent properties
 				button: ev.button,
 				buttons: ev.buttons,
 				clientX: ev.clientX,
@@ -23,14 +35,24 @@ Comlink.transferHandlers.set("EVENT", {
 				pageX: ev.pageX,
 				pageY: ev.pageY,
 				pointerType: ev.pointerType,
-				// WheelEvent properties
+				...serializedEvent
+			}
+		if (ev instanceof WheelEvent)
+			serializedEvent = {
 				deltaX: ev.deltaX,
 				deltaY: ev.deltaY,
-				// KeyboardEvent properties
+				...serializedEvent
+			}
+		if (ev instanceof KeyboardEvent)
+			serializedEvent = {
+				altKey: ev.altKey,
+				ctrlKey: ev.ctrlKey,
+				metaKey: ev.metaKey,
+				shiftKey: ev.shiftKey,
 				keyCode: ev.keyCode,
-			},
-			[],
-		];
+				...serializedEvent
+			}
+		return [serializedEvent, []];
 	},
 	deserialize: (obj) => obj,
 });
