4cee43e9b67cf58245770fd0b43aa6f9279822b5
James Smith <james@floppy.org.uk>
2024-05-23 12:54:06 +0100

    add manyfold-aware uploader that organizes files on disk as is currently done, using model path

---
+++ app/uploaders/library_uploader.rb
@@ -0,0 +1,19 @@
+class LibraryUploader < Shrine
+  plugin :dynamic_storage
+
+  storage /store_(\d)/ do |m|
+    Library.find(m[1]).storage
+  end
+
+  class Attacher
+    def store_key
+      @record.model.library.storage_key
+    end
+  end
+
+  def generate_location(io, record: nil, derivative: nil, metadata: {}, **)
+    return super unless record
+    File.join(record.model.path, record.filename)
+  end
+
+end
