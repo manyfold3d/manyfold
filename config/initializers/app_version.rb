1b988a15adba4c460af8a763af506f807d18749a
James Smith <james@floppy.org.uk>
2024-05-28 11:47:39 +0100

    Get full git sha in dev mode

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -3,7 +3,7 @@
 if Rails.env.development?
   if File.directory? File.expand_path(".git")
     system("git fetch #{Rails.application.config.upstream_repo}")
-    git_sha = `git rev-parse --short=8 HEAD`
+    git_sha = `git rev-parse HEAD`
     git_sha.strip!
     app_version = `git describe --tags --abbrev=0 #{git_sha}`
     app_version.strip!
