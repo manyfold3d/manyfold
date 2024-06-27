691a1db87096fe5f6d2c53f17cd0e577274d45dd
James Smith <james@floppy.org.uk>
2024-06-27 08:56:39 +0100

    increase sidekiq retries

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,6 @@
 class ApplicationJob < ActiveJob::Base
   include ActiveJob::Status
-  sidekiq_options retry: 5
+  sidekiq_options retry: 10
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
