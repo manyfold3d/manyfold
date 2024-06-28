6d85881b4704a8952012545592abe04ea6f36c75
James Smith <james@floppy.org.uk>
2024-06-28 14:59:54 +0100

    move archive extraction into a background job

---
+++ app/jobs/process_uploaded_file_job.rb
@@ -0,0 +1,54 @@
+class ProcessUploadedFileJob < ApplicationJob
+  queue_as :default
+
+  def perform(library_id, uploaded_file)
+    library = Library.find(library_id)
+    return if library.nil?
+    attacher = Shrine::Attacher.new
+    attacher.attach_cached(uploaded_file)
+    datafile = attacher.file
+    # Check file extension as proxy for MIME type - to be improved in other work soon
+    return unless %w[zip rar 7z bz2 gz].include? File.extname(datafile.original_filename).delete(".").downcase
+    # Then open it up
+    file_name_with_zip = datafile.original_filename
+    file_name = File.basename(file_name_with_zip, File.extname(file_name_with_zip))
+    dest_folder_name = File.join(library.path, SecureRandom.uuid)
+    if !Dir.exist?(dest_folder_name)
+      unzip(dest_folder_name, datafile)
+    end
+    # Rename destination folder atomically
+    File.rename(dest_folder_name, File.join(library.path, file_name))
+    # Queue up model creation for new folder
+    Scan::CreateModelJob.perform_later(library.id, file_name, include_all_subfolders: true)
+    # Discard cached file
+    attacher.destroy
+  end
+
+  def unzip(dest_folder_name, datafile)
+    datafile.open do |file|
+      Archive::Reader.open_fd(file.fileno) do |reader|
+        Dir.mkdir(dest_folder_name)
+        Dir.chdir(dest_folder_name) do
+          reader.each_entry do |entry|
+            next if entry.size > SiteSettings.max_file_extract_size
+            reader.extract(entry, Archive::EXTRACT_SECURE)
+          end
+        end
+      end
+    end
+
+    # Checks the directory just created and if it contains only one directory,
+    # moves the contents of that directory up a level, then deletes the empty directory.
+    pn = Pathname.new(dest_folder_name)
+    if pn.children.length == 1 && pn.children[0].directory?
+      dup_dir = Pathname.new(pn.children[0])
+
+      dup_dir.children.each do |child|
+        fixed_path = Pathname.new(pn.to_s + "/" + child.basename.to_s)
+        File.rename(child.to_s, fixed_path.to_s)
+      end
+
+      Dir.delete(dup_dir.to_s)
+    end
+  end
+end
