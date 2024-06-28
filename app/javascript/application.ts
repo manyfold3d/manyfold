ff4c911a8a3563be3d4b901ff6564d720dece6ed
James Smith <james@floppy.org.uk>
2024-06-28 10:15:35 +0100

    refactor uppy config into its own file and make more robust

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -7,17 +7,13 @@ import 'masonry-layout'
 
 import Cocooned from '@notus.sh/cocooned'
 
-import Uppy from '@uppy/core'
-import Dashboard from '@uppy/dashboard'
-import Form from '@uppy/form'
-import XHR from '@uppy/xhr-upload'
-
 import 'src/preview'
 import 'src/bulk_edit'
 import 'src/model_edit'
 import 'src/tag'
 import 'src/carousel'
 import 'src/file_size_validation'
+import 'src/uploads'
 
 // Load i18n definitions
 import { I18n } from 'i18n-js'
@@ -28,24 +24,5 @@ window.$ = $
 
 document.addEventListener('DOMContentLoaded', () => {
 	Rails.start()
-	const uppySettings = document.getElementById('uppy')?.dataset
-	new Uppy({
-		autoProceed: true,
-		restrictions: {
-			allowedFileTypes: uppySettings.allowedFileTypes?.split(","),
-			maxFileSize: +uppySettings.maxFileSize
-		}
-	})
-		.use(Dashboard, {
-			inline: true,
-			target: '#uppy',
-			theme: 'auto',
-			width: '100%',
-			height: '25rem',
-			showRemoveButtonAfterComplete: true,
-			hideProgressAfterFinish: true
-		})
-		.use(Form, { target: '#upload-form'} )
-    .use(XHR, { endpoint: '/upload' })
   Cocooned.start()
 })
---
+++ app/javascript/src/uploads.ts
@@ -0,0 +1,28 @@
+import Uppy from '@uppy/core'
+import Dashboard from '@uppy/dashboard'
+import Form from '@uppy/form'
+import XHR from '@uppy/xhr-upload'
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('#uppy').forEach((element: HTMLDivElement) => {
+		const settings = element.dataset
+		new Uppy({
+			autoProceed: true,
+			restrictions: {
+				allowedFileTypes: settings.allowedFileTypes?.split(","),
+				maxFileSize: +(settings?.maxFileSize || -1)
+			}
+		})
+			.use(Dashboard, {
+				inline: true,
+				target: `#${element.id}`,
+				theme: 'auto',
+				width: '100%',
+				height: '25rem',
+				showRemoveButtonAfterComplete: true,
+				hideProgressAfterFinish: true
+			})
+			.use(Form, { target: element.closest('form') || undefined })
+			.use(XHR, { endpoint: '/upload' })
+	})
+})
