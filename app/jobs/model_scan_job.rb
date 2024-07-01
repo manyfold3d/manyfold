88cd25ba3849b5245ff9d5c03dce6d88cce1ac80
James Smith <james@floppy.org.uk>
2024-07-01 23:04:37 +0100

    use library glob for model file list building

--- app/jobs/model_scan_job.rb
+++ app/jobs/model_scan_job.rb
@@ -3,31 +3,27 @@ require "shellwords"
 class ModelScanJob < ApplicationJob
   queue_as :scan
 
-  def file_list(model_path, include_all_subfolders: false)
-    list = []
-    Dir.open(model_path) do |dir|
-      glob = include_all_subfolders ?
-        [File.join(Shellwords.escape(dir.path), "**", ApplicationJob.file_pattern)] :
-        [File.join(Shellwords.escape(dir.path), ApplicationJob.file_pattern)] +
-          ApplicationJob.common_subfolders.map do |name, pattern|
-            File.join(
-              Shellwords.escape(dir.path),
-              ApplicationJob.case_insensitive_glob_string(name),
-              pattern
-            )
-          end
-      list = Dir.glob(glob).uniq.filter { |x| File.file?(x) }
-    end
-    list
+  def file_list(model_path, library, include_all_subfolders: false)
+    glob = include_all_subfolders ?
+      [File.join(Shellwords.escape(model_path), "**", ApplicationJob.file_pattern)] :
+      [File.join(Shellwords.escape(model_path), ApplicationJob.file_pattern)] +
+        ApplicationJob.common_subfolders.map do |name, pattern|
+          File.join(
+            Shellwords.escape(model_path),
+            ApplicationJob.case_insensitive_glob_string(name),
+            pattern
+          )
+        end
+    library.glob(glob).uniq
   end
 
   def perform(model_id, include_all_subfolders: false)
     model = Model.find(model_id)
     return if Problem.create_or_clear(model, :missing, !model.exist?)
     # For each file in the model, create a file object
-    file_list(model.absolute_path, include_all_subfolders: include_all_subfolders).each do |filename|
+    file_list(model.path, model.library, include_all_subfolders: include_all_subfolders).each do |filename|
       # Create the file
-      file = model.model_files.find_or_create_by(filename: filename.gsub(model.absolute_path + "/", ""))
+      file = model.model_files.find_or_create_by(filename: filename.gsub(model.path + "/", ""))
       ModelFileScanJob.perform_later(file.id) if file.valid?
     end
     # Set tags and default files
