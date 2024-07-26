88c12b21f13d49edddc0cdba9aca5fc7c5ccbcfb
James Smith <james@floppy.org.uk>
2024-07-26 11:57:28 +0100

    move default collection pagination setting into code

--- db/migrate/20230313000001_change_pagination_settings_on_user.rb
+++ db/migrate/20230313000001_change_pagination_settings_on_user.rb
@@ -1,7 +1,4 @@
 class ChangePaginationSettingsOnUser < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :users, :pagination_settings,
-      from: {models: true, creators: true, per_page: 12},
-      to: {models: true, creators: true, collections: true, per_page: 12}
   end
 end
