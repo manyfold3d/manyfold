a270950b6ffc8eb75dd72549c6c1ee537be65ca4
James Smith <james@floppy.org.uk>
2024-07-29 10:34:56 +0100

    tidy up config file

--- config/initializers/federails.rb
+++ config/initializers/federails.rb
@@ -2,22 +2,18 @@
 
 Federails.configure do |conf|
   conf.app_name = "Manyfold"
-
   conf.app_version = Rails.application.config.app_version
 
   conf.site_host = Rails.application.default_url_options["host"]
-
   conf.site_port = Rails.application.default_url_options["port"]
   conf.force_ssl = Rails.application.config.force_ssl
 
   conf.enable_discovery = Flipper.enabled? :federation
 
   conf.app_layout = "layouts/application"
-
   conf.user_class = "::User"
 
   conf.server_routes_path = "federation"
-
   conf.client_routes_path = "app"
 
   conf.user_profile_url_method = "~"
@@ -25,12 +21,4 @@ Federails.configure do |conf|
   conf.user_name_field = "~"
 
   conf.user_username_field = "id"
-
-  ## Test
-  # conf.site_port = "null"
-
-  ## Production
-  # conf.force_ssl = true
-  #
-  # conf.site_host = 443
 end
