dbedcbedbfa583e303a13cb5b62c0aa12ca042f4
James Smith <james@floppy.org.uk>
2024-08-05 16:45:59 +0100

    Don't index hidden files when extracting archives

--- spec/jobs/process_uploaded_file_job_spec.rb
+++ spec/jobs/process_uploaded_file_job_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.describe ProcessUploadedFileJob do
+  context "when detecting hidden files" do
+    it "detects normal files" do
+      expect(described_class.new.send(:hidden?, "test.stl")).to be false
+    end
+
+    it "detects normal files in subfolders" do
+      expect(described_class.new.send(:hidden?, "test/test.stl")).to be false
+    end
+
+    it "detects hidden files" do
+      expect(described_class.new.send(:hidden?, ".test.stl")).to be true
+    end
+
+    it "detects hidden files in subfolders" do
+      expect(described_class.new.send(:hidden?, "test/.test.stl")).to be true
+    end
+
+    it "detects normal files in hidden subfolders" do
+      expect(described_class.new.send(:hidden?, ".test/test.stl")).to be true
+    end
+  end
+
   context "when counting common path prefixes" do
     it "returns zero if there are no directories at all" do
       expect(described_class.new.send(:count_common_elements, [])).to eq 0
