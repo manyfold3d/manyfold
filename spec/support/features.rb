10d6494de9e5a506b4093047121ddd3c66544235
James Smith <james@floppy.org.uk>
2024-08-09 11:26:42 +0100

    mode demo mode check in specs into metadata

---
+++ spec/support/features.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.before(:each, :multiuser) do
+    allow(SiteSettings).to receive(:multiuser_enabled?).and_return(true)
+  end
+
+  config.before(:each, :demo_mode) do
+    allow(SiteSettings).to receive(:demo_mode_enabled?).and_return(true)
+  end
+end
