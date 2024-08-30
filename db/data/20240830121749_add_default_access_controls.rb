d6e360c6edd4375df3b0483ba2b48be224b3c0ac
James Smith <james@floppy.org.uk>
2024-08-30 14:47:06 +0100

    add default permissions for all existing objects

---
+++ db/data/20240830121749_add_default_access_controls.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddDefaultAccessControls < ActiveRecord::Migration[7.1]
+  def up
+    [Creator, Collection, Model].each do |klass|
+      klass.find_each(&:assign_default_view_permissions)
+    end
+  end
+
+  def down
+  end
+end
