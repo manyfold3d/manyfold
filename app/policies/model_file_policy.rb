c42741f5e70158cddb1f3227d6c3ed2b5cdd783e
James Smith <james@floppy.org.uk>
2024-09-03 17:56:22 +0100

    delegate model file policy to model policy (mostly edit permission)

--- app/policies/model_file_policy.rb
+++ app/policies/model_file_policy.rb
@@ -1,9 +1,26 @@
 class ModelFilePolicy < ApplicationPolicy
+
+  def show?
+    ModelPolicy.new(@user, @record.model).show?
+  end
+
+  def create?
+    ModelPolicy.new(@user, @record.model).edit?
+  end
+
+  def update?
+    create?
+  end
+
+  def delete?
+    create?
+  end
+
   def bulk_edit?
-    user&.is_moderator?
+    bulk_update?
   end
 
   def bulk_update?
-    user&.is_moderator?
+    create?
   end
 end
