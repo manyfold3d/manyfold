d6a64a89e27a6740c2f5d157fd6125d73eee1112
James Smith <james@floppy.org.uk>
2024-09-05 19:35:42 +0100

    don't overwrite public IDs

--- db/data/20240904152358_generate_public_i_ds_for_models.rb
+++ db/data/20240904152358_generate_public_i_ds_for_models.rb
@@ -3,7 +3,7 @@
 class GeneratePublicIDsForModels < ActiveRecord::Migration[7.1]
   def up
     [Model, ModelFile, Problem, Creator, Collection, Library].each do |model|
-      model.find_each do |obj|
+      model.where(public_id: nil).find_each do |obj|
         obj.send :generate_public_id
         obj.update_column :public_id, obj.public_id # rubocop:disable Rails/SkipsModelValidations
       end
