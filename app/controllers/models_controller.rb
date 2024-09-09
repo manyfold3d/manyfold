54573a5644c9887e770ea0bab18c35e3e72d3355
James Smith <james@floppy.org.uk>
2024-09-09 15:34:37 +0100

    roll back problematic pluck changes

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -203,6 +203,6 @@ class ModelsController < ApplicationController
   end
 
   def get_available_tags
-    @tags = ActsAsTaggableOn::Tag.includes(:taggings).where("taggings.taggable_type": "Model", "taggings.taggable_id": policy_scope(Model).pluck(:id)).order(:name)
+    @tags = ActsAsTaggableOn::Tag.includes(:taggings).where("taggings.taggable": policy_scope(Model)).order(:name)
   end
 end
