e99c6b3419a22cb3f004f08bcd1d5fd31722a068
James Smith <james@floppy.org.uk>
2024-09-10 10:36:30 +0100

    lint: don't subtract time

--- app/jobs/cache_sweep_job.rb
+++ app/jobs/cache_sweep_job.rb
@@ -1,6 +1,6 @@
 class CacheSweepJob < ApplicationJob
   def perform
     cache = Shrine.storages[:cache]
-    cache.clear! { |path| path.mtime < (Time.now - 6.hours) }
+    cache.clear! { |path| path.mtime < 6.hours.ago }
   end
 end
