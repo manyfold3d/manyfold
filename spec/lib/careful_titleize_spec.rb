7f509485de201303255d81953e396fdaff271374
James Smith <james@floppy.org.uk>
2024-09-20 17:50:39 +0100

    better tests for careful titleize

--- spec/lib/careful_titleize_spec.rb
+++ spec/lib/careful_titleize_spec.rb
@@ -1,12 +1,24 @@
 require "rails_helper"
 
 RSpec.describe CarefulTitleize do
+  [
+    "Death Star II",
+    "README.txt",
+    "camelCase",
+    "OpenGL",
+    "3MF",
+    "Object3D"
+  ].each do |input|
+    it "leaves '#{input}' unchanged" do
+      expect(input.careful_titleize).to eq input
+    end
+  end
+
   {
-    "camelCase" => "camelCase",
-    "README.txt" => "README.Txt",
+    "all along the watchtower" => "All Along The Watchtower",
+    "left_hand_3" => "Left Hand 3",
     "death_star_II" => "Death Star II",
-    "will.i.am" => "Will.i.am",
-    "3jane tessier-ashpool" => "3Jane Tessier-Ashpool"
+    "will.i.am" => "Will.i.am"
   }.each_pair do |input, output|
     it "titleizes '#{input}' to '#{output}'" do
       expect(input.careful_titleize).to eq output
