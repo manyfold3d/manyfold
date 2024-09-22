f7e21c64e0163eb3c923e3d3be24de22a1d5c72b
James Smith <james@floppy.org.uk>
2024-09-22 12:10:05 +0100

    add default from_param

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,10 @@ class ApplicationRecord < ActiveRecord::Base
   def self.ransackable_symbols
     (ransackable_attributes + ransackable_associations + ransackable_scopes).map(&:to_sym)
   end
+
+  # Default from_param implementation
+  # just the same as standard find()
+  def self.from_param(param)
+    find(param)
+  end
 end
