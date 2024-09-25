52d22dd8d00577c4dbb2c88498b5c1b4e63f7322
James Smith <james@floppy.org.uk>
2024-09-25 09:38:16 +0100

    fix safe path validator

--- app/validators/safe_path_validator.rb
+++ app/validators/safe_path_validator.rb
@@ -1,21 +1,24 @@
 class SafePathValidator < ActiveModel::EachValidator
   UNSAFE = [
-    "/bin",
-    "/boot",
-    "/dev",
-    "/etc",
-    "/lib",
-    "/lost",
-    "/proc",
-    "/root",
-    "/run",
-    "/sbin",
-    "/selinux",
-    "/srv",
-    "/usr"
+    nil,
+    "bin",
+    "boot",
+    "dev",
+    "etc",
+    "lib",
+    "lost",
+    "proc",
+    "root",
+    "run",
+    "sbin",
+    "selinux",
+    "srv",
+    "usr"
   ]
 
   def validate_each(record, attribute, value)
-    record.errors.add attribute, :unsafe if value === "/" || UNSAFE.any? { |x| value&.starts_with?(x) }
+    return if value.nil?
+    start = Pathname.new(value).each_filename.to_a.first
+    record.errors.add attribute, :unsafe if UNSAFE.any?(start)
   end
 end
