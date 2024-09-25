28d54d9860cdde6a4db67eb32d3126703c10b282
James Smith <james@floppy.org.uk>
2024-09-25 12:56:24 +0100

    test that creators can be looked up by acct URI
    resolves #2777

--- spec/requests/webfinger_spec.rb
+++ spec/requests/webfinger_spec.rb
@@ -1,54 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "Webfinger", :multiuser do
-  context "when looking up a user by federation URL" do
-    let(:user) { create(:user) }
+  context "when looking up a creator by at_address" do
+    let(:creator) { create(:creator) }
 
     before do
-      get("/.well-known/webfinger?resource=#{user.actor.federated_url}")
+      get("/.well-known/webfinger?resource=acct:#{creator.actor.at_address}")
     end
 
     it "returns a successful response" do
+      puts creator.actor.at_address
       expect(response).to have_http_status :success
     end
 
     it "responds with data on the correct user" do
-      expect(response.parsed_body["links"][0]["href"]).to eq user.actor.federated_url
+      expect(response.parsed_body["links"][0]["href"]).to eq creator.actor.federated_url
     end
-  end
-
-  context "when looking up a user by at_address" do
-    let(:user) { create(:user) }
-
-    before do
-      get("/.well-known/webfinger?resource=acct:#{user.actor.at_address}")
-    end
-
-    it "returns a successful response" do
-      expect(response).to have_http_status :success
-    end
-
-    it "responds with data on the correct user" do
-      expect(response.parsed_body["links"][0]["href"]).to eq user.actor.federated_url
-    end
-  end
-
-  context "when looking up a non-existent user" do
-    let(:user) { create(:user) }
-
-    before do
-      get("/.well-known/webfinger?resource=#{user.actor.federated_url}9999")
-    end
-
-    it "returns a not found response"
   end
 
   [
+    :user,
     :collection,
     :creator,
     :model
   ].each do |followable|
-    context "when looking up a #{followable}" do
+    context "when looking up a #{followable} by URL" do
       let(:object) { create(followable) }
 
       before do
@@ -61,6 +37,10 @@ RSpec.describe "Webfinger", :multiuser do
 
       it "responds with data on the correct #{followable}" do
         expect(response.parsed_body["links"][0]["href"]).to eq object.actor.federated_url
+      end
+
+      it "returns a not found response if item doesn't exist" do
+        expect { get("/.well-known/webfinger?resource=#{object.actor.federated_url}9999") }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
