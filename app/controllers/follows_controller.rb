a20a23ef92397f69006fbbdde10442ee9a54e433
James Smith <james@floppy.org.uk>
2024-10-06 21:57:15 +0100

    add remote-follow endpoint and federails config

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,16 @@
 class FollowsController < ApplicationController
   before_action :get_target
 
+  # Remote follow
+  def new
+    actor = Federails::Actor.find_by_federation_url(params[:uri]) # rubocop:disable Rails/DynamicFindBy
+    if actor&.entity
+      redirect_to actor.entity
+    else
+      raise ActiveRecord::RecordNotFound
+    end
+  end
+
   def create
     authorize Federails::Following
     current_user.follow @target
