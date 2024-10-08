0ca1c22beb74b431f4533f9727f436e0e31eb4f5
James Smith <james@floppy.org.uk>
2024-10-08 12:06:04 +0100

    post lifecycle activities

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,21 @@ RSpec.describe Comment do
   let!(:model) { create(:model) }
   let!(:comment) { create(:comment, commenter: model, commentable: model) }
 
+  it "posts a Federails Activity on creation" do # rubocop:disable RSpec/MultipleExpectations
+    expect { create(:comment, commenter: model, commentable: model) }.to change(Federails::Activity, :count).by(1)
+    expect(Federails::Activity.last.action).to eq "Create"
+  end
+
+  it "posts a Federails Activity on update" do # rubocop:disable RSpec/MultipleExpectations
+    expect { comment.update(comment: "test") }.to change(Federails::Activity, :count).by(1)
+    expect(Federails::Activity.last.action).to eq "Update"
+  end
+
+  it "posts a Federails Activity on deletion" do # rubocop:disable RSpec/MultipleExpectations
+    expect { comment.destroy }.to change(Federails::Activity, :count).by(1)
+    expect(Federails::Activity.last.action).to eq "Delete"
+  end
+
   it "has a federated_url method" do
     expect(comment.federated_url).to eq "http://localhost:3214/models/#{model.public_id}/comments/#{comment.public_id}"
   end
