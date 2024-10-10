6e9d5fae1ca7829154cfc86623770339567236e7
James Smith <james@floppy.org.uk>
2024-10-10 12:07:07 +0100

    add tags in a proper ActivityStreams way

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,16 +14,31 @@ class Comment < ApplicationRecord
   end
 
   def to_activitypub_object
+    # Build tag structure
+    tags = commentable.respond_to?(:tags) ?
+      commentable.tags.pluck(:name).map do |tag|
+        {
+          type: "Hashtag",
+          name: "##{tag}",
+          href: Rails.application.routes.url_helpers.url_for([commentable.class, tag: tag])
+        }
+      end
+    : nil
+    tag_html = tags&.pluck(:name)&.join(" ")
     # Comments become Notes in ActvityPub world
     {
       id: federated_url,
       type: "Note",
-      content: Kramdown::Document.new(comment).to_html,
+      content: [
+        Kramdown::Document.new(comment).to_html,
+        tag_html
+      ].compact.join("\n\n"),
       context: Rails.application.routes.url_helpers.url_for([commentable, only_path: false]),
       published: created_at&.iso8601,
       attributedTo: (commenter&.actor&.respond_to?(:federated_url) ? commenter.actor.federated_url : nil),
       to: ["https://www.w3.org/ns/activitystreams#Public"],
-      cc: [commenter.actor.followers_url]
+      cc: [commenter.actor.followers_url],
+      tags: tags
     }.compact
   end
 
