72522a3d4222e9a981a2c5c828f750f210d3c1b4
James Smith <james@floppy.org.uk>
2024-10-12 00:05:19 +0100

    add UI for remote follow request

--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,8 @@ Rails.application.routes.draw do
   end
 
   get "/authorize_interaction" => "follows#new", :as => :new_follow
-  post "/remote_interaction" => "follows#remote", :as => :remote_follow
-  post "/remote_interaction" => "follows#redirect_remote", :as => :perform_remote_follow
+  post "/remote_follow" => "follows#remote_follow", :as => :remote_follow
+  post "/perform_remote_follow" => "follows#perform_remote_follow", :as => :perform_remote_follow
 
   concern :followable do |options|
     if SiteSettings.multiuser_enabled?
