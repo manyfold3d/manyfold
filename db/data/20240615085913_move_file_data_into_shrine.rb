62bdfbf9f0a34c3787011f7d3791ff8a39d2386e
James Smith <james@floppy.org.uk>
2024-10-18 13:59:48 +0100

    allow validation skipping on file attachment

--- db/data/20240615085913_move_file_data_into_shrine.rb
+++ db/data/20240615085913_move_file_data_into_shrine.rb
@@ -2,7 +2,7 @@
 
 class MoveFileDataIntoShrine < ActiveRecord::Migration[7.0]
   def up
-    ModelFile.find_each { |x| x.attach_existing_file!(refresh: false) }
+    ModelFile.find_each { |x| x.attach_existing_file!(refresh: false, skip_validations: true) }
   end
 
   def down
