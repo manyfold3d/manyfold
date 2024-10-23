568d905ba26386185f174c2c62c18830dfb6821a
James Smith <james@floppy.org.uk>
2024-10-23 15:44:53 +0100

    can't schedule jobs if there's no redis
    but it will happen later

--- config/initializers/usage_reports.rb
+++ config/initializers/usage_reports.rb
@@ -3,4 +3,5 @@ require "usage_report"
 Rails.application.config.after_initialize do
   # Make sure the schedule is loaded correctly
   UsageReport.set_schedule!
+rescue RedisClient::CannotConnectError
 end
