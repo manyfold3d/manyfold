2c82d0a98ee5a3e8cd2e74f2e789cfd6ab0e2b53
James Smith <james@floppy.org.uk>
2024-11-04 14:57:51 +0000

    replace partials with resolve button component

--- app/components/resolve_button_component.rb
+++ app/components/resolve_button_component.rb
@@ -1,17 +1,47 @@
 # frozen_string_literal: true
 
 class ResolveButtonComponent < ViewComponent::Base
+  OPTIONS = {
+    show: {
+      icon: "box",
+      i18n_key: "models.file.open_button.text",
+      button_type: "primary"
+    },
+    edit: {
+      icon: "pencil",
+      i18n_key: "general.edit",
+      button_type: "primary"
+    },
+    destroy: {
+      icon: "trash",
+      i18n_key: "general.delete",
+      button_type: "danger",
+      confirm: "%{type}s.destroy.confirm"
+    },
+    merge: {
+      icon: "box-arrow-in-up-left",
+      i18n_key: "models.problem.merge_all",
+      button_type: "danger"
+    },
+    upload: {
+      icon: "upload",
+      i18n_key: "application.navbar.upload",
+      button_type: "primary"
+    },
+    convert: {
+      icon: "arrow-left-right",
+      i18n_key: "model_files.show.convert",
+      button_type: "warning"
+    }
+  }
+
   def initialize(problem:, user: nil)
     @problem = problem
     @user = user
   end
 
   def before_render
-    # Preload some things we'll need
-    @category = @problem.category
-    @problematic = @problem.problematic
-    @parent_model = @problem.problematic.model if @problematic.is_a?(ModelFile)
-    @strategy = @problem.resolution_strategy
+    @options = OPTIONS[@problem.resolution_strategy.to_sym]
   end
 
   def render?
@@ -19,5 +49,17 @@ class ResolveButtonComponent < ViewComponent::Base
   end
 
   def call
+    text = t @options[:i18n_key]
+    link_to(
+      safe_join([helpers.icon(@options[:icon], text), text], " "),
+      resolve_problem_path(@problem),
+      class: "btn btn-#{@options[:button_type]}",
+      data: {
+        confirm: @options[:confirm] ?
+          translate(@options[:confirm] % {type: @problem.problematic_type.underscore}) :
+          nil
+      },
+      method: :post
+    )
   end
 end
