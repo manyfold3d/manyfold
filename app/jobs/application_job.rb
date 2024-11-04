cbaecfe78933ae76fe2dd1b71a0bebfcf71debc9
James Smith <james@floppy.org.uk>
2024-11-04 16:25:30 +0000

    Discard jobs queued up for removed objects

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,6 +4,7 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
+  discard_on ActiveRecord::RecordNotFound
 
   before_perform do |job|
     begin
