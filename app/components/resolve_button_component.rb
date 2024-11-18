8cb39a9c329bce91828d16527a6662b4b5d6fedb
James Smith <james@floppy.org.uk>
2024-11-18 16:20:52 +0000

    mark up used translations

--- app/components/resolve_button_component.rb
+++ app/components/resolve_button_component.rb
@@ -4,33 +4,33 @@ class ResolveButtonComponent < ViewComponent::Base
   OPTIONS = {
     show: {
       icon: "box",
-      i18n_key: "models.file.open_button.text",
+      i18n_key: "models.file.open_button.text", # i18n-tasks-use t('models.file.open_button.text')
       button_type: "primary"
     },
     edit: {
       icon: "pencil",
-      i18n_key: "general.edit",
+      i18n_key: "general.edit", # i18n-tasks-use t('general.edit')
       button_type: "primary"
     },
     destroy: {
       icon: "trash",
-      i18n_key: "general.delete",
+      i18n_key: "general.delete", # i18n-tasks-use t('general.delete')
       button_type: "danger",
       confirm: "%{type}s.destroy.confirm"
     },
     merge: {
       icon: "box-arrow-in-up-left",
-      i18n_key: "models.problem.merge_all",
+      i18n_key: "models.problem.merge_all", # i18n-tasks-use t('models.problem.merge_all')
       button_type: "danger"
     },
     upload: {
       icon: "upload",
-      i18n_key: "application.navbar.upload",
+      i18n_key: "application.navbar.upload", # i18n-tasks-use t('application.navbar.upload')
       button_type: "primary"
     },
     convert: {
       icon: "arrow-left-right",
-      i18n_key: "model_files.show.convert",
+      i18n_key: "model_files.show.convert", # i18n-tasks-use t('model_files.show.convert')
       button_type: "warning"
     }
   }
