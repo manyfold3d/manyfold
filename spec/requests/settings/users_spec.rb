f7f722952011b888e3f64941e50ab8efe0b4f403
James Smith <james@floppy.org.uk>
2024-11-20 16:32:48 +0000

    improve test user creation

--- spec/requests/settings/users_spec.rb
+++ spec/requests/settings/users_spec.rb
@@ -2,16 +2,18 @@ require "rails_helper"
 
 RSpec.describe "/settings/users", :multiuser do
   describe "GET /index", :as_moderator do
+    before { create(:user) }
+
     it "renders a successful response" do
-      create(:user)
       get "/settings/users"
       expect(response).to be_successful
     end
   end
 
   describe "GET /show", :as_moderator do
+    let(:user) { create(:user) }
+
     it "renders a successful response" do
-      user = create(:user)
       get "/settings/users/#{user.to_param}"
       expect(response).to be_successful
     end
