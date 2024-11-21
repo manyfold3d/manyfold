83f28ddd47591d650b76967ae130c58efe0d8d56
James Smith <james@floppy.org.uk>
2024-11-21 14:32:54 +0000

    don't add a destroy user method

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   if SiteSettings.multiuser_enabled? || Rails.env.test?
     authenticate :user, lambda { |u| u.is_moderator? } do
       namespace :settings do
-        resources :users
+        resources :users, except: [:destroy]
       end
     end
     mount Federails::Engine => "/" if SiteSettings.federation_enabled? || Rails.env.test?
