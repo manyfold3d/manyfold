f617b78f3e136c96bcb855e24398824bda9f6c4b
James Smith <james@floppy.org.uk>
2024-12-10 13:35:26 +0000

    use SameSite=Lax cookies to allow auto-login after linking
    resolves #3294

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -175,7 +175,7 @@ Devise.setup do |config|
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
   config.rememberable_options = {
-    same_site: :strict,
+    same_site: :lax,
     secure: Rails.application.config.force_ssl
   }
 
--- config/initializers/session_storage.rb
+++ config/initializers/session_storage.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
   expire_after: 14.days,
   key: "_manyfold_session",
-  same_site: :strict,
+  same_site: :lax,
   secure: Rails.application.config.force_ssl
