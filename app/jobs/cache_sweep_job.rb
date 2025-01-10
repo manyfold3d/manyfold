b292278b777adbf4a6d7d2fc93ef61e9d4a7578f
James Smith <james@floppy.org.uk>
2025-01-10 16:35:51 +0000

    add cache expiry note for tus

--- app/jobs/cache_sweep_job.rb
+++ app/jobs/cache_sweep_job.rb
@@ -2,5 +2,6 @@ class CacheSweepJob < ApplicationJob
   def perform
     cache = LibraryUploader.storages[:cache]
     cache.clear! { |path| path.mtime < 6.hours.ago }
+    # Don't need to clear tus separately, the storage is the same place
   end
 end
