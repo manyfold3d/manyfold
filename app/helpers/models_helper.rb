3623a545da1627b9c127f04af8469f79418af127
James Smith <james@floppy.org.uk>
2025-01-17 11:34:53 +0000

    Organize files into groups of at least 5% of total count

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -2,7 +2,7 @@ module ModelsHelper
   def group(files)
     return {} if files.empty?
     sections = {}
-    min_section_size = 2
+    min_section_size = (files.count * 0.05).round
     min_prefix_length = 3
     names = files.map { |it| it.filename.downcase }
     slice = names.map(&:length).max
