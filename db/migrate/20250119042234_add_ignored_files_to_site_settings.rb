1f6182bac921210a0062a67373adf7847600b31f
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-01-19 16:38:59 +0900

    Fix migration

--- db/migrate/20250119042234_add_ignored_files_to_site_settings.rb
+++ db/migrate/20250119042234_add_ignored_files_to_site_settings.rb
@@ -1,5 +1,5 @@
 class AddIgnoredFilesToSiteSettings < ActiveRecord::Migration[7.2]
   def change
-    add_column :site_settings, :ignored_files, :text, array: true, default: []
+    add_column :site_settings, :ignored_files, :text
   end
 end
