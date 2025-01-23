062c8d36be26a46cdbf4430ef89eead8eadefd2d
James Smith <james@floppy.org.uk>
2025-01-23 10:28:45 +0000

    add creator links as attachments in activitystreams

--- spec/factories/creator.rb
+++ spec/factories/creator.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     sequence(:public_id) { |n| "creator_#{n}" }
     caption { Faker::Lorem.sentence }
     notes { Faker::Lorem.paragraph }
+    links_attributes { [{url: "http://example.com"}] }
   end
 end
