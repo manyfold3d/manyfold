3d7f5724c57f1a19cc3fa815611b25c6f55ae718
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-01-24 21:27:05 +0900

    Remove unecessary migrations

--- db/migrate/20250119042234_add_ignored_files_to_site_settings.rb
+++
@@ -1,9 +0,0 @@
-class AddIgnoredFilesToSiteSettings < ActiveRecord::Migration[7.2]
-  def change
-    add_column :site_settings, :ignored_files, :text, null: true, default: [
-      /^\.[^\.]+/, # Hidden files starting with .
-      /.*\/@eaDir\/.*/, # Synology temp files
-      /__MACOSX/ # MACOS resource forks
-    ]
-  end
-end
