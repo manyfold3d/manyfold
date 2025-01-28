be6a899890f3b9bcbb7e0830d629d68e4863bb5b
James Smith <james@floppy.org.uk>
2025-01-28 14:57:53 +0000

    add guards to stop serialization of non-public objects

--- app/serializers/activity_pub/model_serializer.rb
+++ app/serializers/activity_pub/model_serializer.rb
@@ -1,6 +1,7 @@
 module ActivityPub
   class ModelSerializer < ApplicationSerializer
     def serialize
+      raise ActiveRecord::RecordNotFound unless federate? # Temporary guard against publishing non-public Federails::ActorEntity objects
       {
         "@context": {
           f3di: "http://purl.org/f3di/ns#"
