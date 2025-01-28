20b40062556aa47524c41e9c3b2cc80005a86b81
James Smith <james@floppy.org.uk>
2025-01-28 19:19:19 +0000

    add hashtags to serialized models

--- app/serializers/activity_pub/model_serializer.rb
+++ app/serializers/activity_pub/model_serializer.rb
@@ -4,13 +4,15 @@ module ActivityPub
       raise ActiveRecord::RecordNotFound unless federate? # Temporary guard against publishing non-public Federails::ActorEntity objects
       {
         "@context": {
-          f3di: "http://purl.org/f3di/ns#"
+          f3di: "http://purl.org/f3di/ns#",
+          Hashtag: "as:Hashtag"
         },
         summary: summary_html,
         "f3di:concreteType": "Model",
         attachment: @object.links.map { |it| {type: "Link", href: it.url} },
-        sensitive: @object.sensitive
-      }.merge(address_fields)
+        sensitive: @object.sensitive,
+        tag: hashtags
+      }.compact.merge(address_fields)
     end
 
     def cc
@@ -19,6 +21,18 @@ module ActivityPub
         @object.creator&.federails_actor&.followers_url,
         @object.collection&.federails_actor&.followers_url
       ].compact
+    end
+
+    private
+
+    def hashtags
+      @object.tags.pluck(:name).map do |tag|
+        {
+          type: "Hashtag",
+          name: "##{tag.tr(" ", "_").camelize}",
+          href: Rails.application.routes.url_helpers.models_url(tag: tag)
+        }
+      end
     end
   end
 end
