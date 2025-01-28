6f586be8a208e275cb727661c9eda76f8d68924f
James Smith <james@floppy.org.uk>
2025-01-28 14:40:19 +0000

    add model and collection serializers for future use

---
+++ app/serializers/activity_pub/collection_serializer.rb
@@ -0,0 +1,36 @@
+module ActivityPub
+  class CollectionSerializer < ApplicationSerializer
+    def serialize
+      {
+        "@context": {
+          f3di: "http://purl.org/f3di/ns#"
+        },
+        summary: summary_html,
+        "f3di:concreteType": "Collection",
+        attachment: @object.links.map { |it| {type: "Link", href: it.url} }
+      }.merge(address_fields)
+    end
+
+    def federate?
+      # Currently unused
+      public?
+    end
+
+    def to
+      PUBLIC_COLLECTION if public?
+    end
+
+    def cc
+      [
+        @object.federails_actor.followers_url,
+        @object.collection&.federails_actor&.followers_url
+      ].compact
+    end
+
+    private
+
+    def public?
+      CollectionPolicy.new(nil, @object).show?
+    end
+  end
+end
---
+++ app/serializers/activity_pub/model_serializer.rb
@@ -0,0 +1,37 @@
+module ActivityPub
+  class ModelSerializer < ApplicationSerializer
+    def serialize
+      {
+        "@context": {
+          f3di: "http://purl.org/f3di/ns#"
+        },
+        summary: summary_html,
+        "f3di:concreteType": "Model",
+        attachment: @object.links.map { |it| {type: "Link", href: it.url} }
+      }.merge(address_fields)
+    end
+
+    def federate?
+      # Currently unused
+      public?
+    end
+
+    def to
+      PUBLIC_COLLECTION if public?
+    end
+
+    def cc
+      [
+        @object.federails_actor.followers_url,
+        @object.creator&.federails_actor&.followers_url,
+        @object.collection&.federails_actor&.followers_url
+      ].compact
+    end
+
+    private
+
+    def public?
+      ModelPolicy.new(nil, @object).show?
+    end
+  end
+end
