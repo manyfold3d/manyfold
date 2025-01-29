2a0ea6899db72c8d51f73b9030f6f2a0b14e0eb8
James Smith <james@floppy.org.uk>
2025-01-29 15:55:49 +0000

    add actor factory

---
+++ spec/factories/federails/actor.rb
@@ -0,0 +1,33 @@
+FactoryBot.define do
+  factory :actor, class: "Federails::Actor" do
+    name { Faker::Name.name }
+    entity { nil }
+    federated_url { "https://example.com/actors/#{rand(1...10_000)}" }
+    username { Faker::Internet.username separators: ["-", "_"] }
+    server { "example.com" }
+    inbox_url { "#{federated_url}/inbox" }
+    outbox_url { "#{federated_url}/outbox" }
+    followers_url { "#{federated_url}/followers" }
+    followings_url { "#{federated_url}/followings" }
+    profile_url { "https://example.com/users/#{federated_url.split("/").last}" }
+    extensions {
+      {
+        summary: Faker::Lorem.sentence,
+        content: Faker::Lorem.paragraph
+      }
+    }
+
+    factory :model_actor do
+      extensions {
+        {
+          summary: Faker::Lorem.sentence,
+          content: Faker::Lorem.paragraph,
+          "f3di:concreteType": "3DModel",
+          "spdx:license": {
+            "spdx:licenseId": "MIT"
+          }
+        }
+      }
+    end
+  end
+end
