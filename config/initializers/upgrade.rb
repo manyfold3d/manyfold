3efe6e35f588f6ac4a525eede6ec9337bbe083fa
James Smith <james@floppy.org.uk>
2025-02-27 15:53:59 +0000

    Add upgrade job to backfill datapackages in the background

--- config/initializers/upgrade.rb
+++ config/initializers/upgrade.rb
@@ -1,4 +1,5 @@
 Rails.application.config.after_initialize do
   Upgrade::FixNilFileSizeValues.set(queue: :upgrade).perform_async
+  Upgrade::BackfillDataPackages.set(queue: :upgrade).perform_later
 rescue RedisClient::CannotConnectError
 end
