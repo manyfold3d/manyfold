0c7bd22e94abcd56921913a5eeb6b5dc7ff626e3
James Smith <james@floppy.org.uk>
2025-02-27 16:41:09 +0000

    disable job uniqueness in test mode

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,6 +1,7 @@
 RSpec.configure do |config|
   config.before do
     ActiveJob::Base.queue_adapter = :test
+    ActiveJob::Uniqueness.test_mode!
     allow(Sidekiq::Queue).to receive(:new).with("scan").and_return([])
   end
 end
