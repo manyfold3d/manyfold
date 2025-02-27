8295a1710edd4fd6bd4931bd01591984209c0fa6
James Smith <james@floppy.org.uk>
2025-02-27 20:47:27 +0000

    attempt to connect to redis before queueing upgrade jobs

--- config/initializers/upgrade.rb
+++ config/initializers/upgrade.rb
@@ -1,4 +1,7 @@
 Rails.application.config.after_initialize do
+  # Attempt to connect to Redis first before queueing, and fail early
+  Sidekiq.redis { |conn| conn.info }
+  # Queue upgrade jobs if Redis is good to go
   Upgrade::FixNilFileSizeValues.set(queue: :upgrade).perform_later
   Upgrade::BackfillDataPackages.set(queue: :upgrade).perform_later
 rescue RedisClient::CannotConnectError
