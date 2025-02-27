d6e98323461a5e061e2c90744f2ee84cabe8f310
James Smith <james@floppy.org.uk>
2025-02-27 14:35:04 +0000

    Improve datapackage file creation
    Avoiding using Rack::Test::UploadedFile which doesn't work in production

--- app/jobs/update_datapackage_job.rb
+++ app/jobs/update_datapackage_job.rb
@@ -1,15 +1,12 @@
 class UpdateDatapackageJob < ApplicationJob
   def perform(model_id)
     model = Model.find(model_id)
-
-    datapackage = model.model_files.including_special.find_or_create_by(filename: "datapackage.json")
-
-    json_file = Rack::Test::UploadedFile.new(
-      StringIO.new(JSON.pretty_generate(DataPackage::ModelSerializer.new(model).serialize)),
-      "application/json",
-      original_filename: "datapackage.json"
-    )
-
-    datapackage.update attachment: json_file
+    file = model.model_files.including_special.find_or_initialize_by(filename: "datapackage.json")
+    file.update!(attachment: LibraryUploader.upload(
+      StringIO.new(
+        JSON.pretty_generate(DataPackage::ModelSerializer.new(model).serialize)
+      ),
+      :cache
+    ))
   end
 end
