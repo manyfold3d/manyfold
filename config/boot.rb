0103408e0974f09bc16cebfd903e8c00c0ff4291
James Smith <james@floppy.org.uk>
2025-03-03 12:06:55 +0000

    suppress parser/current warning

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Ignore parser/current warning so that it doesn't complain about patchlevel differences
+require "warning"
+Warning.ignore(/warning: parser\/current/)
