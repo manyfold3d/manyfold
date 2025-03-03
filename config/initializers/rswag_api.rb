86e759dca7777e23ee50e7492610fc7a7d083de6
James Smith <james@floppy.org.uk>
2025-03-03 17:00:50 +0000

    set server URL from Rails root_url

--- config/initializers/rswag_api.rb
+++ config/initializers/rswag_api.rb
@@ -8,6 +8,7 @@ Rswag::Api.configure do |c|
   # Inject a lambda function to alter the returned Swagger prior to serialization
   # The function will have access to the rack env for the current request
   # For example, you could leverage this to dynamically assign the "host" property
-  #
-  # c.swagger_filter = lambda { |swagger, env| swagger['host'] = env['HTTP_HOST'] }
+  c.swagger_filter = lambda { |swagger, env|
+    swagger["servers"][0]["url"] = Rails.application.routes.url_helpers.root_url
+  }
 end
