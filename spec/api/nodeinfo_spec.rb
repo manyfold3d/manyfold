496689efebf340ac17af4036030601082c229dbb
James Smith <james@floppy.org.uk>
2025-03-03 17:02:00 +0000

    add OpenAPI definitions for NodeInfo endpoints

--- spec/api/nodeinfo_spec.rb
+++ spec/api/nodeinfo_spec.rb
@@ -1,16 +1,25 @@
 # spec/requests/blogs_spec.rb
 require "swagger_helper"
 
-describe "Nodeinfo" do
+describe "NodeInfo" do # rubocop:disable RSpec/EmptyExampleGroup
   path "/.well-known/nodeinfo" do
-    get "Get nodeinfo URL" do
-      tags "Nodeinfo"
-      produces "application/json"
+    get "Discovery URL for NodeInfo protocol" do
+      tags "NodeInfo"
+      produces "application/json; profile=\"http://nodeinfo.diaspora.software/ns/schema/2.0#\""
 
-      response "200", "blog found" do
+      response "200", "Success" do
         schema type: :object,
           properties: {
-            links: {type: :array}
+            links: {
+              type: :array,
+              items: {
+                type: :object,
+                properties: {
+                  rel: {type: :string, example: "https://nodeinfo.diaspora.software/ns/schema/2.0"},
+                  href: {type: :string, example: "/nodeinfo/2.0", description: "The URL to the full NodeInfo document"}
+                }
+              }
+            }
           },
           required: ["links"]
 
@@ -20,19 +29,52 @@ describe "Nodeinfo" do
   end
 
   path "/nodeinfo/2.0" do
-    get "Get nodeinfo" do
-      tags "Nodeinfo"
-      produces "application/json"
+    get "Get server metadata in NodeInfo 2.0 format" do
+      tags "NodeInfo"
+      produces "application/json; profile=\"http://nodeinfo.diaspora.software/ns/schema/2.0#\""
 
-      response "200", "node info" do
+      response "200", "Success" do
         schema type: :object,
           properties: {
-            version: {type: :string},
-            protocols: {type: :array},
-            services: {type: :object},
-            openRegistrations: {type: :coolean}
+            version: {type: :string, example: "2.0"},
+            software: {
+              type: :object,
+              properties: {
+                name: {type: :string, example: "Manyfold"},
+                version: {type: :string, example: "v0.103.0"}
+              }
+            },
+            protocols: {
+              type: :array,
+              items: {
+                type: :string,
+                example: "activitypub"
+              }
+            },
+            services: {
+              type: :object,
+              properties: {
+                inbound: {type: :array, items: {type: :string}, example: []},
+                outbound: {type: :array, items: {type: :string}, example: []}
+              }
+            },
+            openRegistrations: {type: :boolean},
+            usage: {
+              type: :object,
+              properties: {
+                users: {
+                  type: :object,
+                  properties: {
+                    total: {type: :integer},
+                    activeMonth: {type: :integer},
+                    activeHalfyear: {type: :integer}
+                  }
+                }
+              }
+            },
+            metadata: {type: :object}
           },
-          required: ["version", "protocols", "services", "openRegistrations"]
+          required: ["version", "software", "protocols", "openRegistrations"]
 
         run_test!
       end
