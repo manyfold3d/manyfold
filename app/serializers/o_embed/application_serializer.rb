4fb9967f22f445af32d6d253c7a817682d620eb7
James Smith <james@floppy.org.uk>
2025-03-06 19:04:00 +0000

    add all properties to oembed model serializer

--- app/serializers/o_embed/application_serializer.rb
+++ app/serializers/o_embed/application_serializer.rb
@@ -1,14 +1,17 @@
 module OEmbed
   class ApplicationSerializer
-    def initialize(object)
+    def initialize(object, maxwidth: nil, maxheight: nil)
       @object = object
+      @maxwidth = maxwidth
+      @maxheight = maxheight
     end
 
-    def basic_properties
+    def generic_properties
       {
         version: "1.0",
         provider_name: ENV.fetch("SITE_NAME", "Manyfold"),
-        provider_url: Rails.application.routes.url_helpers.root_url
+        provider_url: Rails.application.routes.url_helpers.root_url,
+        cache_age: 86400
       }
     end
   end
--- app/serializers/o_embed/model_serializer.rb
+++ app/serializers/o_embed/model_serializer.rb
@@ -2,8 +2,81 @@ module OEmbed
   class ModelSerializer < ApplicationSerializer
     def serialize
       {
-        type: "rich"
-      }.merge(basic_properties)
+        title: @object.name
+      }.merge(generic_properties, author_properties, type_properties)
+    end
+
+    private
+
+    def author_properties
+      return {} if @object.creator.nil?
+      {
+        author_name: @object.creator.name,
+        author_url: Rails.application.routes.url_helpers.creator_url(@object.creator)
+      }
+    end
+
+    def type_properties
+      if @object.preview_file.is_image?
+        photo_properties
+      elsif @object.preview_file.is_renderable?
+        renderable_properties
+      elsif @object.preview_file.is_video?
+        video_properties
+      else
+        link_properties
+      end
+    end
+
+    def link_properties
+      {
+        type: "link"
+      }
+    end
+
+    def photo_properties
+      width = @maxwidth || 512 # TODO proper scale calc based on image size
+      height = @maxheight || 512
+      {
+        type: "photo",
+        url: Rails.application.routes.url_helpers.model_model_file_url(@object, @object.preview_file, format: @object.preview_file.extension),
+        width: width,
+        height: height
+      }
+    end
+
+    def video_properties
+      width = @maxwidth || 512
+      height = Math.min(@maxheight, width * 0.75) # TODO proper aspect ratio calculation
+      html = <<~EOF
+        <video controls width="#{width}" height="#{height}">
+          <source
+            src="#{Rails.application.routes.url_helpers.model_model_file_url(@object, @object.preview_file, format: @object.preview_file.extension)}"
+            type="#{@object.preview_file.mime_type}"
+          />
+        </video>
+      EOF
+      {
+        type: "video",
+        html: html,
+        width: width,
+        height: height
+      }
+    end
+
+    def renderable_properties
+      width = @maxwidth || 512
+      height = @maxheight || 512
+      html = <<~EOF
+        <iframe>
+        </iframe>
+      EOF
+      {
+        type: "rich",
+        html: html,
+        width: width,
+        height: height
+      }
     end
   end
 end
