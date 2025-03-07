1a1d8e7163288f241d4462f62f6636afa0e0b206
James Smith <james@floppy.org.uk>
2025-03-07 16:36:45 +0000

    refactor lots of oembed serializer code into base class

--- app/serializers/o_embed/application_serializer.rb
+++ app/serializers/o_embed/application_serializer.rb
@@ -14,5 +14,79 @@ module OEmbed
         cache_age: 86400
       }
     end
+
+    def author_properties(creator)
+      return {} if creator.nil?
+      {
+        author_name: creator.name,
+        author_url: Rails.application.routes.url_helpers.creator_url(creator)
+      }
+    end
+
+    def model_file_properties(model_file)
+      if model_file&.is_image?
+        photo_properties(model_file)
+      elsif model_file&.is_renderable?
+        renderable_properties(model_file)
+      elsif model_file&.is_video?
+        video_properties(model_file)
+      else
+        link_properties
+      end
+    end
+
+    def link_properties
+      {
+        type: "link"
+      }
+    end
+
+    def photo_properties(model_file)
+      width = @maxwidth || 512 # TODO proper scale calc based on image size
+      height = @maxheight || 512
+      {
+        type: "photo",
+        url: Rails.application.routes.url_helpers.model_model_file_url(model_file.model, model_file, format: model_file.extension),
+        width: width,
+        height: height
+      }
+    end
+
+    def video_properties(model_file)
+      width = @maxwidth || 512
+      height = [@maxheight, width * 0.75].compact.min # TODO proper aspect ratio calculation
+      html = <<~EOF
+        <video controls width="#{width}" height="#{height}">
+          <source
+            src="#{Rails.application.routes.url_helpers.model_model_file_url(model_file.model, model_file, format: model_file.extension)}"
+            type="#{model_file.mime_type}"
+          />
+        </video>
+      EOF
+      {
+        type: "video",
+        html: html,
+        width: width,
+        height: height
+      }
+    end
+
+    def renderable_properties(model_file)
+      width = @maxwidth || 512
+      height = @maxheight || 512
+      html = <<~EOF
+        <iframe
+          src="#{Rails.application.routes.url_helpers.model_model_file_url(model_file.model, model_file, embed: true)}"
+          width="#{width}" height="#{height}" loading="lazy" referrerpolicy="no-referrer" scrolling="no" style="overflow:hidden"
+          sandbox="allow-scripts allow-pointer-lock allow-same-origin"
+        </iframe>
+      EOF
+      {
+        type: "rich",
+        html: html,
+        width: width,
+        height: height
+      }
+    end
   end
 end
--- app/serializers/o_embed/model_serializer.rb
+++ app/serializers/o_embed/model_serializer.rb
@@ -3,83 +3,11 @@ module OEmbed
     def serialize
       {
         title: @object.name
-      }.merge(generic_properties, author_properties, type_properties)
-    end
-
-    private
-
-    def author_properties
-      return {} if @object.creator.nil?
-      {
-        author_name: @object.creator.name,
-        author_url: Rails.application.routes.url_helpers.creator_url(@object.creator)
-      }
-    end
-
-    def type_properties
-      if @object.preview_file&.is_image?
-        photo_properties
-      elsif @object.preview_file&.is_renderable?
-        renderable_properties
-      elsif @object.preview_file&.is_video?
-        video_properties
-      else
-        link_properties
-      end
-    end
-
-    def link_properties
-      {
-        type: "link"
-      }
-    end
-
-    def photo_properties
-      width = @maxwidth || 512 # TODO proper scale calc based on image size
-      height = @maxheight || 512
-      {
-        type: "photo",
-        url: Rails.application.routes.url_helpers.model_model_file_url(@object, @object.preview_file, format: @object.preview_file.extension),
-        width: width,
-        height: height
-      }
-    end
-
-    def video_properties
-      width = @maxwidth || 512
-      height = [@maxheight, width * 0.75].compact.min # TODO proper aspect ratio calculation
-      html = <<~EOF
-        <video controls width="#{width}" height="#{height}">
-          <source
-            src="#{Rails.application.routes.url_helpers.model_model_file_url(@object, @object.preview_file, format: @object.preview_file.extension)}"
-            type="#{@object.preview_file.mime_type}"
-          />
-        </video>
-      EOF
-      {
-        type: "video",
-        html: html,
-        width: width,
-        height: height
-      }
-    end
-
-    def renderable_properties
-      width = @maxwidth || 512
-      height = @maxheight || 512
-      html = <<~EOF
-        <iframe
-          src="#{Rails.application.routes.url_helpers.model_model_file_url(@object, @object.preview_file, embed: true)}"
-          width="#{width}" height="#{height}" loading="lazy" referrerpolicy="no-referrer" scrolling="no" style="overflow:hidden"
-          sandbox="allow-scripts allow-pointer-lock allow-same-origin"
-        </iframe>
-      EOF
-      {
-        type: "rich",
-        html: html,
-        width: width,
-        height: height
-      }
+      }.merge(
+        generic_properties,
+        author_properties(@object.creator),
+        model_file_properties(@object.preview_file)
+      )
     end
   end
 end
