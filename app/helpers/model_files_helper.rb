673ff0d6f12e12d9b7a5e13079a8cae83a72353e
James Smith <james@floppy.org.uk>
2025-03-14 16:19:04 +0000

    add slicer link helper

--- app/helpers/model_files_helper.rb
+++ app/helpers/model_files_helper.rb
@@ -1,2 +1,25 @@
 module ModelFilesHelper
+  def slicer_url(slicer, file)
+    case slicer
+    when :orca
+      slic3r_family_open_url "orcaslicer", file
+    when :prusa
+      slic3r_family_open_url "prusaslicer", file
+    when :bambu
+      slic3r_family_open_url "bambustudio", file
+    when :cura
+      slic3r_family_open_url "cura", file
+    end
+  end
+
+  private
+
+  def slic3r_family_open_url(scheme, file)
+    url = model_url(file.model) + "/model_files/#{file.signed_id}.#{file.extension}"
+    URI::Generic.new(
+      scheme, nil,
+      "open", nil, nil, nil, nil,
+      {file: url}.to_query, nil
+    ).to_s
+  end
 end
