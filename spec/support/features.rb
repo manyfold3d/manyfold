241d454e5b2ee3f854915d0aaeeeada0cba34464
James Smith <james@floppy.org.uk>
2025-03-20 10:20:34 +0000

    add singleuser mode tag

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -3,6 +3,10 @@ RSpec.configure do |config|
     allow(SiteSettings).to receive(:multiuser_enabled?).and_return(true)
   end
 
+  config.before(:each, :singleuser) do
+    allow(SiteSettings).to receive(:multiuser_enabled?).and_return(false)
+  end
+
   config.before(:each, :demo_mode) do
     allow(SiteSettings).to receive(:demo_mode_enabled?).and_return(true)
   end
