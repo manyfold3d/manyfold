789bf7c401b7b438dfd45673f345f056aa0a3234
James Smith <james@floppy.org.uk>
2025-03-27 14:10:11 +0000

    add/move some helpers for stats

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -2,4 +2,38 @@ module SettingsHelper
   def masked_email(email)
     email.gsub(/(?<=^.)[^@]*|(?<=@.).*(?=\.[^.]+$)/, "****")
   end
+
+  def model_count(library: nil)
+    scope = policy_scope(Model)
+    scope = scope.where(library: library) if library
+    scope.count
+  end
+
+  def model_file_count(library: nil)
+    scope = policy_scope(ModelFile)
+    scope = scope.includes(:model).where("models.library": library) if library
+    scope.count
+  end
+
+  def total_file_size(library: nil)
+    scope = policy_scope(ModelFile)
+    scope = scope.includes(:model).where("models.library": library) if library
+    scope.sum(:size)
+  end
+
+  def creator_count
+    policy_scope(Creator).count
+  end
+
+  def collection_count
+    policy_scope(Collection).count
+  end
+
+  def tag_count
+    policy_scope(ActsAsTaggableOn::Tag).count
+  end
+
+  def user_count
+    policy_scope(User).count
+  end
 end
