5661304fdbcb101e5ef5e281da9db5cc77eaccff
James Smith <james@floppy.org.uk>
2025-04-02 17:36:20 +0100

    port tests of path component parser to ParseMetadataJob

--- spec/jobs/scan/model/parse_metadata_job_spec.rb
+++ spec/jobs/scan/model/parse_metadata_job_spec.rb
@@ -87,4 +87,65 @@ RSpec.describe Scan::Model::ParseMetadataJob do
       end
     end
   end
+
+  context "when parsing metadata from a path template" do
+    before do
+      allow(SiteSettings).to receive_messages(
+        model_tags_tag_model_directory_name: false,
+        parse_metadata_from_path: true,
+        model_tags_auto_tag_new: nil,
+      )
+    end
+
+    let(:model) { create(:model, path: "/top/middle/bottom/prefix - name#42") }
+
+    {
+      "{tags}" => %r{^/?.*?(?<tags>[[:print:]]*)$},
+      "{creator}" => %r{^/?.*?(?<creator>[[:print:]&&[^/]]*?)$},
+      "{collection}" => %r{^/?.*?(?<collection>[[:print:]&&[^/]]*?)$},
+      "{tags}/{creator}" => %r{^/?.*?(?<tags>[[:print:]]*)/(?<creator>[[:print:]&&[^/]]*?)$},
+      "{tags}/{creator}/{modelName}{modelId}" => %r{^/?.*?(?<tags>[[:print:]]*)/(?<creator>[[:print:]&&[^/]]*?)/(?<model_name>[[:print:]&&[^/]]*?)(?<model_id>#[[:digit:]]+)?$},
+      "@{creator}{modelId}" => %r{^/?.*?@(?<creator>[[:print:]&&[^/]]*?)(?<model_id>#[[:digit:]]+)?$},
+      "{creator}/{collection}/{tags}/{modelName}{modelId}" => %r{^/?.*?(?<creator>[[:print:]&&[^/]]*?)/(?<collection>[[:print:]&&[^/]]*?)/(?<tags>[[:print:]]*)/(?<model_name>[[:print:]&&[^/]]*?)(?<model_id>#[[:digit:]]+)?$}
+    }.each_pair do |tag, regexp|
+      it "correctly converts #{tag} into a regexp matcher" do
+        allow(SiteSettings).to receive(:model_path_template).and_return(tag)
+        expect(described_class.send(:path_parse_pattern)).to eql regexp
+      end
+    end
+
+    {
+      "{tags}/{modelName}{modelId}" => {
+        tags: ["top", "middle", "bottom"],
+        model_name: "prefix - name"
+      },
+      "{creator}/{modelName}{modelId}" => {
+        creator: "bottom",
+        model_name: "prefix - name"
+      },
+      "{collection}/{modelName}{modelId}" => {
+        collection: "bottom",
+        model_name: "prefix - name"
+      },
+      "{tags}/{creator}/{modelName}{modelId}" => {
+        creator: "bottom",
+        tags: ["top", "middle"],
+        model_name: "prefix - name"
+      },
+      "{creator}{modelId}" => {
+        creator: "prefix - name"
+      },
+      "{tags}/{creator}/{collection} - {modelName}{modelId}" => {
+        tags: ["top", "middle"],
+        creator: "bottom",
+        collection: "prefix",
+        model_name: "name"
+      }
+    }.each_pair do |tag, values|
+      it "correctly matches components of #{tag}" do
+        allow(SiteSettings).to receive(:model_path_template).and_return(tag)
+        expect(described_class.send(:extract_path_components, model)).to eql values
+      end
+    end
+  end
 end
