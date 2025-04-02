739595cc50d1d4be86d9fab8a796e98a8907cb7d
James Smith <james@floppy.org.uk>
2025-04-02 17:15:57 +0100

    port tags-from-folder-name tests from PathParser to ParseMetadataJob

--- spec/jobs/scan/model/parse_metadata_job_spec.rb
+++ spec/jobs/scan/model/parse_metadata_job_spec.rb
@@ -25,4 +25,50 @@ RSpec.describe Scan::Model::ParseMetadataJob do
     expect { described_class.perform_now(nil) }
       .to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  context "when generating tags from folder name" do
+    before do
+      allow(SiteSettings).to receive_messages(
+        model_tags_tag_model_directory_name: true,
+        parse_metadata_from_path: false,
+        model_tags_auto_tag_new: nil
+      )
+    end
+
+    context "without stop word filtering" do
+      before do
+        allow(SiteSettings).to receive(:model_tags_filter_stop_words).and_return(false)
+      end
+
+      it "skips single letter tags" do
+        model = create(:model, path: "/library1/stuff/a")
+        described_class.perform_now(model.id)
+        expect(model.tag_list).to eq []
+      end
+
+      it "generates tag from whitespace delimited file names" do
+        model = create(:model, path: "/library1/stuff/this is a fantasy model", tags: [])
+        described_class.perform_now(model.id)
+        expect(model.tag_list).to eq ["this", "is", "fantasy", "model"]
+      end
+
+      it "generates tag from underscore delimited file names" do
+        model = create(:model, path: "/library1/stuff/this_is_a_fantasy_model")
+        described_class.perform_now(model.id)
+        expect(model.tag_list).to eq ["this", "is", "fantasy", "model"]
+      end
+
+      it "generates tag from plus delimited file names" do
+        model = create(:model, path: "/library1/stuff/this+is+a+fantasy+model")
+        described_class.perform_now(model.id)
+        expect(model.tag_list).to eq ["this", "is", "fantasy", "model"]
+      end
+
+      it "generates tag from hyphen delimited file names" do
+        model = create(:model, path: "/library1/stuff/this-is-a-fantasy-model")
+        described_class.perform_now(model.id)
+        expect(model.tag_list).to eq ["this", "is", "fantasy", "model"]
+      end
+    end
+  end
 end
