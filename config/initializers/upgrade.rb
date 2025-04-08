2254a03ffd344fe36e2ed0dd1b4dff244d5510c2
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-04-08 21:38:29 +0900

    Fixing linting errors

--- config/initializers/upgrade.rb
+++ config/initializers/upgrade.rb
@@ -2,8 +2,8 @@ Rails.application.config.after_initialize do
   # Attempt to connect to Redis first before queueing, and fail early
   Sidekiq.redis { |conn| conn.info }
   # Queue upgrade jobs if Redis is good to go
-  #
-  if Rails.const_defined?( "Server" )
+  # and if in server mode
+  if Rails.const_defined?(:Server)
     Upgrade::FixNilFileSizeValues.set(queue: :upgrade).perform_later
     Upgrade::BackfillDataPackages.set(queue: :upgrade).perform_later
     Upgrade::DisambiguateUsernamesJob.set(queue: :upgrade).perform_later
