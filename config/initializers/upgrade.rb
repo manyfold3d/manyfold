d0f171061b5ec1c010f111912e34b6f92a931807
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-04-08 21:35:57 +0900

    Fixes upgrade initializer to only run when as server and not in rails console sessions

--- config/initializers/upgrade.rb
+++ config/initializers/upgrade.rb
@@ -2,9 +2,12 @@ Rails.application.config.after_initialize do
   # Attempt to connect to Redis first before queueing, and fail early
   Sidekiq.redis { |conn| conn.info }
   # Queue upgrade jobs if Redis is good to go
-  Upgrade::FixNilFileSizeValues.set(queue: :upgrade).perform_later
-  Upgrade::BackfillDataPackages.set(queue: :upgrade).perform_later
-  Upgrade::DisambiguateUsernamesJob.set(queue: :upgrade).perform_later
-  Upgrade::UpdateActorsJob.set(queue: :upgrade).perform_later
+  #
+  if Rails.const_defined?( "Server" )
+    Upgrade::FixNilFileSizeValues.set(queue: :upgrade).perform_later
+    Upgrade::BackfillDataPackages.set(queue: :upgrade).perform_later
+    Upgrade::DisambiguateUsernamesJob.set(queue: :upgrade).perform_later
+    Upgrade::UpdateActorsJob.set(queue: :upgrade).perform_later
+  end
 rescue RedisClient::CannotConnectError
 end
