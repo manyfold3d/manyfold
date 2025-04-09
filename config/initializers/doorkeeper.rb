3d103debb9d46a9a80683b193fbb3fca4268d305
James Smith <james@floppy.org.uk>
2025-04-09 22:02:20 +0100

    forbid data and vbscript redirects
    Resolves #3963
    
    Co-authored-by: Emelia Smith <30827+ThisIsMissEm@users.noreply.github.com>

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -12,7 +12,7 @@ Doorkeeper.configure do
   authorization_code_expires_in 10.minutes
   access_token_expires_in 2.hours
   force_ssl_in_redirect_uri Manyfold::Application.config.force_ssl
-  forbid_redirect_uri { |uri| uri.scheme.to_s.downcase == "javascript" }
+  forbid_redirect_uri { |uri| %w[data vbscript javascript].include?(uri.scheme.to_s.downcase) }
 
   # Authentication
   resource_owner_from_credentials { nil }
