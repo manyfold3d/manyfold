f4e5b3bd7b4b2961cf64223433e8597560b06fa3
James Smith <james@floppy.org.uk>
2025-04-17 17:02:17 +0100

    enable branch coverage measurement

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,9 @@ require "spec_helper"
 
 ENV["RAILS_ENV"] ||= "test"
 require "simplecov"
-SimpleCov.start "rails"
+SimpleCov.start "rails" do
+  enable_coverage :branch
+end
 
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
