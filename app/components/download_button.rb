8baccb81d224bd50adac9f4192fdb05f530bc80b
James Smith <james@floppy.org.uk>
2025-05-08 10:38:26 +0100

    only add download intent to link if download is ready

--- app/components/download_button.rb
+++ app/components/download_button.rb
@@ -38,7 +38,7 @@ class Components::DownloadButton < Components::Base
 
   def download_link(selection: nil, file_type: nil, html_class: "dropdown-item")
     downloader = ArchiveDownloadService.new(model: @model, selection: selection || file_type)
-    link_to model_path(@model, format: @format, selection: selection || file_type), class: html_class, download: "download" do
+    link_to model_path(@model, format: @format, selection: selection || file_type), class: html_class, download: (downloader.ready? ? "download" : nil) do
       if downloader.ready?
         icon("cloud-download", t("components.download_button.download.ready"))
       elsif downloader.preparing?
