b29c2cf617d95d5aed27780e51a2c557fc9452de
James Smith <james@floppy.org.uk>
2025-05-09 15:23:03 +0100

    add turbo stream refresh for model pages

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 require "rack/contrib"
