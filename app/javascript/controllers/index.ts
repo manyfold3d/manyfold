af68ecd82b45e609fa4f9920314b1d33878a3b56
James Smith <james@floppy.org.uk>
2025-05-13 18:32:58 +0100

    move tom-select setup into a stimulus controller

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -2,4 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application'
+
+import TagInputController from "./tag_input_controller"
+application.register("tag-input", TagInputController)
---
+++ app/javascript/controllers/tag_input_controller.ts
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from 'tom-select'
+import type { TomInput } from 'tom-select/dist/cjs/types'
+
+// Connects to data-controller="tag-input"
+export default class extends Controller {
+  connect (): void {
+    new TomSelect((this.element as TomInput), { // eslint-disable-line no-new
+      addPrecedence: true,
+      create: true,
+      plugins: ['remove_button'],
+      selectOnTab: true,
+      onItemAdd: function () {
+        this.setTextboxValue('')
+        this.refreshOptions()
+      }
+    })
+  }
+}
