766437bbd9d6f6ee598630b583910165dbe10f0e
James Smith <james@floppy.org.uk>
2025-05-14 18:09:43 +0100

    automatically include subfolders in model check if there aren't any nested models

--- app/jobs/scan/check_model_job.rb
+++ app/jobs/scan/check_model_job.rb
@@ -5,7 +5,7 @@ class Scan::CheckModelJob < ApplicationJob
   def perform(model_id)
     model = Model.find(model_id)
     # Scan for new files (runs integrity check automatically)
-    model.add_new_files_later
+    model.add_new_files_later(include_all_subfolders: !model.contains_other_models?)
     # Rerun analysis job on individual files
     model.model_files.without_special.each do |file|
       file.analyse_later
