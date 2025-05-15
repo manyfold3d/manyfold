f79693e82bccae6bb640a5c34a506302302e459d
James Smith <james@floppy.org.uk>
2025-05-15 21:23:40 +0100

    check that special characters work in searches

--- spec/services/query_parser_service_spec.rb
+++ spec/services/query_parser_service_spec.rb
@@ -85,4 +85,57 @@ RSpec.describe QueryParserService do
       })
     end
   end
+
+  context "with excluded terms" do
+    let(:query) { "cat in the -hat" }
+
+    it "includes minus operator on relevant term" do
+      expect(service.parse(query)[:query]).to include({
+        term: "hat",
+        operator: "-"
+      })
+    end
+  end
+
+  context "with field prefixes" do
+    let(:query) { "cat in the -tag:hat" }
+
+    it "includes prefix and operator on relevant term" do
+      expect(service.parse(query)[:query]).to include({
+        term: "hat",
+        operator: "-",
+        prefix: "tag"
+      })
+    end
+  end
+
+  context "with quoted phrase" do
+    let(:query) { "\"cat in\" the hat" }
+
+    it "includes quoted part as a single term" do
+      expect(service.parse(query)[:query]).to include({
+        term: "cat in"
+      })
+    end
+  end
+
+  context "when using non alphanumeric characters in terms" do
+    it "allows apostrophes in terms" do
+      expect(service.parse("Bob's Burgers")[:query]).to include({
+        term: "Bob's"
+      })
+    end
+
+    it "allows emoji in terms" do
+      expect(service.parse("I ❤️ NY")[:query]).to include({
+        term: "❤️"
+      })
+    end
+
+    it "allows hyphenated words" do
+      expect(service.parse("Miles Cholmondley-Warner")[:query]).to include({
+        term: "Cholmondley-Warner"
+      })
+    end
+  end
 end
