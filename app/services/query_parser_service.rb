71f82be9d94e08b93a28c64afac5e295cb04b220
James Smith <james@floppy.org.uk>
2025-05-15 13:07:52 +0100

    Convert parser to simple parslet grammar

--- app/services/query_parser_service.rb
+++ app/services/query_parser_service.rb
@@ -1,9 +1,5 @@
-class QueryParserService
-  def parse(query)
-    {
-      query: [
-        {term: query}
-      ]
-    }
-  end
+class QueryParserService < Parslet::Parser
+  root(:query)
+  rule(:query) { term.repeat.as(:query) }
+  rule(:term) { match("[a-zA-Z0-9\s]").repeat(1).as(:term) }
 end
