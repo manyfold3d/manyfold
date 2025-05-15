5110a064523b37df57a5ad7a8aa203b3a6f07cdb
James Smith <james@floppy.org.uk>
2025-05-15 19:59:12 +0100

    handle quoted strings in ransack search

--- app/services/ransack_query_transformer.rb
+++ app/services/ransack_query_transformer.rb
@@ -1,13 +1,12 @@
 class RansackQueryTransformer < Parslet::Transform
   rule(term: simple(:term)) { term.to_s }
   rule(query: sequence(:terms)) {
-    search = terms.join(" ")
     {
       m: "or",
-      name_cont: search,
-      tags_name_in: search,
-      creator_name_cont: search,
-      collection_name_cont: search
+      name_cont_any: terms,
+      tags_name_in: terms,
+      creator_name_cont_any: terms,
+      collection_name_cont_any: terms
     }
   }
 end
--- spec/services/ransack_query_transformer_spec.rb
+++ spec/services/ransack_query_transformer_spec.rb
@@ -12,19 +12,33 @@ RSpec.describe RansackQueryTransformer do
       end
 
       it "searches for string contained in model name field" do
-        expect(result[:name_cont]).to eq "cat in the hat"
+        expect(result[:name_cont_any]).to eq %w[cat in the hat]
       end
 
       it "searches for string contained in creator name field" do
-        expect(result[:creator_name_cont]).to eq "cat in the hat"
+        expect(result[:creator_name_cont_any]).to eq %w[cat in the hat]
       end
 
       it "searches for string contained in collection name field" do
-        expect(result[:collection_name_cont]).to eq "cat in the hat"
+        expect(result[:collection_name_cont_any]).to eq %w[cat in the hat]
       end
 
       it "searches for tag" do
-        expect(result[:tags_name_in]).to eq "cat in the hat"
+        expect(result[:tags_name_in]).to eq %w[cat in the hat]
+      end
+    end
+  end
+
+  context "with a quoted query" do
+    let(:query) { QueryParserService.new.parse("cat in \"the hat\"") }
+
+    context "when converting to ransack" do
+      it "searches fields using OR" do
+        expect(result[:m]).to eq "or"
+      end
+
+      it "treats quoted part as a single term" do
+        expect(result[:name_cont_any]).to eq ["cat", "in", "the hat"]
       end
     end
   end
