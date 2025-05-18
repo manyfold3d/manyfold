e142b3ae9b44f6878a630320f61bf5f2e7217f88
James Smith <james@floppy.org.uk>
2025-05-18 17:28:02 +0100

    refactor search help component a bit to reduce method length

--- app/components/search_help.rb
+++ app/components/search_help.rb
@@ -4,78 +4,81 @@ class Components::SearchHelp < Components::Base
       a "data-bs-toggle": "modal", "data-bs-target": "#search-help" do
         yield
       end
+      modal
+    end
+  end
 
-      render Components::Modal.new(id: "search-help", title: t("components.search_help.title")) do
-        p do
-          t("components.search_help.intro")
+  def modal
+    render Components::Modal.new(id: "search-help", title: t("components.search_help.title")) do
+      p do
+        t("components.search_help.intro")
+      end
+      table class: "table table-striped" do
+        tr do
+          td { code { "cat hat" } }
+          td { t("components.search_help.simple") }
         end
-        table class: "table table-striped" do
-          tr do
-            td { code { "cat hat" } }
-            td { t("components.search_help.simple") }
-          end
-          tr do
-            td { code { "cat or hat" } }
-            td { t("components.search_help.boolean") }
-          end
-          tr do
-            td do
-              code { "cat -hat" }
-              br
-              code { "cat !hat" }
-              br
-              code { "cat not hat" }
-            end
-            td { t("components.search_help.negation") }
-          end
-          tr do
-            td { code { '"cat hat"' } }
-            td { t("components.search_help.quotes") }
-          end
-          tr do
-            td { code { "(cat or hat) and not bat" } }
-            td { t("components.search_help.parentheses") }
-          end
-          tr do
-            td do
-              code { "tag = cat" }
-            end
-            td { t("components.search_help.tag") }
-          end
-          tr do
-            td do
-              code { "tag != cat" }
-            end
-            td { t("components.search_help.without_tag") }
-          end
-          tr do
-            td do
-              code { "notes ~ cat" }
-              br
-              code { "caption ~ cat" }
-              br
-              code { "creator ~ cat" }
-              br
-              code { "collection ~ cat" }
-            end
-            td { t("components.search_help.specific_fields") }
-          end
-          tr do
-            td do
-              code { "not set? tag" }
-            end
-            td { t("components.search_help.unset") }
-          end
-          tr do
-            td do
-              code { "filename = cat.stl" }
-            end
-            td { t("components.search_help.filename") }
-          end
+        tr do
+          td { code { "cat or hat" } }
+          td { t("components.search_help.boolean") }
         end
-        p do
-          t("components.search_help.more_details_html")
+        tr do
+          td do
+            code { "cat -hat" }
+            br
+            code { "cat !hat" }
+            br
+            code { "cat not hat" }
+          end
+          td { t("components.search_help.negation") }
         end
+        tr do
+          td { code { '"cat hat"' } }
+          td { t("components.search_help.quotes") }
+        end
+        tr do
+          td { code { "(cat or hat) and not bat" } }
+          td { t("components.search_help.parentheses") }
+        end
+        tr do
+          td do
+            code { "tag = cat" }
+          end
+          td { t("components.search_help.tag") }
+        end
+        tr do
+          td do
+            code { "tag != cat" }
+          end
+          td { t("components.search_help.without_tag") }
+        end
+        tr do
+          td do
+            code { "description ~ cat" }
+            br
+            code { "caption ~ cat" }
+            br
+            code { "creator ~ cat" }
+            br
+            code { "collection ~ cat" }
+          end
+          td { t("components.search_help.specific_fields") }
+        end
+        tr do
+          td do
+            code { "not set? tag" }
+          end
+          td { t("components.search_help.unset") }
+        end
+        tr do
+          td do
+            code { "filename = cat.stl" }
+          end
+          td { t("components.search_help.filename") }
+        end
+      end
+      p do
+        t("components.search_help.more_details_html")
       end
     end
   end
