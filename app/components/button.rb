fccbb4bf802eb66b66fcf836c1505c3e41a5d0a6
James Smith <james@floppy.org.uk>
2025-05-20 17:04:31 +0100

    Add button component

---
+++ app/components/button.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Components::Button < Components::Base
+  include Phlex::Rails::Helpers::LinkTo
+
+  def initialize(icon:, text:, href:, variant:, method: nil)
+    @icon = icon
+    @text = text
+    @href = href
+    @variant = variant
+    @method = method
+  end
+
+  def view_template
+    link_to(@href, method: @method, class: "btn btn-#{@variant}") do
+      if @icon
+        Icon(icon: @icon, label: @text)
+        whitespace
+      end
+      span { @text }
+    end
+  end
+end
