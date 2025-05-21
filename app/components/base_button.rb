9baab35ec11aa13c1ea5931a7b29a3e35eed4ef2
James Smith <james@floppy.org.uk>
2025-05-21 14:22:32 +0100

    add support for aria labels and data-confirm

--- app/components/base_button.rb
+++ app/components/base_button.rb
@@ -3,22 +3,24 @@
 class Components::BaseButton < Components::Base
   include Phlex::Rails::Helpers::ButtonTo
 
-  def initialize(icon:, label:, href:, variant:, method: nil, icon_only: false)
+  def initialize(label:, href:, variant:, icon: nil, method: nil, icon_only: false, aria_label: nil, confirm: nil)
     @icon = icon
     @label = label
     @href = href
     @variant = variant
     @method = method
     @icon_only = icon_only
+    @aria_label = aria_label
+    @confirm = confirm
   end
 
   def view_template
-    helper(@href, method: @method, class: "btn btn-#{@variant}", aria: {label: @icon_only ? @label : nil}) do
+    helper(@href, method: @method, class: "btn btn-#{@variant}", aria: {label: @aria_label || (@icon_only ? @label : nil)}, data: {confirm: @confirm}) do
       if @icon
         Icon(icon: @icon, label: @label)
         whitespace
       end
-      span { @label } unless @icon_only
+      span(class: @icon_only ? "visually-hidden" : nil) { @label }
     end
   end
 
--- app/components/dropdown_item.rb
+++ app/components/dropdown_item.rb
@@ -3,16 +3,18 @@
 class Components::DropdownItem < Components::Base
   include Phlex::Rails::Helpers::LinkTo
 
-  def initialize(icon:, label:, path:, method: nil)
+  def initialize(icon:, label:, path:, method: nil, aria_label: nil, confirm: nil)
     @icon = icon
     @label = label
     @path = path
     @method = method
+    @aria_label = aria_label
+    @confirm = confirm
   end
 
   def view_template
     li do
-      link_to @path, method: @method, class: "dropdown-item" do
+      link_to @path, method: @method, class: "dropdown-item", aria: {label: @aria_label}, data: {confirm: @confirm} do
         Icon(icon: @icon, label: @label)
         whitespace
         span { @label }
