7c8dd1f9eba8e93f0d584047b8e2e7aeb328c3b4
James Smith <james@floppy.org.uk>
2025-05-21 11:00:35 +0100

    DRY link/button into a common base

--- app/components/button.rb
+++ app/components/button.rb
@@ -1,24 +1,9 @@
 # frozen_string_literal: true
 
-class Components::Button < Components::Base
+class Components::Button < Components::BaseButton
   include Phlex::Rails::Helpers::ButtonTo
 
-  def initialize(icon:, label:, href:, variant:, method: nil, icon_only: false)
-    @icon = icon
-    @label = label
-    @href = href
-    @variant = variant
-    @method = method
-    @icon_only = icon_only
-  end
-
-  def view_template
-    button_to(@href, method: @method, class: "btn btn-#{@variant}", aria: {label: @icon_only ? @label : nil}) do
-      if @icon
-        Icon(icon: @icon, label: @label)
-        whitespace
-      end
-      span { @label } unless @icon_only
-    end
+  def helper(*args)
+    button_to(*args) { yield }
   end
 end
