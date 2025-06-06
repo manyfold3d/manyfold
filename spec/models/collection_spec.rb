5c0fa233be264a6d4b1b49ff967cad7ca081954b
James Smith <james@floppy.org.uk>
2025-06-06 14:36:49 +0100

    add tests for collection publish job

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Collection do
   it_behaves_like "Commentable"
   it_behaves_like "Caber::Object"
   it_behaves_like "Sluggable"
+
+  context "when creating a collection" do
+    let(:model) { create(:collection) }
+
+    it "doesn't queue any activity jobs" do
+      expect {
+        create(:collection)
+      }.not_to have_enqueued_job(Activity::CollectionPublishedJob)
+    end
+
+    it "queues publish activity job if the collection was created as public" do
+      expect {
+        create(:collection, :public)
+      }.to have_enqueued_job(Activity::CollectionPublishedJob).once
+    end
+
+    it "queues publish activity job if the collection created private but then made public" do
+      collection = create(:collection)
+      expect {
+        collection.grant_permission_to "view", nil
+      }.to have_enqueued_job(Activity::CollectionPublishedJob).once
+    end
+  end
 end
