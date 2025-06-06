c62876685dec45ee6a459536b369211845d9e1c9
James Smith <james@floppy.org.uk>
2025-06-06 11:48:37 +0100

    add tests for comment addressing
    currently not respected by federails though

--- spec/serializers/activity_pub/comment_serializer_spec.rb
+++ spec/serializers/activity_pub/comment_serializer_spec.rb
@@ -26,4 +26,64 @@ RSpec.describe ActivityPub::CommentSerializer do
       expect(ap["content"]).not_to include "<p role=\"list\">"
     end
   end
+
+  context "when a public creator comments on a public model" do
+    let(:creator) { create(:creator, :public) }
+    let(:model) { create(:model, :public) }
+    let(:comment) { create(:comment, commenter: creator, commentable: model) }
+
+    it "includes public collection in to" do
+      expect(serializer.to).to include "https://www.w3.org/ns/activitystreams#Public"
+    end
+
+    it "includes model followers collection in cc" do
+      expect(serializer.cc).to include model.federails_actor.followers_url
+    end
+
+    it "includes creator followers collection in cc" do
+      expect(serializer.cc).to include creator.federails_actor.followers_url
+    end
+  end
+
+  context "when a public creator comments on a public model in a public collection" do
+    let(:creator) { create(:creator, :public) }
+    let(:collection) { create(:collection, :public) }
+    let(:model) { create(:model, :public, collection: collection) }
+    let(:comment) { create(:comment, commenter: creator, commentable: model) }
+
+    it "includes collection followers collection in cc" do
+      expect(serializer.cc).to include collection.federails_actor.followers_url
+    end
+  end
+
+  context "when a public creator comments on a public collection" do
+    let(:creator) { create(:creator, :public) }
+    let(:collection) { create(:collection, :public) }
+    let(:comment) { create(:comment, commenter: creator, commentable: collection) }
+
+    it "includes collection followers collection in cc" do
+      expect(serializer.cc).to include collection.federails_actor.followers_url
+    end
+  end
+
+  context "when a public collection comments on itself with a public creator" do
+    let(:creator) { create(:creator, :public) }
+    let(:collection) { create(:collection, :public, creator: creator) }
+    let(:comment) { create(:comment, commenter: collection, commentable: collection) }
+
+    it "includes creator followers collection in cc" do
+      expect(serializer.cc).to include creator.federails_actor.followers_url
+    end
+  end
+
+  context "when a public creator comments on a public collection that is itself inside a public collection" do
+    let(:creator) { create(:creator, :public) }
+    let(:parent_collection) { create(:collection, :public) }
+    let(:collection) { create(:collection, :public, collection: parent_collection) }
+    let(:comment) { create(:comment, commenter: creator, commentable: collection) }
+
+    it "includes parent collection followers collection in cc" do
+      expect(serializer.cc).to include parent_collection.federails_actor.followers_url
+    end
+  end
 end
