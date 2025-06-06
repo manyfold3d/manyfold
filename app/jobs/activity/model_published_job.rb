11bcf60c38ce09525a28a7f92c49f51fe34bdf5a
James Smith <james@floppy.org.uk>
2025-06-06 11:13:58 +0100

    send model published note from model if no creator is present

--- app/jobs/activity/model_published_job.rb
+++ app/jobs/activity/model_published_job.rb
@@ -4,11 +4,10 @@ class Activity::ModelPublishedJob < ApplicationJob
 
   def perform(model_id)
     model = Model.find(model_id)
-    return if model.creator.nil?
     Comment.create!(
       system: true,
       commentable: model,
-      commenter: model.creator,
+      commenter: model.creator || model,
       comment: I18n.t("jobs.activity.model_published.comment", # rubocop:disable I18n/RailsI18n/DecorateStringFormattingUsingInterpolation
         model_name: model.name,
         url: model.federails_actor.profile_url,
