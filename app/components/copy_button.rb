9f77af7a66a90bb4e8cdc540d43d8a83161e3a1d
James Smith <james@floppy.org.uk>
2025-06-07 12:04:53 +0100

    add translation string for copy tooltip

--- app/components/copy_button.rb
+++ app/components/copy_button.rb
@@ -5,7 +5,7 @@ class Components::CopyButton < Components::Base
 
   def view_template
     a href: "#", class: "link-secondary", data: {controller: "copy-text", action: "click->copy-text#copy:prevent", copy_text_text_value: @text} do
-      Icon icon: "clipboard-plus", label: t(".copy")
+      Icon icon: "clipboard-plus", label: t("components.copy_button.copy")
     end
   end
 end
