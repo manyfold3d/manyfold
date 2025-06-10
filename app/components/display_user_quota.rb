f5f26ec4994a9450bf7b3bbdc798f31f5ea7bfa4
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-06-10 21:12:42 +0900

    Fixing linting errors and adjusting styles to include bootstrap progress bar

--- app/components/display_user_quota.rb
+++ app/components/display_user_quota.rb
@@ -4,16 +4,19 @@ class Components::DisplayUserQuota < Components::Base
   include Phlex::Rails::Helpers::NumberToHumanSize
 
   def initialize(current_size:, quota:)
-    @quota = quota
-    @current_size = current_size
+    @quota = quota.to_f
+    @current_size = current_size.to_f
   end
 
   def view_template
-    quota_in_MB = number_to_human_size(@quota)
-    current_size_in_MB = number_to_human_size(@current_size)
+    quota_in_mb = number_to_human_size(@quota)
+    current_size_in_mb = number_to_human_size(@current_size)
+    percent_used = ((@current_size / @quota) * 100).ceil
     h1 do
-      plain "#{current_size_in_MB}/#{quota_in_MB}"
+      plain "#{current_size_in_mb}/#{quota_in_mb}"
+    end
+    div class: %(progress), role: %(progressbar), "aria-label": "Quota progress bar", "aria-valuemin": 0, "aria-valuemax": 100, "aria-value": percent_used do
+      div class: "progress-bar", style: "width:#{percent_used}%"
     end
   end
-
 end
