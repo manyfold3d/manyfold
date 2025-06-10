e7907f779c0c84e4dff0df4c21ca03241f108a4e
Matthew Badeau <matthewbadeau@users.noreply.github.com>
2025-06-10 21:29:29 +0900

    Style change to adjust background color based on percent used

--- app/components/display_user_quota.rb
+++ app/components/display_user_quota.rb
@@ -12,11 +12,22 @@ class Components::DisplayUserQuota < Components::Base
     quota_in_mb = number_to_human_size(@quota)
     current_size_in_mb = number_to_human_size(@current_size)
     percent_used = ((@current_size / @quota) * 100).ceil
+    progress_bar_background = "text-bg-success"
+    case percent_used
+    when 0..60
+      progress_bar_background = "text-bg-success"
+    when 70..90
+      progress_bar_background = "text-bg-warning"
+    when 90..Float::INFINITY
+      progress_bar_background = "text-bg-danger"
+    end
     h1 do
       plain "#{current_size_in_mb}/#{quota_in_mb}"
     end
-    div class: %(progress), role: %(progressbar), "aria-label": "Quota progress bar", "aria-valuemin": 0, "aria-valuemax": 100, "aria-value": percent_used do
-      div class: "progress-bar", style: "width:#{percent_used}%"
+    div class: "progress", role: %(progressbar), "aria-label": "Quota progress bar", "aria-valuemin": 0, "aria-valuemax": 100, "aria-value": percent_used do
+      div class: "progress-bar #{progress_bar_background}", style: "width:#{percent_used}%" do
+        "#{percent_used}%"
+      end
     end
   end
 end
