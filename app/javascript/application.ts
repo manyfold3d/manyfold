0278bfc3241ce79b4845cd31e9503e73aefe0257
James Smith <james@floppy.org.uk>
2025-06-10 10:03:38 +0100

    Add altcha javascript

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -4,6 +4,16 @@ import 'controllers/index'
 
 import Rails from '@rails/ujs'
 
+import 'altcha/external'
+import 'altcha/i18n/cs'
+import 'altcha/i18n/de'
+import 'altcha/i18n/en'
+import 'altcha/i18n/es-es'
+import 'altcha/i18n/fr-fr'
+import 'altcha/i18n/ja'
+import 'altcha/i18n/nl'
+import 'altcha/i18n/pl'
+
 document.addEventListener('DOMContentLoaded', () => {
   // Legacy Rails UJS
   Rails.start()
