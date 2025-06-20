67230bd44704c7d30d412de78797bd7411c1075d
James Smith <james@floppy.org.uk>
2025-06-20 18:09:49 +0100

    remove indexable scope and use method to filter

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -4,8 +4,6 @@ module Indexable
   included do
     enum :indexable, {inherit: nil, no: 0, yes: 1}, suffix: true
     enum :ai_indexable, {inherit: nil, no: 0, yes: 1}, suffix: true
-
-    scope :indexable, -> { SiteSettings.default_indexable ? all : none }
   end
 
   def indexable?
