80dc8d596a0a6b0906ac38fec3cf6bbaf9594867
James Smith <james@floppy.org.uk>
2025-07-09 10:30:41 +0100

    test uppy companion url meta endpoint

--- app/controllers/uppy_companion_controller.rb
+++ app/controllers/uppy_companion_controller.rb
@@ -1,16 +1,23 @@
 class UppyCompanionController < ApplicationController
-  skip_forgery_protection
+  protect_from_forgery with: :exception
 
   def url_meta
     authorize :model, :new?
-    # Do HEAD request for url
-    response = Faraday.head params[:url]
-    render json: {
-      name: response.headers["filename"],
-      type: response.headers["content-type"],
-      size: response.headers["content-length"],
-      status_code: response.status
-    }
+    uri = URI.parse(params[:url])
+    if ["https", "http"].include?(uri.scheme)
+      # Do HEAD request for url
+      response = Faraday.head uri
+      render json: {
+        name: uri.path.split("/").last,
+        type: response.headers["content-type"],
+        size: response.headers["content-length"].to_i,
+        status_code: response.status
+      }
+    else
+      head :bad_request
+    end
+  rescue URI::InvalidURIError
+    head :bad_request
   end
 
   def url_get
