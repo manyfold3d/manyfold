072be1eab752f98b631fc0404efd58d7d518b433
James Smith <james@floppy.org.uk>
2025-07-09 11:13:42 +0100

    refactor common code in uppy companion controller

--- app/controllers/uppy_companion_controller.rb
+++ app/controllers/uppy_companion_controller.rb
@@ -1,28 +1,31 @@
 class UppyCompanionController < ApplicationController
   protect_from_forgery with: :exception
 
+  before_action :check_auth
+  before_action :validate_uri
+
   def url_meta
-    authorize :model, :new?
-    uri = URI.parse(params[:url])
-    if ["https", "http"].include?(uri.scheme)
-      # Do HEAD request for url
-      response = Faraday.head uri
-      render json: {
-        name: uri.path.split("/").last,
-        type: response.headers["content-type"],
-        size: response.headers["content-length"].to_i,
-        status_code: response.status
-      }
-    else
-      head :bad_request
-    end
-  rescue URI::InvalidURIError
-    head :bad_request
+    response = Faraday.head @uri
+    render json: {
+      name: @uri.path.split("/").last,
+      type: response.headers["content-type"],
+      size: response.headers["content-length"].to_i,
+      status_code: response.status
+    }
   end
 
   def url_get
+
+  private
+
+  def check_auth
     authorize :model, :new?
-    Faraday.get params[:url]
-    render status: :internal_server_error, json: {}
+  end
+
+  def validate_uri
+    @uri = URI.parse(params.permit(uppy_companion: [:url]).dig(:uppy_companion, :url))
+    head :bad_request unless ["https", "http"].include?(@uri.scheme)
+  rescue URI::InvalidURIError
+    head :bad_request
   end
 end
