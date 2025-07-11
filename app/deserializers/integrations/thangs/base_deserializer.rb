d5fe924fac31b2be80266c17e88607830bd35a40
James Smith <james@floppy.org.uk>
2025-07-11 11:20:57 +0100

    Add thangs base metadata deserializer

---
+++ app/deserializers/integrations/thangs/base_deserializer.rb
@@ -0,0 +1,31 @@
+class Integrations::Thangs::BaseDeserializer < Integrations::BaseDeserializer
+  USERNAME_PATTERN = /[[:alnum:]_\-]+/
+
+  private
+
+  def api_configured?
+    true
+  end
+
+  def fetch(api_url)
+    connection = Faraday.new do |builder|
+      builder.response :json
+    end
+    connection.get("https://thangs.com/api/#{api_url}", {},
+      {
+        Accept: "application/json"
+      })
+  end
+
+  def canonicalize(uri)
+    u = URI.parse(uri)
+    return if u.host != "thangs.com"
+    return unless valid_path?(u.path)
+    # Force https
+    u.scheme = "https"
+    # Remove query and fragment
+    u.query = u.fragment = nil
+    u.to_s
+  rescue URI::InvalidURIError
+  end
+end
---
+++ spec/deserializers/integrations/thangs/base_deserializer_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe Integrations::Thangs::BaseDeserializer, :thingiverse_api_key do
+  context "when creating from URI" do
+    let(:canonical) { "https://thangs.com/designer/CHEP" }
+
+    it "validates URI" do
+      deserializer = described_class.new(uri: canonical)
+      expect(deserializer).to be_valid
+    end
+
+    it "canonicalizes http to https" do
+      deserializer = described_class.new(uri: "http://thangs.com/designer/CHEP")
+      expect(deserializer.uri).to eq canonical
+    end
+
+    it "removes query parts" do
+      deserializer = described_class.new(uri: "https://thangs.com/designer/CHEP?query=test")
+      expect(deserializer.uri).to eq canonical
+    end
+
+    it "removes fragment parts" do
+      deserializer = described_class.new(uri: "https://thangs.com/designer/CHEP#section")
+      expect(deserializer.uri).to eq canonical
+    end
+
+    it "rejects non-MMF domains" do
+      deserializer = described_class.new(uri: "https://thängs.com/designer/CHEP")
+      expect(deserializer).not_to be_valid
+    end
+  end
+end
