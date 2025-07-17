d91dd00183d26c33f998ebf66e7207febc3f1b50
James Smith <james@floppy.org.uk>
2025-07-17 15:19:40 +0100

    match creators to existing items

--- app/jobs/update_metadata_from_link_job.rb
+++ app/jobs/update_metadata_from_link_job.rb
@@ -7,20 +7,30 @@ class UpdateMetadataFromLinkJob < ApplicationJob
     linkable = link.linkable
     # Preserve existing tags
     data[:tag_list]&.concat(linkable.tag_list)&.uniq
+    # Match to existing creator
+    if data[:creator_attributes]
+      creator = Creator.find_by(name: data.dig(:creator_attributes, :name))
+      if creator
+        data.delete :creator_attributes
+        data[:creator_id] = creator.id
+      end
+    end
     # Update object
     linkable.update! data.except(:file_urls, :preview_filename)
     # Import files for models
     if linkable.is_a? Model
-      # Organize model if set
       linkable.organize! if organize
-      # Download files
-      data.dig(:file_urls)&.each do |it|
-        linkable.add_file_from_url(url: it[:url], filename: it[:filename])
-      rescue ActiveRecord::RecordInvalid
-        Rails.logger.info("Couldn't add file #{it[:url]} to model #{linkable.to_param}")
-      end
-      # Select preview file
-      data[:preview_file] = linkable.model_files.find_by(filename: data[:preview_filename]) if data[:preview_filename].present?
+      import_files(data)
     end
+  end
+
+  def import_files(data)
+    data.dig(:file_urls)&.each do |it|
+      linkable.add_file_from_url(url: it[:url], filename: it[:filename])
+    rescue ActiveRecord::RecordInvalid
+      Rails.logger.info("Couldn't add file #{it[:url]} to model #{linkable.to_param}")
+    end
+    # Select preview file
+    linkable.update!(preview_file: linkable.model_files.find_by(filename: data[:preview_filename])) if data[:preview_filename].present?
   end
 end
---
+++ spec/jobs/update_metadata_from_link_job_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe UpdateMetadataFromLinkJob do
+  subject(:job) { described_class.new }
+
+  before { create(:library) }
+
+  context "when importing a model" do
+    let(:model) { create(:model, links_attributes: [{url: "https://example.com"}]) }
+    let(:deserializer) {
+      deserializer = instance_double(Integrations::Thingiverse::ModelDeserializer)
+      allow(deserializer).to receive(:deserialize).and_return({creator_attributes: {
+        name: "creator"
+      }})
+      deserializer
+    }
+    let(:link) do
+      link = model.links.first
+      allow(link).to receive(:deserializer).and_return(deserializer)
+      link
+    end
+
+    context "with an existing creator" do
+      let!(:creator) { create(:creator, name: "creator") }
+
+      it "does not make a new creator" do
+        expect { described_class.perform_now(link: link) }.not_to change(Creator, :count)
+      end
+
+      it "sets creator" do
+        described_class.perform_now(link: link)
+        expect(model.reload.creator).to eq creator
+      end
+    end
+
+    it "creates a new creator if nothing matches" do
+      expect { described_class.perform_now(link: link) }.to change(Creator, :count).by(1)
+    end
+  end
+end
