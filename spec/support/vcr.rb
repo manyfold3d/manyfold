03e99a3d39103383f0376aa17d3e0158bf8b640a
James Smith <james@floppy.org.uk>
2025-07-21 13:11:29 +0100

    change vcr hook to webmock, so we can record stuff done by Down inside Shrine

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,7 +2,7 @@ require "vcr"
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
-  config.hook_into :faraday
+  config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
 end
