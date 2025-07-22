f985145ad9b89c29ffe6ac21f5a9e2e54101d841
James Smith <james@floppy.org.uk>
2025-07-22 10:47:48 +0100

    Fix error when downloading some Thingiverse models

--- app/deserializers/integrations/thingiverse/model_deserializer.rb
+++ app/deserializers/integrations/thingiverse/model_deserializer.rb
@@ -10,8 +10,8 @@ class Integrations::Thingiverse::ModelDeserializer < Integrations::Thingiverse::
       tag_list: r.body["tags"]&.pluck("tag"),
       sensitive: r.body["is_nsfw"],
       file_urls:
-        r.body.dig("zip_data", "images").map { |it| {url: it.dig("url"), filename: "images/" + filename_from_url(it.dig("url"))} } +
-          r.body.dig("zip_data", "files").map { |it| {url: it.dig("url"), filename: "files/" + filename_from_url(it.dig("url"))} },
+        r.body.dig("zip_data", "images").map { |it| {url: it.dig("url"), filename: "images/" + it.dig("name")} } +
+          r.body.dig("zip_data", "files").map { |it| {url: it.dig("url"), filename: "files/" + it.dig("name")} },
       preview_filename: "images/" + r.body.dig("default_image", "name")
     }.merge(creator_attributes(r.body["creator"]))
   end
