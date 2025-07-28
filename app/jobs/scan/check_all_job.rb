a323d5e117db490168e212f4b379b82d01c29b9f
James Smith <james@floppy.org.uk>
2025-07-28 13:40:40 +0100

    rewrite CheckAllJob to use JobIteration

--- app/jobs/scan/check_all_job.rb
+++ app/jobs/scan/check_all_job.rb
@@ -2,9 +2,16 @@ class Scan::CheckAllJob < ApplicationJob
   queue_as :scan
   unique :until_executed
 
-  def perform
-    # Check all models
-    status[:step] = "jobs.scan.check_all.queueing_model_checks" # i18n-tasks-use t('jobs.scan.check_all.queueing_model_checks')
-    Model.find_each(&:check_later)
+  include JobIteration::Iteration
+  queue_as :scan
+  unique :until_executed
+
+  def build_enumerator(cursor:)
+    scope = Model.all
+    enumerator_builder.active_record_on_records(scope, cursor: cursor)
+  end
+
+  def each_iteration(model)
+    model.check_later
   end
 end
