786687b8ed6897c8b2be796f4ff279c35c391e88
James Smith <james@floppy.org.uk>
2025-07-29 18:57:27 +0100

    raise http_error problems if links are broken

--- spec/jobs/update_metadata_from_link_job_spec.rb
+++ spec/jobs/update_metadata_from_link_job_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UpdateMetadataFromLinkJob do
 
   before { create(:library) }
 
-  context "when importing a model" do
+  context "when updating a model" do
     let(:model) { create(:model, links_attributes: [{url: "https://example.com"}]) }
     let(:deserializer) {
       deserializer = instance_double(Integrations::Thingiverse::ModelDeserializer)
@@ -23,6 +23,30 @@ RSpec.describe UpdateMetadataFromLinkJob do
 
     it "creates a new creator if nothing matches" do
       expect { described_class.perform_now(link: link) }.to change(Creator, :count).by(1)
+    end
+  end
+
+  context "when link fetch fails" do
+    let(:model) { create(:model, links_attributes: [{url: "https://example.com"}]) }
+    let(:deserializer) {
+      deserializer = instance_double(Integrations::Thingiverse::ModelDeserializer)
+      allow(deserializer).to receive(:deserialize).and_raise(Faraday::ResourceNotFound.new("not found"))
+      deserializer
+    }
+    let(:link) do
+      link = model.links.first
+      allow(link).to receive(:deserializer).and_return(deserializer)
+      link
+    end
+
+    it "logs a problem on the link" do
+      described_class.perform_now(link: link)
+      expect(Problem.last.problematic).to eq link
+    end
+
+    it "include failure message in problem note" do
+      described_class.perform_now(link: link)
+      expect(Problem.last.note).to eq "not found"
     end
   end
 end
