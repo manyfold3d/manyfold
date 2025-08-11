72de1c54bcdc5291e7543593ad990299b49ca1b5
James Smith <james@floppy.org.uk>
2025-08-11 09:47:02 +0100

    set layout and controller base for FASP

--- config/initializers/fasp_client.rb
+++ config/initializers/fasp_client.rb
@@ -2,4 +2,6 @@ FaspClient.configure do |conf|
   conf.authenticate = ->(request) do
     request.env["warden"]&.user&.is_administrator?
   end
+  conf.layout = "settings"
+  conf.controller_base = "::ApplicationController"
 end
