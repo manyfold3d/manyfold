c85d51d815cb5858bfac7de2855c6e94a93a5287
James Smith <james@floppy.org.uk>
2025-08-21 13:00:34 +0100

    add more fixture data into SQL files
    creators, collections (once they appear), models, libraries

--- spec/fixtures/migrations/20240731165647.sql
+++ spec/fixtures/migrations/20240731165647.sql
@@ -8,3 +8,42 @@ VALUES
 		1755768929,
 		1755768929
 	);
+
+INSERT INTO
+	creators (id, name, created_at, updated_at)
+VALUES
+	(1, 'Creator', 1755768929, 1755768929);
+
+INSERT INTO
+	collections (id, name, created_at, updated_at)
+VALUES
+	(1, 'Collection', 1755768929, 1755768929);
+
+INSERT INTO
+	libraries (id, path, created_at, updated_at)
+VALUES
+	(
+		1,
+		'/tmp/library',
+		1755768929,
+		1755768929
+	);
+
+INSERT INTO
+	models(
+		id,
+		library_id,
+		name,
+		path,
+		created_at,
+		updated_at
+	)
+VALUES
+	(
+		1,
+		1,
+		'Model',
+		'path/to/model',
+		1755768929,
+		1755768929
+	);
