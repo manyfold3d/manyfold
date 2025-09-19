3218f9453e7f4ca21c706a3065bda7ab4254806e
James Smith <james@floppy.org.uk>
2025-09-19 15:57:37 +0100

    disable transactional fixtures in favour of database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,10 +34,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [Rails.root.join("spec/fixtures")]
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # Let database cleaner handle transactions and cleanup
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
