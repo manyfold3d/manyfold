0a11a50c91a87a0850e3b06343d76c7bb347bb54
James Smith <james@floppy.org.uk>
2025-09-24 17:39:54 +0100

    Set avatar and banner for remote ActivityPub creators

--- app/deserializers/activity_pub/creator_deserializer.rb
+++ app/deserializers/activity_pub/creator_deserializer.rb
@@ -18,8 +18,10 @@ module ActivityPub
         links_attributes: parse_link_attributes(@object),
         caption: @object.extensions&.dig("summary"),
         notes: @object.extensions&.dig("content"),
-        indexable: (@object.extensions&.dig("indexable") == true) ? "yes" : "no"
-      }
+        indexable: (@object.extensions&.dig("indexable") == true) ? "yes" : "no",
+        avatar_remote_url: (@object.extensions&.dig("icon", "type") == "Image") ? @object.extensions&.dig("icon", "url") : nil,
+        banner_remote_url: (@object.extensions&.dig("image", "type") == "Image") ? @object.extensions&.dig("image", "url") : nil
+      }.compact
     end
   end
 end
