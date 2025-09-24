e16c3cc6590a528f1a71bffc517ddeb027dac487
James Smith <james@floppy.org.uk>
2025-09-24 15:09:28 +0100

    Move missing slug generation into upgrade job

---
+++ app/jobs/upgrade/generate_slugs_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Originally done in db/data/20230617222353_generate_slugs.rb
+class Upgrade::GenerateSlugsJob < ApplicationJob
+  include JobIteration::Iteration
+  unique :until_executed
+
+  def build_enumerator(model, cursor:)
+    enumerator_builder.active_record_on_records(model.unscoped.where(slug: nil), cursor: cursor)
+  end
+
+  def each_iteration(record, _model)
+    record.send(:slugify_name)
+    record.save!(validate: false)
+  end
+end
--- db/data/20230617222353_generate_slugs.rb
+++ db/data/20230617222353_generate_slugs.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
 class GenerateSlugs < ActiveRecord::Migration[7.0]
-  def up
-    Model.where(slug: nil).find_each do |model|
-      model.send(:slugify_name)
-      model.save!(validate: false)
-    end
-    Creator.where(slug: nil).find_each do |creator|
-      creator.send(:slugify_name)
-      creator.save!(validate: false)
-    end
-    Collection.where(slug: nil).find_each do |collection|
-      collection.send(:slugify_name)
-      collection.save!(validate: false)
-    end
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
+  # Replaced with Upgrade::GenerateSlugsJob
 end
