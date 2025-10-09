b561219a2a24e07cd0f8a7603c2c08c68bf7b8ee
James Smith <james@floppy.org.uk>
2025-10-09 16:49:02 +0100

    add tests for permission_preset

--- spec/models/concerns/caber_object_shared.rb
+++ spec/models/concerns/caber_object_shared.rb
@@ -19,6 +19,44 @@ shared_examples "Caber::Object" do
     expect(object.grants_permission_to?("own", admin)).to be false
   end
 
+  context "when assigning permissions" do
+    context "with public preset" do
+      let(:object) { create(described_class.to_s.underscore.to_sym, permission_preset: "public") }
+
+      it "grants view permission to public role" do
+        expect(object.grants_permission_to?("view", nil)).to be true
+      end
+
+      it "does not grant public update permission" do
+        expect(object.grants_permission_to?("update", nil)).to be false
+      end
+    end
+
+    context "with member preset" do
+      let(:object) { create(described_class.to_s.underscore.to_sym, permission_preset: "member") }
+
+      it "grants view permission to member role" do
+        expect(object.grants_permission_to?("view", Role.find_by!(name: "member"))).to be true
+      end
+
+      it "does not grant public view permission" do
+        expect(object.grants_permission_to?("view", nil)).to be false
+      end
+    end
+
+    context "with private preset" do
+      let(:object) { create(described_class.to_s.underscore.to_sym, permission_preset: "private") }
+
+      it "does not grant view permission to member role" do
+        expect(object.grants_permission_to?("view", Role.find_by!(name: "member"))).to be false
+      end
+
+      it "does not grant public view permission" do
+        expect(object.grants_permission_to?("view", nil)).to be false
+      end
+    end
+  end
+
   context "with default permissions set to public" do
     before do
       allow(SiteSettings).to receive(:default_viewer_role).and_return(:public)
