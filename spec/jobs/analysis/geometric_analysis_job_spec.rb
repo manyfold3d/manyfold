e5f16913deed69eca033e6bbe068aaeee4adbfdf
James Smith <james@floppy.org.uk>
2025-10-17 08:41:46 +0100

    test non-manifold meshes properly using plane geometry

--- spec/jobs/analysis/geometric_analysis_job_spec.rb
+++ spec/jobs/analysis/geometric_analysis_job_spec.rb
@@ -3,33 +3,37 @@ require "support/mock_directory"
 
 RSpec.describe Analysis::GeometricAnalysisJob do
   let(:file) { create(:model_file, filename: "test.stl") }
-  let(:mesh) do
+  let(:sphere) do
     m = Mittsu::Mesh.new(Mittsu::SphereGeometry.new(2.0, 32, 16))
+    m.geometry.merge_vertices
+    m
+  end
+  let(:plane) do
+    m = Mittsu::Mesh.new(Mittsu::PlaneGeometry.new(2.0, 2.0, 16, 16))
     m.geometry.merge_vertices
     m
   end
 
   before do
-    allow(described_class).to receive(:load_mesh).with(file).and_return(mesh)
+    allow(described_class).to receive(:load_mesh).with(file).and_return(sphere)
     allow(ModelFile).to receive(:find).and_call_original
     allow(ModelFile).to receive(:find).with(file.id).and_return(file)
     allow(SiteSettings).to receive(:analyse_manifold).and_return(true)
   end
 
   it "does not create Problems for a good mesh" do
-    allow(described_class).to receive(:load_mesh).with(file).and_return(mesh)
+    allow(described_class).to receive(:load_mesh).with(file).and_return(sphere)
     expect { described_class.perform_now(file.id) }.not_to change(Problem, :count)
   end
 
   it "creates a Problem for a non-manifold mesh" do # rubocop:todo RSpec/MultipleExpectations
-    allow(mesh).to receive(:manifold?).and_return(false)
-    allow(described_class).to receive(:load_mesh).with(file).and_return(mesh)
+    allow(described_class).to receive(:load_mesh).with(file).and_return(plane)
     expect { described_class.perform_now(file.id) }.to change(Problem, :count).from(0).to(1)
     expect(Problem.first.category).to eq "non_manifold"
   end
 
   it "removes a manifold problem if the mesh is OK" do
-    allow(described_class).to receive(:load_mesh).with(file).and_return(mesh)
+    allow(described_class).to receive(:load_mesh).with(file).and_return(sphere)
     create(:problem, problematic: file, category: :non_manifold)
     expect { described_class.perform_now(file.id) }.to change(Problem, :count).from(1).to(0)
   end
