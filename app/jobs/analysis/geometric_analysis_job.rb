9048ad0d40171c4cf125a8715e0ce3b091edf045
James Smith <james@floppy.org.uk>
2025-10-17 08:55:58 +0100

    move mmanifold check code into analysis job
    in preparation for a rewrite

--- app/jobs/analysis/geometric_analysis_job.rb
+++ app/jobs/analysis/geometric_analysis_job.rb
@@ -17,7 +17,7 @@ class Analysis::GeometricAnalysisJob < ApplicationJob
       if mesh
         status[:step] = "jobs.analysis.geometric_analysis.manifold_check" # i18n-tasks-use t('jobs.analysis.geometric_analysis.manifold_check')
         # Check for manifold mesh
-        manifold = mesh.manifold?
+        manifold = manifold?(mesh)
         Problem.create_or_clear(
           file,
           :non_manifold,
@@ -52,5 +52,34 @@ class Analysis::GeometricAnalysisJob < ApplicationJob
   def self.load_mesh(file)
     # TODO: This can be better, but needs changes upstream in Mittsu to allow loaders to parse from an IO object
     loader(file)&.new&.parse(file.attachment.read)
+  end
+
+  private
+
+  def manifold?(mesh)
+    # Shortcut if there is nothing here
+    return true if mesh.geometry.nil? && mesh.children.empty?
+    # Recurse children to see if they are manifold
+    children_are_manifold = mesh.children.map { |x| manifold?(x) }.all?
+    # Detect manifold geometry in this object
+    edges = {}
+    # For each face, record its edges in the edge hash
+    mesh.geometry&.faces&.each do |face|
+      update_edge_hash face.a, face.b, edges
+      update_edge_hash face.b, face.c, edges
+      update_edge_hash face.c, face.a, edges
+    end
+    # If there's anything left in the edge hash, then either
+    # we have holes, or we have badly oriented faces
+    edges.empty? && children_are_manifold
+  end
+
+  # Updates edge hash with the passed vertex indexes
+  # First, the reverse edge is searched for in the hash
+  # If found, it's removed as we've got a match
+  # If not, we record this edge in the hash
+  def update_edge_hash(v1, v2, edges)
+    return if edges.delete "#{v2}->#{v1}"
+    edges["#{v1}->#{v2}"] = true
   end
 end
