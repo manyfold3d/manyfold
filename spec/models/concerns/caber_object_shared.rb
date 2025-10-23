0e9a2d7144ebed3f9827bcc418c09fe0d0a4511f
James Smith <james@floppy.org.uk>
2025-10-23 17:23:07 +0100

    check that owner relation isn't overwritten by presets

--- spec/models/concerns/caber_object_shared.rb
+++ spec/models/concerns/caber_object_shared.rb
@@ -110,7 +110,11 @@ shared_examples "Caber::Object" do
   end
 
   context "when assigning permissions presets during update" do
-    let(:object) { create(described_class.to_s.underscore.to_sym) }
+    let(:object) {
+      options = {owner: contributor}
+      options[:creator] = create(:creator) if described_class.column_names.include?("creator_id")
+      create(described_class.to_s.underscore.to_sym, options)
+    }
 
     context "with public preset" do
       before do
@@ -151,6 +155,24 @@ shared_examples "Caber::Object" do
 
       it "does not grant public view permission" do
         expect(object.grants_permission_to?("view", nil)).to be false
+      end
+    end
+
+    context "with both preset and explicit permissions" do
+      before do
+        object.update!(permission_preset: "member", caber_relations_attributes: [{permission: "view", subject: nil}])
+      end
+
+      it "grants view permission to member role" do
+        expect(object.grants_permission_to?("view", Role.find_by!(name: "member"))).to be true
+      end
+
+      it "does not grant public view permission" do
+        expect(object.grants_permission_to?("view", nil)).to be false
+      end
+
+      it "does not wipe owner relation" do
+        expect(object.grants_permission_to?("own", contributor)).to be true
       end
     end
   end
