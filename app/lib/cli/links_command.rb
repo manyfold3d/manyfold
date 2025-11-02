98a67b8bda415b95ae43c427567317396a02ef3a
James Smith <james@floppy.org.uk>
2025-11-02 14:49:37 +0000

    Add CLI command to remove links matching particular domain or substring
    e.g. `bin/manyfold links purge --match="localhost"`

--- app/lib/cli/links_command.rb
+++ app/lib/cli/links_command.rb
@@ -18,5 +18,13 @@ module Cli
       puts "\nQueueing #{scope.count} links for synchronisation"
       scope.find_each(&:update_metadata_from_link_later)
     end
+
+    desc "purge", "remove links"
+    option :match, required: true, type: :string, description: "purge links with URLs that match"
+    def purge
+      scope = Link.where("url LIKE '%#{options[:match]}%'") # rubocop:disable Pundit/UsePolicyScope
+      puts "\Removing #{scope.count} links matching #{options[:match]}"
+      scope.destroy_all
+    end
   end
 end
