d72ed6b2f2a98519e6d055793ab7bbbfa0f1f436
James Smith <james@floppy.org.uk>
2025-11-03 23:37:54 +0000

    update file uploader form handling

--- app/deserializers/form/uploaded_file_deserializer.rb
+++ app/deserializers/form/uploaded_file_deserializer.rb
@@ -2,7 +2,7 @@ module Form
   class UploadedFileDeserializer < BaseDeserializer
     def deserialize
       return nil unless @params
-      @params.permit(
+      @params.require(:model).permit(
         file: [
           [:id, :name]
         ]
