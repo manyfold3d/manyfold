221698360b8a167ed4d246e4c532dacce812cfe9
James Smith <james@floppy.org.uk>
2025-11-04 21:46:29 +0000

    make dropdown item icon optional and add nofollow arg

--- app/components/dropdown_item.rb
+++ app/components/dropdown_item.rb
@@ -3,20 +3,23 @@
 class Components::DropdownItem < Components::Base
   include Phlex::Rails::Helpers::LinkTo
 
-  def initialize(icon:, label:, path:, method: nil, aria_label: nil, confirm: nil)
+  def initialize(label:, path:, icon: nil, method: nil, aria_label: nil, confirm: nil, nofollow: nil)
     @icon = icon
     @label = label
     @path = path
     @method = method
     @aria_label = aria_label
     @confirm = confirm
+    @nofollow = nofollow
   end
 
   def view_template
     li do
-      link_to @path, method: @method, class: "dropdown-item", aria: {label: @aria_label}, data: {confirm: @confirm} do
-        Icon(icon: @icon, label: @label)
-        whitespace
+      link_to @path, method: @method, class: "dropdown-item", aria: {label: @aria_label}, data: {confirm: @confirm}, nofollow: @nofollow do
+        if @icon
+          Icon(icon: @icon, label: @label)
+          whitespace
+        end
         span { @label }
       end
     end
