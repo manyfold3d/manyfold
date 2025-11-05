eb8e23c8fdb4b34f9900fd71b6e1825544284012
James Smith <james@floppy.org.uk>
2025-11-05 11:39:46 +0000

    use and store user setting for sort if logged in

--- app/controllers/concerns/sortable.rb
+++ app/controllers/concerns/sortable.rb
@@ -5,19 +5,33 @@ module Sortable
     before_action :remember_ordering
   end
 
+  def current_ordering
+    if current_user
+      current_user.sort_order.to_s
+    else
+      session["order"].to_s
+    end
+  end
+
   def remember_ordering
-    session["order"] ||= "name"
-    session["order"] = params["order"] if params["order"]
+    if current_user
+      current_user.update!(sort_order: params["order"]) if params["order"].presence
+    else
+      session["order"] ||= "name"
+      session["order"] = params["order"] if params["order"].presence
+    end
   end
 
   def apply_sort_order(scope)
-    case session["order"]
+    case current_ordering
     when "recent"
       scope.order(created_at: :desc)
     when "updated"
       scope.order(updated_at: :desc)
-    else
+    when "name"
       scope.order(name_lower: :asc)
+    else
+      scope
     end
   end
 end
