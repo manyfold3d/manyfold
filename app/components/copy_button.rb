ab12ae79d4633f07eb2e7fe24fd230b8b11e11d1
James Smith <james@floppy.org.uk>
2025-11-17 23:29:24 +0000

    Improve labels on clipboard copy buttons

--- app/components/copy_button.rb
+++ app/components/copy_button.rb
@@ -1,11 +1,16 @@
 class Components::CopyButton < Components::Base
-  def initialize(text:)
+  def initialize(text:, label: nil)
     @text = text
+    @label = label
+  end
+
+  def before_template
+    @label ||= t("components.copy_button.copy")
   end
 
   def view_template
     a href: "#", class: "link-secondary", data: {controller: "copy-text", action: "click->copy-text#copy:prevent", copy_text_text_value: @text} do
-      Icon icon: "clipboard-plus", label: t("components.copy_button.copy")
+      Icon icon: "clipboard-plus", label: @label
     end
   end
 end
