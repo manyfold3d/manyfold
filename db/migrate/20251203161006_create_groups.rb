52e922c2b17fbb7d8bed6fdf89dfc12e81c300c7
James Smith <james@floppy.org.uk>
2025-12-03 17:01:38 +0000

    groups are owned by creators, not users

--- db/migrate/20251203161006_create_groups.rb
+++ db/migrate/20251203161006_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[8.0]
   def change
     create_table :groups do |t|
-      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :creator, null: false, foreign_key: true
 
       t.timestamps
     end
--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :group do
-    user
+    creator
   end
 end
