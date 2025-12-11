1a0999f4635ed9e4e75f5719a5bfac454787bb63
James Smith <james@floppy.org.uk>
2025-12-11 17:26:41 +0000

    create new PageTitle component including heading and breadcrumbs

---
+++ app/components/page_title.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Components::PageTitle < Components::Base
+  def initialize(title:, breadcrumbs: {})
+    @title = title
+    @breadcrumbs = breadcrumbs
+  end
+
+  def view_template
+    nav aria: {label: "breadcrumb"} do
+      ol class: "breadcrumb" do
+        li class: "breadcrumb-item" do
+          a(href: root_url) { Icon icon: "house", label: t("application.navbar.home") }
+        end
+        @breadcrumbs.map do |text, path|
+          li class: "breadcrumb-item" do
+            a(href: path) { text }
+          end
+        end
+        li(class: "breadcrumb-item active", aria: {current: "page"}) { @title }
+      end
+    end
+    whitespace
+    h1 do
+      span { @title }
+    end
+  end
+end
