2a3a70a3313f0d0f27e6e0acd66525839b4ba5c3
James Smith <james@floppy.org.uk>
2025-12-11 22:16:25 +0000

    add description field to UI

--- app/views/groups/index.rb
+++ app/views/groups/index.rb
@@ -16,6 +16,7 @@ class Views::Groups::Index < Views::Base
       tr do
         th { Group.human_attribute_name :name }
         th { Group.human_attribute_name :memberships }
+        th { Group.human_attribute_name :description }
         th { Group.human_attribute_name :typed_id }
         th
       end
@@ -23,6 +24,7 @@ class Views::Groups::Index < Views::Base
         tr do
           td { group.name }
           td { t("views.groups.index.member_count", count: group.members.count) }
+          td { group.description }
           td { CopyableText text: group.typed_id, label: t("views.groups.index.copy") }
           td { GoButton label: t("views.groups.edit.title"), href: edit_creator_group_path(@creator, group), icon: "pencil", variant: :primary }
         end
