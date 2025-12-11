65dd597af712512a9d09350c36116cee2d2decd8
James Smith <james@floppy.org.uk>
2025-12-11 10:23:07 +0000

    show typed IDs in list

--- app/views/groups/index.rb
+++ app/views/groups/index.rb
@@ -17,12 +17,14 @@ class Views::Groups::Index < Views::Base
       tr do
         th { t("views.groups.index.name") }
         th { t("views.groups.index.membership") }
+        th { t("views.groups.index.typed_id") }
         th
       end
       @groups.each do |group|
         tr do
           td { group.name }
           td { t("views.groups.index.member_count", count: group.members.count) }
+          td { CopyableText text: group.typed_id, label: t("views.groups.index.copy") }
           td { GoButton label: t("views.groups.edit.title"), href: edit_creator_group_path(@creator, group), icon: "pencil", variant: :primary }
         end
       end
