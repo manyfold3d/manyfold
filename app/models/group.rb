6021bd22078d1bc8ff45588b358305c4bcbca341
James Smith <james@floppy.org.uk>
2025-12-11 09:38:16 +0000

    refactor typed id generation and matching for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,4 +8,15 @@ class Group < ApplicationRecord
   has_many :members, through: "memberships", source: :user
 
   accepts_nested_attributes_for :memberships, reject_if: :all_blank, allow_destroy: true
+
+  def typed_id
+    "group::#{id}"
+  end
+
+  TYPED_ID_PATTERN = /group::([[:digit:]]+)/
+
+  def self.find_by_typed_id(typed_id, scope: Group)
+    typed_id.match(TYPED_ID_PATTERN)
+    scope.find($1) if $1
+  end
 end
