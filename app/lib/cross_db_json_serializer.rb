9411574a2c65a9a6d7a2016b688b6e300972c4f6
James Smith <james@floppy.org.uk>
2025-12-17 10:55:54 +0000

    Make JSON serializer on MySQL safer

--- app/lib/cross_db_json_serializer.rb
+++ app/lib/cross_db_json_serializer.rb
@@ -1,9 +1,21 @@
 module CrossDbJsonSerializer
   def self.load(payload)
-    payload.is_a?(String) ? JSON.load(payload) : payload # rubocop:disable Security/JSONLoad
+    return if payload.nil?
+    case ApplicationRecord.connection.adapter_name
+    when "Mysql2" # Actually this is the behaviour for MariaDB
+      payload.is_a?(String) ? JSON.parse(payload) : payload
+    else
+      payload
+    end
   end
 
   def self.dump(payload)
-    payload.is_a?(String) ? payload : JSON.dump(payload)
+    return if payload.nil?
+    case ApplicationRecord.connection.adapter_name
+    when "Mysql2" # Actually this is the behaviour for MariaDB
+      payload.is_a?(String) ? payload : JSON.generate(payload)
+    else
+      payload
+    end
   end
 end
