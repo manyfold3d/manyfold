c07eb2a516ce550b09b8e260d498aed84bbff8fd
James Smith <james@floppy.org.uk>
2026-01-06 13:21:42 +0000

    Move cocooned code back into an ERB form
    otherwise it doesn't work

--- app/views/groups/form.rb
+++ app/views/groups/form.rb
@@ -8,25 +8,12 @@ class Views::Groups::Form < Views::Base
     @group = group
   end
 
-  def view_template
-    form_with model: [@creator, @group], class: "container-md tabular-form" do |form|
-      Components::TextInputRow(form: form, attribute: :name, label: Group.human_attribute_name(:name))
-      Components::RichTextInputRow(form: form, attribute: :description, label: Group.human_attribute_name(:description))
-      div do
-        form.submit class: "btn btn-primary"
+  def before_template
+    @group.memberships.build if @group.memberships.empty?
+  end
 
-        if @group.persisted? && policy(@group).destroy?
-          link_to creator_group_path(@creator, @group), {
-            method: :delete,
-            class: "float-end btn btn-outline-danger",
-            data: {confirm: translate("views.groups.form.confirm_destroy")}
-          } do
-            Icon(icon: "trash", label: t("general.delete"))
-            whitespace
-            span { t("general.delete") }
-          end
-        end
-      end
-    end
+  def view_template
+    # Using an ERB form otherwise coccooned doesn't work
+    render partial("form", creator: @creator, group: @group)
   end
 end
