b4a8b32416716a95f7b10d3ae9cad7b115228b9c
James Smith <james@floppy.org.uk>
2026-01-14 13:02:34 +0000

    fix tomselect import

--- app/javascript/controllers/tag_input_controller.ts
+++ app/javascript/controllers/tag_input_controller.ts
@@ -1,14 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 import TomSelect from 'tom-select'
-import type { TomInput } from 'tom-select/dist/cjs/types'
 
 // Connects to data-controller="tag-input"
 export default class extends Controller {
   tomSelect: TomSelect | null
 
   connect (): void {
-    this.tomSelect = new TomSelect((this.element as TomInput), { // eslint-disable-line no-new
+    this.tomSelect = new TomSelect((this.element as HTMLSelectElement), { // eslint-disable-line no-new
       addPrecedence: true,
       create: true,
       plugins: ['remove_button'],
