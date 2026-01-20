e2acbaa1ca2285e9c5e651dc8be232f79df500f0
James Smith <james@floppy.org.uk>
2026-01-20 12:52:38 +0000

    Tell i18n linter that subject string is used

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,10 +1,6 @@
 class GroupMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.group_mailer.new_member.subject
-  #
   def new_member
+    # i18n-tasks-use t('group_mailer.new_member.subject')
     @user = params[:notification].recipient
     @group = params[:membership].group
     mail to: @user.email
