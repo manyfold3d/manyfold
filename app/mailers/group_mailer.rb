71b7907e19541172ad732e95c1972a1673a0280b
James Smith <james@floppy.org.uk>
2026-01-22 16:12:27 +0000

    generate invitation token when sending invite in mailer

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -7,8 +7,9 @@ class GroupMailer < ApplicationMailer
     params = {to: @user.email}
     if @inviting
       params[:subject] = t("group_mailer.new_member.inviting.subject", site_name: SiteSettings.site_name)
-      @user.invitation_sent_at = DateTime.now
-      @user.save!(validate: false)
+      # Generate a new invitation token so we have everything we need
+      @user.invitation_sent_at = DateTime.now.utc
+      @user.send :generate_invitation_token!
     else
       params[:subject] = t("group_mailer.new_member.existing.subject", site_name: SiteSettings.site_name)
     end
