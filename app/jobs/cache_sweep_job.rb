ce009266a3e30625ac00af5f6c579dd68c1fa8a7
James Smith <james@floppy.org.uk>
2026-01-27 12:22:55 +0000

    Automatically clean up old OAuth tokens and grants

--- app/jobs/cache_sweep_job.rb
+++ app/jobs/cache_sweep_job.rb
@@ -6,5 +6,29 @@ class CacheSweepJob < ApplicationJob
 
     # Clear up old ALTCHA records
     AltchaSolution.cleanup
+
+    # Clean up expired doorkeeper tokens
+    doorkeeper_cleanup
+  end
+
+  def doorkeeper_cleanup
+    # Code taken from doorkeeper cleanup rake task
+
+    # Removes stale access tokens
+    cleaner = Doorkeeper::StaleRecordsCleaner.new(Doorkeeper.config.access_token_model)
+    cleaner.clean_revoked
+
+    # Removes expired (TTL passed) access tokens
+    expirable_tokens = Doorkeeper.config.access_token_model.where(refresh_token: nil)
+    cleaner = Doorkeeper::StaleRecordsCleaner.new(expirable_tokens)
+    cleaner.clean_expired(Doorkeeper.config.access_token_expires_in)
+
+    # Removes stale access grants
+    cleaner = Doorkeeper::StaleRecordsCleaner.new(Doorkeeper.config.access_grant_model)
+    cleaner.clean_revoked
+
+    # Removes expired (TTL passed) access grants
+    cleaner = Doorkeeper::StaleRecordsCleaner.new(Doorkeeper.config.access_grant_model)
+    cleaner.clean_expired(Doorkeeper.config.authorization_code_expires_in)
   end
 end
