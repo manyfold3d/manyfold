3104526a96183199dfab55b3094e77278d3a2627
James Smith <james@floppy.org.uk>
2026-01-28 10:56:34 +0000

    improve expiry display

---
+++ app/helpers/doorkeeper_applications_helper.rb
@@ -0,0 +1,11 @@
+module DoorkeeperApplicationsHelper
+
+  def token_expiry_string(token)
+    return t(".revoked") if token.revoked?
+    return t(".never") if token.expires_in.nil?
+    expires_at = token.created_at + token.expires_in
+    return t(".expired") if expires_at < Time.now.utc
+    distance_of_time_in_words(Time.now.utc, expires_at)
+  end
+
+end
