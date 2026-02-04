1c7cf14f3e24d8cbdcb155f550aea574f9e02fbb
James Smith <james@floppy.org.uk>
2026-02-04 16:01:25 +0000

    Adjust location of loading bar and add icon

--- app/components/renderer.rb
+++ app/components/renderer.rb
@@ -36,10 +36,12 @@ class Components::Renderer < Components::Base
           render_style: @settings["render_style"],
           auto_load: ((@file.size || 9_999_999.megabytes) < (@settings["auto_load_max_size"] || 9_999_999).megabytes) ? "true" : "false"
         }
-      div class: "p-0 btn btn-secondary load-progress object-preview-progress position-absolute top-50 start-50 translate-middle", role: "presentation" do
+      div class: "p-0 btn btn-sm btn-secondary load-progress object-preview-progress position-absolute mt-3 start-50 translate-middle-x translate-top", role: "presentation" do
         div class: "progress-bar bg-info progress-bar-animated progress-bar-striped", role: "progressbar", style: "width: 0%; height: 100%",
           aria_label: "Loading progress", aria_valuenow: "0", aria_valuemin: "0", aria_valuemax: "100"
         span class: "progress-label position-absolute top-50 start-50 translate-middle", role: "button" do
+          Icon icon: "box"
+          whitespace
           span { t("renderer.load") }
           whitespace
           span { "(#{number_to_human_size @file.size, precision: 2})" }
