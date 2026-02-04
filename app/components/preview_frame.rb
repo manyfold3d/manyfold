0ed7584018320a10be814f29972c132ad6150979
James Smith <james@floppy.org.uk>
2026-02-04 16:02:06 +0000

    display render derivatives if present

--- app/components/preview_frame.rb
+++ app/components/preview_frame.rb
@@ -38,6 +38,8 @@ class Components::PreviewFrame < Components::Base
       div class: "card-img-top #{"sensitive" if needs_hiding?}" do
         Renderer file: @file
       end
+    elsif @file.is_3d_model? && @file.attachment(:render)
+      image model_model_file_path(@file.model, @file, format: @file.extension, derivative: "render"), @file.name
     else
       empty
     end
