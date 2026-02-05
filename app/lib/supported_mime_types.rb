732af78589a010436352acd55939c5026556ea56
James Smith <james@floppy.org.uk>
2026-02-05 14:19:08 +0000

    Make MIME type checks more efficient
    with memoization

--- app/lib/supported_mime_types.rb
+++ app/lib/supported_mime_types.rb
@@ -1,73 +1,91 @@
 module SupportedMimeTypes
-  def self.image_types
-    Mime::LOOKUP.filter { |k, v| is_image_mime_type?(v) }.values
-  end
-
-  def self.image_extensions
-    Mime::EXTENSION_LOOKUP.filter { |k, v| is_image_mime_type?(v) }.keys
-  end
-
-  def self.video_types
-    Mime::LOOKUP.filter { |k, v| is_video_mime_type?(v) }.values
-  end
-
-  def self.video_extensions
-    Mime::EXTENSION_LOOKUP.filter { |k, v| is_video_mime_type?(v) }.keys
-  end
-
-  def self.document_types
-    Mime::LOOKUP.filter { |k, v| is_document_mime_type?(v) }.values
-  end
-
-  def self.document_extensions
-    Mime::EXTENSION_LOOKUP.filter { |k, v| is_document_mime_type?(v) }.keys
-  end
-
-  def self.archive_types
-    Mime::LOOKUP.filter { |k, v| is_archive_mime_type?(v) }.values
-  end
-
-  def self.archive_extensions
-    Mime::EXTENSION_LOOKUP.filter { |k, v| is_archive_mime_type?(v) }.keys
-  end
-
-  def self.model_types
-    Mime::LOOKUP.filter { |k, v| is_model_mime_type?(v) }.values
-  end
-
-  def self.model_extensions
-    Mime::EXTENSION_LOOKUP.filter { |k, v| is_model_mime_type?(v) }.keys
-  end
-
-  def self.loadable
-    Mime::EXTENSION_LOOKUP.slice(
-      *Assimp.extension_list.to_s.delete("*.").split(";")
-    ).values.map(&:to_sym)
-  end
-
-  def self.can_load?(type)
-    loadable.include? type
-  end
-
-  def self.exportable
-    Mime::EXTENSION_LOOKUP.slice(
-      *(0...Assimp.aiGetExportFormatCount).map { |it| Assimp.aiGetExportFormatDescription it }.map(&:file_extension)
-    ).values.map(&:to_sym)
-  end
-
-  def self.can_export?(type)
-    exportable.include? type
-  end
-
-  def self.indexable_types
-    image_types + model_types + video_types + document_types + archive_types
-  end
-
-  def self.indexable_extensions
-    image_extensions + model_extensions + video_extensions + document_extensions + archive_extensions
-  end
-
   class << self
+    extend Memoist
+
+    def image_types
+      Mime::LOOKUP.filter { |k, v| is_image_mime_type?(v) }.values
+    end
+    memoize :image_types
+
+    def image_extensions
+      Mime::EXTENSION_LOOKUP.filter { |k, v| is_image_mime_type?(v) }.keys
+    end
+    memoize :image_extensions
+
+    def video_types
+      Mime::LOOKUP.filter { |k, v| is_video_mime_type?(v) }.values
+    end
+    memoize :video_types
+
+    def video_extensions
+      Mime::EXTENSION_LOOKUP.filter { |k, v| is_video_mime_type?(v) }.keys
+    end
+    memoize :video_extensions
+
+    def document_types
+      Mime::LOOKUP.filter { |k, v| is_document_mime_type?(v) }.values
+    end
+    memoize :document_types
+
+    def document_extensions
+      Mime::EXTENSION_LOOKUP.filter { |k, v| is_document_mime_type?(v) }.keys
+    end
+    memoize :document_extensions
+
+    def archive_types
+      Mime::LOOKUP.filter { |k, v| is_archive_mime_type?(v) }.values
+    end
+    memoize :archive_types
+
+    def archive_extensions
+      Mime::EXTENSION_LOOKUP.filter { |k, v| is_archive_mime_type?(v) }.keys
+    end
+    memoize :archive_extensions
+
+    def model_types
+      Mime::LOOKUP.filter { |k, v| is_model_mime_type?(v) }.values
+    end
+    memoize :model_types
+
+    def model_extensions
+      Mime::EXTENSION_LOOKUP.filter { |k, v| is_model_mime_type?(v) }.keys
+    end
+    memoize :model_extensions
+
+    def loadable
+      Mime::EXTENSION_LOOKUP.slice(
+        *Assimp.extension_list.to_s.delete("*.").split(";")
+      ).values.map(&:to_sym)
+    end
+    memoize :loadable
+
+    def can_load?(type)
+      loadable.include? type
+    end
+    memoize :can_load?
+
+    def exportable
+      Mime::EXTENSION_LOOKUP.slice(
+        *(0...Assimp.aiGetExportFormatCount).map { |it| Assimp.aiGetExportFormatDescription it }.map(&:file_extension)
+      ).values.map(&:to_sym)
+    end
+    memoize :exportable
+
+    def can_export?(type)
+      exportable.include? type
+    end
+    memoize :can_export?
+
+    def indexable_types
+      image_types + model_types + video_types + document_types + archive_types
+    end
+    memoize :indexable_types
+
+    def indexable_extensions
+      image_extensions + model_extensions + video_extensions + document_extensions + archive_extensions
+    end
+    memoize :indexable_extensions
+
     private
 
     def is_image_mime_type?(type)
@@ -77,10 +95,12 @@ module SupportedMimeTypes
       ]
       type.to_s.start_with?("image/") && exclusions.exclude?(type.to_s)
     end
+    memoize :is_image_mime_type?
 
     def is_video_mime_type?(type)
       type.to_s.start_with?("video/")
     end
+    memoize :is_video_mime_type?
 
     def is_document_mime_type?(type)
       [
@@ -102,6 +122,7 @@ module SupportedMimeTypes
         "application/x-kicad-worksheet"
       ].include?(type.to_s)
     end
+    memoize :is_document_mime_type?
 
     def is_model_mime_type?(type)
       extras = [
@@ -116,6 +137,7 @@ module SupportedMimeTypes
       ]
       type.to_s.start_with?("model/") || extras.include?(type.to_s)
     end
+    memoize :is_model_mime_type?
 
     def is_archive_mime_type?(type)
       [
@@ -126,5 +148,6 @@ module SupportedMimeTypes
         "application/x-bzip2"
       ].include?(type.to_s)
     end
+    memoize :is_archive_mime_type?
   end
 end
