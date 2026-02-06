a1276e87045ef596e2ae0512145c188aa840fea6
James Smith <james@floppy.org.uk>
2026-02-06 13:07:21 +0000

    add helper for mock uploaded files

--- spec/factories/model_file.rb
+++ spec/factories/model_file.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :model_file do
     model
     filename { Faker::File.file_name(ext: "stl") }
-    attachment { filename ? Rack::Test::UploadedFile.new(StringIO.new("solid #{filename}\n", original_filename: filename) : nil }
+    attachment { filename ? mock_upload(content: "solid #{filename}\n", filename: filename) : nil }
 
     after :create do |file, context|
       type = Mime::EXTENSION_LOOKUP[File.extname(file.filename).tr(".", "").downcase].to_s
