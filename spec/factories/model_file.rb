5665eaf42aba9fbf9de910fec8339120276fcde5
James Smith <james@floppy.org.uk>
2026-02-06 11:56:19 +0000

    include actual file content by default for digest calculation in tests

--- spec/factories/model_file.rb
+++ spec/factories/model_file.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :model_file do
     model
     filename { Faker::File.file_name(ext: "stl") }
-    attachment { filename ? Rack::Test::UploadedFile.new(StringIO.new, original_filename: filename) : nil }
+    attachment { filename ? Rack::Test::UploadedFile.new(StringIO.new("solid #{filename}\n"), original_filename: filename) : nil }
   end
 end
