1328a5209e3a0d74c7988bbbfe52c7edab603a63
James Smith <james@floppy.org.uk>
2026-02-09 15:20:25 +0000

    Fix email test command documentation
    Resolves #5456

--- app/lib/cli/email_command.rb
+++ app/lib/cli/email_command.rb
@@ -5,7 +5,7 @@ module Cli
     COMMAND = "email"
     DESCRIPTION = "test email"
 
-    desc "test_email", "sends test email"
+    desc "test", "sends test email"
     def test
       UserMailer.with(user: User.find(1)).account_approved.deliver_now # rubocop:disable Pundit/UsePolicyScope
     end
