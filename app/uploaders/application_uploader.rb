6d79cfd4cb4a2630a7eb1417171b471382ae2cfe
James Smith <james@floppy.org.uk>
2026-02-13 13:10:13 +0000

    fix derivative enabled check

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -91,14 +91,17 @@ class ApplicationUploader < Shrine
   end
 
   Attacher.derivatives do |original|
-    break unless SiteSettings.generate_image_derivatives
-    if context[:record]&.is_image?
-      Shrine.with_file(original) do |it|
-        magick = ImageProcessing::MiniMagick.source(it)
-        {
-          preview: magick.resize_to_limit!(320, 320),
-          carousel: magick.resize_to_limit!(1024, 768)
-        }
+    if SiteSettings.generate_image_derivatives
+      if context[:record]&.is_image?
+        Shrine.with_file(original) do |it|
+          magick = ImageProcessing::MiniMagick.source(it)
+          {
+            preview: magick.resize_to_limit!(320, 320),
+            carousel: magick.resize_to_limit!(1024, 768)
+          }
+        end
+      else
+        {}
       end
     else
       {}
