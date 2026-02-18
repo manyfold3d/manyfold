0cfceceaa4a510dec041124f3f76d66ae35d2f4a
James Smith <james@floppy.org.uk>
2026-02-18 18:37:01 +0000

    Don't include SVG in image backfill

--- app/jobs/upgrade/backfill_image_derivatives.rb
+++ app/jobs/upgrade/backfill_image_derivatives.rb
@@ -2,7 +2,7 @@ class Upgrade::BackfillImageDerivatives < Upgrade::BackfillDerivativesBase
   queue_as :low
 
   def mime_types
-    SupportedMimeTypes.image_types
+    SupportedMimeTypes.image_types.without(Mime[:svg])
   end
 
   def derivative
