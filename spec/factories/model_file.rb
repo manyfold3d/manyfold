8a4b5a2f11f87bb09db54f255a412524693cfa34
James Smith <james@floppy.org.uk>
2026-02-18 16:10:58 +0000

    add fake MIME type to modelfiles created by factorybot

--- spec/factories/model_file.rb
+++ spec/factories/model_file.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     model
     filename { Faker::File.file_name(ext: "stl") }
     attachment { filename ? Rack::Test::UploadedFile.new(StringIO.new, original_filename: filename) : nil }
+
+    after :create do |file, context|
+      type = Mime::EXTENSION_LOOKUP[File.extname(file.filename).tr(".", "")].to_s
+      file.attachment_attacher.add_metadata("mime_type" => type)
+      file.save!
+    end
   end
 end
