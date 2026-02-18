924c759198c63db8140b8a69ba74727b5dbdb353
James Smith <james@floppy.org.uk>
2026-02-18 16:34:42 +0000

    fix case in factory

--- spec/factories/model_file.rb
+++ spec/factories/model_file.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     attachment { filename ? Rack::Test::UploadedFile.new(StringIO.new, original_filename: filename) : nil }
 
     after :create do |file, context|
-      type = Mime::EXTENSION_LOOKUP[File.extname(file.filename).tr(".", "")].to_s
+      type = Mime::EXTENSION_LOOKUP[File.extname(file.filename).tr(".", "").downcase].to_s
       file.attachment_attacher.add_metadata("mime_type" => type)
       file.save!
     end
