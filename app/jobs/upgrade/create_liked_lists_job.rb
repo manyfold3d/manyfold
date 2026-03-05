ae6646c8524971bc7fe460f2b8c7466178e2603d
James Smith <james@floppy.org.uk>
2026-03-05 17:22:52 +0000

    Create a special "liked" list for every user

---
+++ app/jobs/upgrade/create_liked_lists_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Upgrade::CreateLikedListsJob < Upgrade::IterationJob
+  def build_enumerator(cursor:)
+    enumerator_builder.active_record_on_records(User.all, cursor: cursor)
+  end
+
+  def each_iteration(user)
+    List.create(name: "lists.special.liked", special: :liked, owner: user) if user.lists.find_by(special: :liked).nil?
+  end
+end
