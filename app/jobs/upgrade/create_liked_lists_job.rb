7eba404a4847b991200ea91a87fd4358ae4e3896
James Smith <james@floppy.org.uk>
2026-03-06 09:52:06 +0000

    Add translation string for liked list

--- app/jobs/upgrade/create_liked_lists_job.rb
+++ app/jobs/upgrade/create_liked_lists_job.rb
@@ -6,6 +6,7 @@ class Upgrade::CreateLikedListsJob < Upgrade::IterationJob
   end
 
   def each_iteration(user)
+    # i18n-tasks-use t('lists.special.liked')
     List.create(name: "lists.special.liked", special: :liked, owner: user) if user.lists.find_by(special: :liked).nil?
   end
 end
