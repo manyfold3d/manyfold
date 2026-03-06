9da5f947a03c07f0e6dc597e6c4ac3ec96834004
James Smith <james@floppy.org.uk>
2026-03-06 09:55:49 +0000

    don't allow renaming or removal of special lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -46,7 +46,7 @@ class ListsController < ApplicationController
   def update
     respond_to do |format|
       format.html do
-        if @list.update(list_params)
+        if @list.update(list_params(list: @list))
           redirect_back_or_to @list, notice: t(".success"), status: :see_other
         else
           render Views::Lists::Edit.new(list: @list), status: :unprocessable_content
@@ -69,11 +69,17 @@ class ListsController < ApplicationController
     authorize @list
   end
 
-  def list_params
-    params.require(:list).permit( # rubocop:todo Rails/StrongParametersExpect
-      :name,
-      list_items_attributes: [:id, :listable_type, :listable_id, :_destroy]
-    )
+  def list_params(list: nil)
+    if list&.special
+      params.require(:list).permit( # rubocop:todo Rails/StrongParametersExpect
+        list_items_attributes: [:id, :listable_type, :listable_id, :_destroy]
+      )
+    else
+      params.require(:list).permit( # rubocop:todo Rails/StrongParametersExpect
+        :name,
+        list_items_attributes: [:id, :listable_type, :listable_id, :_destroy]
+      )
+    end
   end
 
   def check_list_item_permissions
