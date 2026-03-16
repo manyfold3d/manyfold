70c50076bb832d6633ff2842c1b3602a971cf972
James Smith <james@floppy.org.uk>
2026-03-16 12:50:04 +0000

    fix show/hide

--- app/components/path_template_preview.rb
+++ app/components/path_template_preview.rb
@@ -11,28 +11,29 @@ class Components::PathTemplatePreview < Components::Base
 
   def view_template
     turbo_frame_tag "parse-preview" do
-      break unless @library.parse_metadata_from_path
-      p { t("components.path_template_preview.description") }
-      table class: "table table-striped table-sm" do
-        tr do
-          th { t("components.path_template_preview.path") }
-          th { Creator.model_name.human }
-          th { Collection.model_name.human }
-          th { ActsAsTaggableOn::Tag.model_name.human(count: 100) }
-          th { Model.model_name.human }
-        end
-        @paths.map do |path, parsed|
+      if @library.parse_metadata_from_path
+        p { t("components.path_template_preview.description") }
+        table class: "table table-striped table-sm" do
           tr do
-            td { path }
-            td { find_or_new_from_path_component(Creator, parsed[:creator])&.name || "❌" }
-            td { find_or_new_from_path_component(Collection, parsed[:collection])&.name || "❌" }
-            td {
-              parsed[:tags]&.map do |it|
-                Tag tag: ActsAsTaggableOn::Tag.new(name: it), link: false
-                whitespace
-              end || "❌"
-            }
-            td { to_human_name(parsed[:model_name]) || "❌" }
+            th { t("components.path_template_preview.path") }
+            th { Creator.model_name.human }
+            th { Collection.model_name.human }
+            th { ActsAsTaggableOn::Tag.model_name.human(count: 100) }
+            th { Model.model_name.human }
+          end
+          @paths.map do |path, parsed|
+            tr do
+              td { path }
+              td { find_or_new_from_path_component(Creator, parsed[:creator])&.name || "❌" }
+              td { find_or_new_from_path_component(Collection, parsed[:collection])&.name || "❌" }
+              td {
+                parsed[:tags]&.map do |it|
+                  Tag tag: ActsAsTaggableOn::Tag.new(name: it), link: false
+                  whitespace
+                end || "❌"
+              }
+              td { to_human_name(parsed[:model_name]) || "❌" }
+            end
           end
         end
       end
