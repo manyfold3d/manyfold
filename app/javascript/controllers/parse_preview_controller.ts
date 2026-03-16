618fd0542a1f4944829c3a812edff35c8acd3836
James Smith <james@floppy.org.uk>
2026-03-16 11:54:56 +0000

    Add stimulus controller to library form to trigger preview refresh

---
+++ app/javascript/controllers/parse_preview_controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="parse-preview"
+export default class extends Controller {
+  parsePreviewFrame: HTMLElement | null
+  templateInput: HTMLInputElement | null
+  parseMetadataInput: HTMLInputElement | null
+
+  connect () {
+    this.parsePreviewFrame = this.element.querySelector('#parse-preview')
+    this.templateInput = this.element.querySelector('#library_path_template')
+    this.parseMetadataInput = this.element.querySelector('#library_parse_metadata_from_path')
+    this.handleChange()
+  }
+
+  handleChange () {
+    if (this.parsePreviewFrame && this.templateInput && this.parseMetadataInput) {
+      this.parsePreviewFrame.src = `${this.parsePreviewFrame.dataset.src}?template=${this.templateInput.value}&enabled=${this.parseMetadataInput.checked ? "true" : "false"}`
+    }
+  }
+}
