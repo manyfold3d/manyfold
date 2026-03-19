218fce96387e76400034d518789946adeb1cff79
James Smith <james@floppy.org.uk>
2026-03-19 18:19:00 +0000

    Add new comment form

---
+++ app/components/new_comment_form.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Components::NewCommentForm < Components::Base
+  include Phlex::Rails::Helpers::FormFor
+
+  register_value_helper :policy
+
+  def initialize(commentable:)
+    @commentable = commentable
+    @comment = @commentable.comments.new
+  end
+
+  def render?
+    policy(@comment).create?
+  end
+
+  def view_template
+    form_for [@commentable, @comment] do |f|
+      div class: "new-comment-form-component", name: "new-comment" do
+        div(class: "comment-header") do
+          Icon(icon: "pen")
+          whitespace
+          span { t("components.new_comment_form.title") }
+        end
+        div(class: "comment-body") do
+          f.text_area(:comment, {class: "form-control"})
+        end
+        div(class: "comment-footer") do
+          f.submit t("components.new_comment_form.submit")
+        end
+      end
+    end
+  end
+end
