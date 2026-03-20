4c361f91345e50d498b4e7247d3549329191d0f3
James Smith <james@floppy.org.uk>
2026-03-20 16:59:19 +0000

    Make comments reportable

--- app/components/comment.rb
+++ app/components/comment.rb
@@ -30,7 +30,17 @@ class Components::Comment < Components::Base
           span(class: "comment-time", title: @comment.created_at) { t("components.comment.posted", time: time_ago_in_words(@comment.created_at)) }
         end
         div class: "comment-header-links" do
-          link_to t("components.comment.delete"), [@comment.commentable, @comment], method: :delete, class: "link-danger", data: {confirm: t("components.comment.confirm_delete")} if policy(@comment).destroy?
+          link_to polymorphic_path([@comment.commentable, @comment, :report], action: :new) do
+            Icon(icon: "flag", label: t("components.comment.report"))
+          end
+          whitespace
+          if policy(@comment).destroy?
+            span(class: "ms-2") do
+              link_to [@comment.commentable, @comment], method: :delete, class: "link-danger", data: {confirm: t("components.comment.confirm_delete")} do
+                Icon(icon: "trash", label: t("components.comment.delete"))
+              end
+            end
+          end
         end
       end
       div(class: "comment-body") { markdownify(@comment.comment) }
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+  def link_to_reportable(report)
+    reportable = report.object&.entity
+    return if reportable.nil?
+    if reportable.is_a? User
+      link_to reportable.username, settings_user_path(reportable)
+    else
+      link_to reportable.name, reportable
+    end
+  end
 end
