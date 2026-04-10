b2ba0a3acb71dbaae3892cd2f7b342b8d74f5e29
James Smith <james@floppy.org.uk>
2026-04-10 16:04:32 +0100

    include federated likes in like count

--- app/models/concerns/likeable.rb
+++ app/models/concerns/likeable.rb
@@ -2,6 +2,14 @@ module Likeable
   extend ActiveSupport::Concern
 
   def like_count
-    list_items.includes(:list).where("list.special": :liked).count
+    calculate_like_count
   end
+
+  private
+
+  def calculate_like_count
+    list_items.includes(:list).where("list.special": :liked).count +
+      Federails::Activity.includes(:actor).where(action: "Like", entity: [self] + comments.where(system: true), 'actor.local': false).count
+  end
+
 end
--- spec/models/concerns/likeable_shared.rb
+++ spec/models/concerns/likeable_shared.rb
@@ -5,4 +5,34 @@ shared_examples "Likeable" do
   it "changes like count when added to a list" do
     expect{user.liked_list.models << thing}.to change(thing, :like_count).by(1)
   end
+
+  context "with federation", :federated do
+    let(:actor) { create :actor, :distant }
+
+    it "includes remote Like activities in count" do
+      expect{
+        Federails::Activity.create action: "Like", entity: thing, actor: actor
+      }.to change(thing, :like_count).by(1)
+    end
+
+    it "includes Likes on system comments in count" do
+      comment = create :comment, commentable: thing, system: true, commenter: thing
+      expect{
+        Federails::Activity.create action: "Like", entity: comment, actor: actor
+      }.to change(thing, :like_count).by(1)
+    end
+
+    it "ignores Likes on non-system comments in count" do
+      comment = create :comment, commentable: thing, system: false, commenter: thing
+      expect{
+        Federails::Activity.create action: "Like", entity: comment, actor: actor
+      }.not_to change(thing, :like_count)
+    end
+
+    it "ignores local Like activities in count" do
+      expect{
+        Federails::Activity.create action: "Like", entity: thing, actor: user.federails_actor
+      }.not_to change(thing, :like_count)
+    end
+  end
 end
