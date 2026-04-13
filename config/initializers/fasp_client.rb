b75f954a9b92aeef0b635d63084fb4eb1323a35d
James Smith <james@floppy.org.uk>
2026-04-13 16:29:29 +0100

    Fix translation hints for FaspClient classes

--- config/initializers/fasp_client.rb
+++ config/initializers/fasp_client.rb
@@ -5,3 +5,7 @@ FaspClient.configure do |conf|
   conf.layout = "settings"
   conf.controller_base = "::ApplicationController"
 end
+
+# i18n-tasks-use t("activerecord.models.fasp_client_backfill_request")
+# i18n-tasks-use t("activerecord.models.fasp_client_event_subscription")
+# i18n-tasks-use t("activerecord.models.fasp_client_provider")
