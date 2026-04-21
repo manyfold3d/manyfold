9badf864ea052522f655f45cab0e3dd255cc10bc
James Smith <james@floppy.org.uk>
2026-04-21 17:35:20 +0100

    Add basic tests for quote auth model

---
+++ spec/models/federails/quote_authorization_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe Federails::QuoteAuthorization do
+  let(:model) { create(:model, :public) }
+  let(:comment) { create(:comment, federails_actor: model.federails_actor, commentable: model) }
+  let(:auth) {
+    described_class.create(
+      federails_actor: model.federails_actor,
+      quoting_actor: create(:actor, :distant),
+      interaction_target: comment,
+      interacting_object_url: "https://example.org/statuses/1",
+      quote_request_url: "https://example.org/quote_requests/1"
+    )
+  }
+
+  it "autogenerates a UUID" do
+    expect(auth.uuid).to be_present
+  end
+
+  it "can be accepted" do
+    expect { auth.accept! }.to change(auth.reload, :state).from(nil).to("accepted")
+  end
+
+  it "can be rejected" do
+    expect { auth.reject! }.to change(auth.reload, :state).from(nil).to("rejected")
+  end
+end
