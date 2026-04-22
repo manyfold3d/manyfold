f867e5d8af81d772f2e5b8ad7cc24e08a10e5298
James Smith <james@floppy.org.uk>
2026-04-22 17:39:46 +0100

    remove ancient data migration that manipulates collections

--- db/data/20230308006000_move_collection_tag_into_object.rb
+++ db/data/20230308006000_move_collection_tag_into_object.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
 
 class MoveCollectionTagIntoObject < ActiveRecord::Migration[7.0]
-  def up
-    Model.find_each do |model|
-      if defined?(model.collections) && !model.collection
-        model.collections.each do |collection|
-          newcol = Collection.find_or_create_by name: collection.name
-          newcol.models << model
-          newcol.save
-        end
-      end
-    end
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
 end
