26de37a95a4e24200a1a2e4f8bdbfcc6ba3c8281
James Smith <james@floppy.org.uk>
2026-04-25 01:00:26 +0100

    add guard on collected job

--- app/jobs/activity/model_collected_job.rb
+++ app/jobs/activity/model_collected_job.rb
@@ -4,7 +4,9 @@ class Activity::ModelCollectedJob < ApplicationJob
 
   def perform(model_id, collection_id)
     model = Model.find(model_id)
-    collection = Collection.find(collection_id)
+    collection = model.collections.find(collection_id)
+    # Make sure the model is still in the collection, this could be later on
+    return unless collection
     # Boost the new model and its creation comment to the collection's followers
     model.federails_actor.announce! actor: collection.federails_actor
     model.creation_comment.announce! actor: collection.federails_actor
