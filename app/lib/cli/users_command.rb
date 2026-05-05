4d498b2e255cf32651f7bbbb3e351f3ada446459
James Smith <james@floppy.org.uk>
2026-05-05 23:05:31 +0100

    Add user creation CLI command
    Resolves #6042

--- app/lib/cli/users_command.rb
+++ app/lib/cli/users_command.rb
@@ -30,5 +30,26 @@ module Cli
     rescue RuntimeError => e
       puts "\n#{e}"
     end
+
+    desc "create", "create a new user"
+    option :email, required: true, type: :string, aliases: :mail
+    option :username, required: true, type: :string, aliases: :login
+    option :password, required: true, type: :string
+    option :role, required: false, type: :string, enum: %w[administrator moderator contributor member]
+    def create
+      u = User.create(
+        username: options[:username],
+        email: options[:email],
+        password: options[:password],
+        password_confirmation: options[:password_confirmation]
+      )
+      if u.valid?
+        u.add_role(options[:role].to_sym) if options[:role]
+        puts "\nNew user created OK"
+      else
+        puts "\nError when creating user:\n"
+        puts u.errors.full_messages.inspect
+      end
+    end
   end
 end
