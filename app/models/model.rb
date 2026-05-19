62ae0a038c5b4280456d2527dfc6f1bcf2007b36
James Smith <james@floppy.org.uk>
2026-05-19 21:59:48 +0100

    allow preview files if loadable by f3d

--- app/models/model.rb
+++ app/models/model.rb
@@ -211,7 +211,7 @@ class Model < ApplicationRecord
   end
 
   def valid_preview_files
-    model_files.select { |it| it.is_image? || it.is_video? || Components::Renderers::Three.supports?(it) }
+    model_files.select { |it| it.is_image? || it.is_video? || FileHandlers::F3d.can_load?(it.mime_type) || FileHandlers::Three.can_load?(it.mime_type) }
   end
 
   def image_files
