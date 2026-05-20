319f3f4d4397f78663d985121041bae551c34da6
James Smith <james@floppy.org.uk>
2026-05-20 11:18:18 +0100

    Add validation for relationship predicates

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -7,28 +7,39 @@ RSpec.describe Relationship do
     let(:second) { create(:model_file, model: model) }
 
     it "creates valid objects" do
-      relationship = described_class.create(subject: first, objekt: second, predicate: "supported")
+      relationship = described_class.create(subject: first, objekt: second, predicate: "supported_version_of")
       expect(relationship).to be_valid
     end
 
+    it "must use known predicates" do
+      relationship = described_class.create(subject: first, objekt: second, predicate: "invalid_predicate_for")
+      expect(relationship).not_to be_valid
+    end
+
     it "cannot duplicate objects" do
-      described_class.create(subject: first, objekt: second, predicate: "supported")
-      relationship = described_class.create(subject: first, objekt: second, predicate: "supported")
+      described_class.create(subject: first, objekt: second, predicate: "supported_version_of")
+      relationship = described_class.create(subject: first, objekt: second, predicate: "supported_version_of")
       expect(relationship).not_to be_valid
     end
   end
 
-  it "can create relationships between models" do
-    model1 = create(:model)
-    model2 = create(:model)
-    relationship = described_class.create(subject: model1, objekt: model2, predicate: "remixed")
-    expect(relationship).to be_valid
+  context "when creating relationships between models" do
+    let(:first) { create(:model) }
+    let(:second) { create(:model) }
+
+    it "creates valid objects" do
+      relationship = described_class.create(subject: first, objekt: second, predicate: "adapted_from")
+      expect(relationship).to be_valid
+    end
   end
 
-  it "can create relationships between models and links" do
-    model = create(:model)
-    link = create(:link, linkable: model)
-    relationship = described_class.create(subject: model, objekt: link, predicate: "remixed")
-    expect(relationship).to be_valid
+  context "when creating relationships between models and links" do
+    let(:model) { create(:model) }
+    let(:link) { create(:link, linkable: model) }
+
+    it "creates valid objects" do
+      relationship = described_class.create(subject: model, objekt: link, predicate: "adapted_from")
+      expect(relationship).to be_valid
+    end
   end
 end
