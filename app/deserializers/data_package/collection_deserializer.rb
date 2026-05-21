60dca68adc625e0d70127359b9613f6714b63c88
James Smith <james@floppy.org.uk>
2026-05-21 18:00:08 +0100

    Fall back to matching creators and collections by name when parsing datapackages

--- app/deserializers/data_package/collection_deserializer.rb
+++ app/deserializers/data_package/collection_deserializer.rb
@@ -5,18 +5,24 @@ module DataPackage
       attributes = {
         name: @object["title"],
         caption: @object["caption"],
-        notes: @object["description"]
+        notes: @object["description"],
+        links_attributes: []
       }
       begin
         route_options = Rails.application.routes.recognize_path(@object["path"])
         if route_options[:controller] == "collections"
-          attributes[:id] = Collection.find_param(route_options[:id]).id
+          attributes[:id] = Collection.find_param(route_options[:id])&.id
         end
-      rescue ActionController::RoutingError, ActiveRecord::RecordNotFound
+      rescue ActiveRecord::RecordNotFound
+        # ID wasn't found, match by name instead (at least until we allow collections with the same name)
+        attributes[:id] = Collection.find_by(name: attributes[:name])&.id
+      rescue ActionController::RoutingError
+        # ID wasn't found, match by name instead
+        attributes[:id] = Collection.find_by(name: attributes[:name])&.id
+        attributes[:links_attributes] << {url: @object["path"]} if @object["path"]&.match?(URI::RFC2396_PARSER.make_regexp)
       end
-      attributes[:links_attributes] = @object["links"]&.map { |it| LinkDeserializer.new(it).deserialize } || []
-      attributes[:links_attributes] << {url: @object["path"]} unless attributes.has_key?(:id)
-      attributes.compact
+      attributes[:links_attributes].concat(@object["links"]&.map { |it| LinkDeserializer.new(it).deserialize } || [])
+      attributes.compact_blank
     end
   end
 end
