3eb1b6bf21eb3b92d07ab156002596fa77ee9575
James Smith <james@floppy.org.uk>
2026-05-22 16:55:13 +0100

    get form working with presupported versions properly

--- app/deserializers/form/model_file_deserializer.rb
+++ app/deserializers/form/model_file_deserializer.rb
@@ -2,7 +2,7 @@ module Form
   class ModelFileDeserializer < BaseDeserializer
     def deserialize
       return nil unless @params
-      params = @params.require(:model_file).permit([
+      @params.require(:model_file).permit([
         :filename, # i18n-tasks-use t("activerecord.attributes.model_file.filename")
         :presupported, # i18n-tasks-use t("activerecord.attributes.model_file.presupported")
         # printed isn't actually deserialized here but we'll include the form attribute anyway
@@ -10,18 +10,10 @@ module Form
         :notes, # i18n-tasks-use t("activerecord.attributes.model_file.notes")
         :caption, # i18n-tasks-use t("activerecord.attributes.model_file.caption")
         :y_up, # i18n-tasks-use t("activerecord.attributes.model_file.y_up")
-        :previewable, # i18n-tasks-use t("activerecord.attributes.model_file.previewable")
-        :presupported_version_id # i18n-tasks-use t("activerecord.attributes.model_file.presupported_version_id")
+        :previewable # i18n-tasks-use t("activerecord.attributes.model_file.previewable")
+        # not actually deserialized here
+        # i18n-tasks-use t("activerecord.attributes.model_file.presupported_version_id")
       ])
-      if (id = params.delete("presupported_version_id"))
-        if (file = @record.model.model_files.find(id))
-          params[:reverse_relationships_attributes] = [{
-            subject: file,
-            predicate: "supported_version_of"
-          }]
-        end
-      end
-      params
     end
   end
 end
