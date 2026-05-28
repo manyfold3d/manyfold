ad7933fb026e873c6909fbf3aa8f32e40c40529c
James Smith <james@floppy.org.uk>
2026-05-28 10:57:18 +0100

    use separate predicate lists for different type relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,12 +1,17 @@
 class Relationship < ApplicationRecord
-  PREDICATES = [
-    "adapted_from",
-    "alternative_format_of",
-    "supported_version_of"
+  MODEL_TO_MODEL_PREDICATES = [
+    "adapted_from" # i18n-tasks-use t("activerecord.models.relationships.predicates.adapted_from")
   ]
+
+  FILE_TO_FILE_PREDICATES = [
+    "alternative_format_of", # i18n-tasks-use t("activerecord.models.relationships.predicates.alternative_format_of")
+    "supported_version_of" # i18n-tasks-use t("activerecord.models.relationships.predicates.supported_version_of")
+  ]
+
+  ALL_PREDICATES = MODEL_TO_MODEL_PREDICATES + FILE_TO_FILE_PREDICATES
 
   belongs_to :subject, polymorphic: true
   belongs_to :objekt, polymorphic: true
 
-  validates :predicate, uniqueness: {scope: [:subject, :objekt]}, inclusion: {in: PREDICATES}
+  validates :predicate, uniqueness: {scope: [:subject, :objekt]}, inclusion: {in: ALL_PREDICATES}
 end
