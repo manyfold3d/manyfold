156e73169c0482559abccb03982cf8c8529c4d9d
James Smith <james@floppy.org.uk>
2026-05-29 12:53:07 +0100

    memoize database detector

--- app/lib/database_detector.rb
+++ app/lib/database_detector.rb
@@ -1,40 +1,50 @@
 module DatabaseDetector
-  def self.server
-    ActiveRecord::Base.with_connection do |connection|
-      case connection.adapter_name
-      when "PostgreSQL"
-        :postgresql
-      when "Mysql2"
-        :mysql
-      when "SQLite"
-        :sqlite
-      when "NullDB"
-        :null
-      else
-        raise NotImplementedError.new("Unknown database adapter #{connection.adapter_name}")
+  class << self
+    extend Memoist
+
+    def server
+      ActiveRecord::Base.with_connection do |connection|
+        case connection.adapter_name
+        when "PostgreSQL"
+          :postgresql
+        when "Mysql2"
+          :mysql
+        when "SQLite"
+          :sqlite
+        when "NullDB"
+          :null
+        else
+          raise NotImplementedError.new("Unknown database adapter #{connection.adapter_name}")
+        end
       end
     end
-  end
+    memoize :server
 
-  def self.is_mysql?
-    server == :mysql
-  end
-
-  def self.is_mariadb?
-    is_mysql?
-  end
-
-  def self.is_postgres?
-    server == :postgresql
-  end
-
-  def self.is_sqlite?
-    server == :sqlite
-  end
-
-  def self.table_ready?(table_name)
-    ActiveRecord::Base.with_connection do |connection|
-      connection.data_source_exists? table_name
+    def is_mysql?
+      server == :mysql
     end
+    memoize :is_mysql?
+
+    def is_mariadb?
+      is_mysql?
+    end
+    memoize :is_mariadb?
+
+    def is_postgres?
+      server == :postgresql
+    end
+    memoize :is_postgres?
+
+    def is_sqlite?
+      server == :sqlite
+    end
+    memoize :is_sqlite?
+
+    def table_ready?(table_name)
+      ActiveRecord::Base.with_connection do |connection|
+        connection.data_source_exists? table_name
+      end
+    end
+    memoize :table_ready?
   end
 end
