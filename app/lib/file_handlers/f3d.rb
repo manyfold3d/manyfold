9e233e1a02e18551648df0253171665fe5679542
James Smith <james@floppy.org.uk>
2026-05-29 13:10:37 +0100

    update f3d tests for changes in structure

--- app/lib/file_handlers/f3d.rb
+++ app/lib/file_handlers/f3d.rb
@@ -1,8 +1,13 @@
 class FileHandlers::F3d < FileHandlers::Base
+  def self.readers
+    `f3d --list-readers`
+  end
+
   ENVIRONMENTS = [:server].freeze
-  INPUT_TYPES = `f3d --list-readers`.lines
+  INPUT_TYPES = self.readers.lines
     .filter_map { |it| it.match(/\w[a-z]*\/[0-9a-z.+-]*\w/)&.to_s }
     .filter_map { |it| Mime::Type.lookup(it) }
     .uniq
     .freeze
+
 end
