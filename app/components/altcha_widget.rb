4febf6e64f69cd80b7a64183143997f982d98f2d
James Smith <james@floppy.org.uk>
2026-06-04 21:02:25 +0100

    use standard altcha widget
    customisation wasn't working, and it's fine

--- app/components/altcha_widget.rb
+++ app/components/altcha_widget.rb
@@ -4,15 +4,10 @@ class Components::AltchaWidget < Components::Base
   register_element :altcha_widget
 
   def view_template
-    div class: "altcha-wrapper" do
-      Icon(icon: "robot", label: t("components.altcha_widget.help"))
-      altcha_widget(
-        id: "altcha-widget",
-        auto: "onload",
-        challenge: Altcha.create_challenge.to_json,
-        hidefooter: true,
-        hidelogo: true
-      )
-    end
+    altcha_widget(
+      id: "altcha-widget",
+      auto: "onload",
+      challenge: Altcha.create_challenge.to_json
+    )
   end
 end
