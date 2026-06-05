7a8a9541f269eb4b0dc0706e670886eddbdc91d9
James Smith <james@floppy.org.uk>
2026-06-05 16:37:30 +0100

    explicitly silence sass declarations

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import RubyPlugin from 'vite-plugin-ruby'
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-  ]
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["import", "color-functions", "global-builtin", "if-function"]
+      }
+    }
+  }
 })
