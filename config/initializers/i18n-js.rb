6a80c1b477d9acc8bc02e25e6141d345a4bf05f3
James Smith <james@floppy.org.uk>
2026-06-05 11:33:16 +0100

    Only run i18n listener in dev mode

--- config/initializers/i18n-js.rb
+++ config/initializers/i18n-js.rb
@@ -1,6 +1,8 @@
-Rails.application.config.after_initialize do
-  require "i18n-js/listen"
-  I18nJS.listen(
-    config_file: Rails.root.join("config/i18n-js.yml")
-  )
+if Rails.env.development?
+  Rails.application.config.after_initialize do
+    require "i18n-js/listen"
+    I18nJS.listen(
+      config_file: Rails.root.join("config/i18n-js.yml")
+    )
+  end
 end
