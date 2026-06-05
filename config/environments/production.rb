7fca563c24086f55789fc950bd756d2a764c38a8
James Smith <james@floppy.org.uk>
2026-06-05 17:31:52 +0100

    remove propshaft setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,6 @@ Rails.application.configure do
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = {"cache-control" => "public, max-age=#{1.year.to_i}"}
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
