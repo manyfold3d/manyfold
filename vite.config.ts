607ae07777892c44d22d15a0745f9513f0f0f680
James Smith <james@floppy.org.uk>
2026-06-08 10:14:31 +0100

    add vite wasm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    wasm()
   ],
   css: {
     preprocessorOptions: {
