f1c7da25a2a766a20ecba08773f9e54971f3ca11
James Smith <james@floppy.org.uk>
2026-06-09 17:20:51 +0100

    Use the Rails plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import Rails from 'vite-plugin-rails'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
+    Rails(),
   ],
   css: {
     preprocessorOptions: {
