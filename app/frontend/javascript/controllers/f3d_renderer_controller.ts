5b7b10ed9d6a55db84d4cdae7f55c3fe3622af26
James Smith <james@floppy.org.uk>
2026-06-10 12:47:04 +0100

    More accurate rendering and camera position

--- app/frontend/javascript/controllers/f3d_renderer_controller.ts
+++ app/frontend/javascript/controllers/f3d_renderer_controller.ts
@@ -3,47 +3,61 @@ import f3d from 'f3d'
 
 // Connects to data-controller="f3d-renderer"
 export default class extends Controller {
-  init (data: Uint8Array): void {
+
+  engine: any;
+
+  async init (data: Uint8Array): Promise<void> {
     const settings = {
       canvas: this.element,
-      setupOptions: (options) => {
-        // background must be set to black for proper blending with transparent canvas
-        options.setAsString("render.background.color", "#555555");
-
-        // make it look nice
-        options.toggle("render.effect.antialiasing.enable");
-        options.toggle("render.effect.tone_mapping");
-        options.toggle("render.effect.ambient_occlusion");
-        options.toggle("render.hdri.ambient");
-
-        // display widgets
-        options.toggle("ui.axis", true);
-        options.toggle("render.grid.enable", true);
-
-        // default to +Z
-        options.setAsString("scene.up_direction", "+Z");
-      }
     };
     f3d(settings).then(async (F3D) => {
       F3D.Engine.autoloadPlugins();
-      F3D.engineInstance = F3D.Engine.create();
-      F3D.setupOptions(F3D.engineInstance.getOptions());
-      const canvas = this.element;
-      const scale = window.devicePixelRatio;
-      F3D.engineInstance
-        .getWindow()
-        .setSize(scale * canvas.clientWidth, scale * canvas.clientHeight);
+      this.engine = F3D.Engine.create();
+      const options = this.engine.getOptions();
+      // background must be set to black for proper blending with transparent canvas
+      options.setAsString("render.background.color", "#555555");
 
-      const scene = F3D.engineInstance.getScene();
-      scene.clear();
+      // make it look nice
+      options.toggle('render.effect.antialiasing.enable')
+      options.toggle('render.effect.tone_mapping')
+      options.toggle('render.effect.ambient_occlusion')
+
+      options.toggle('scene.animation.autoplay', true)
+
+      // display widgets
+      options.toggle('render.grid.enable', true)
+      options.setAsString('render.grid.color', '#00ffff')
+      options.setAsString('render.grid.subdivisions', '0')
+      options.setAsString('render.grid.unit', '10')
+
+      // default to +Z
+      options.setAsString('scene.up_direction', '+Z')
+
+      const canvas = this.element
+      const scale = window.devicePixelRatio
+      console.log(scale * canvas.clientWidth)
+      console.log(scale * canvas.clientHeight)
+      this.engine.getWindow().setSize(scale * canvas.clientWidth, scale * canvas.clientHeight)
+
+      const scene = this.engine.getScene()
+      scene.clear()
       try {
         scene.addBuffer(data)
       } catch (e) {
         console.log('Unsupported file')
       }
-      F3D.engineInstance.getWindow().getCamera().resetToBounds(0.9);
-      F3D.engineInstance.getWindow().render();
-      F3D.engineInstance.getInteractor().start();
+
+      const camera = this.engine.getWindow().getCamera()
+      const foc = camera.focalPoint
+      const dir = [-1, 1, -0.5]
+      const pos = [0, 0, 0]
+      for (let i = 0; i < 3; i++) {
+        pos[i] = foc[i] - dir[i]
+      }
+      camera.position = pos
+      camera.resetToBounds(0.9)
+      this.engine.getWindow().render()
+      this.engine.getInteractor().start()
     })
   }
 
