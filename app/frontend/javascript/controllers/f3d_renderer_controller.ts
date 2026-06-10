193577711b2cb7c0ed14227da2e62e66fc0c7476
James Smith <james@floppy.org.uk>
2026-06-10 10:48:49 +0100

    load and render file - working!

--- app/frontend/javascript/controllers/f3d_renderer_controller.ts
+++ app/frontend/javascript/controllers/f3d_renderer_controller.ts
@@ -3,12 +3,12 @@ import f3d from 'f3d'
 
 // Connects to data-controller="f3d-renderer"
 export default class extends Controller {
-  onLoad (): void {
+  init (data: Uint8Array): void {
     const settings = {
       canvas: this.element,
       setupOptions: (options) => {
         // background must be set to black for proper blending with transparent canvas
-        options.setAsString("render.background.color", "#000000");
+        options.setAsString("render.background.color", "#555555");
 
         // make it look nice
         options.toggle("render.effect.antialiasing.enable");
@@ -33,9 +33,32 @@ export default class extends Controller {
       F3D.engineInstance
         .getWindow()
         .setSize(scale * canvas.clientWidth, scale * canvas.clientHeight);
-      // do a first render and start the interactor
+
+      const scene = F3D.engineInstance.getScene();
+      scene.clear();
+      try {
+        scene.addBuffer(data)
+      } catch (e) {
+        console.log('Unsupported file')
+      }
+      F3D.engineInstance.getWindow().getCamera().resetToBounds(0.9);
       F3D.engineInstance.getWindow().render();
       F3D.engineInstance.getInteractor().start();
     })
+  }
+
+  async onLoad () {
+    const url = this.element.dataset.previewUrl;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.bytes();
+      this.init(result);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 }
