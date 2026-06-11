ce3f3d86be348bde1efe7b6ea9132c6904a39906
James Smith <james@floppy.org.uk>
2026-06-11 13:09:34 +0100

    update F3D WASM file support list

--- app/lib/file_handlers/f3d_wasm.rb
+++ app/lib/file_handlers/f3d_wasm.rb
@@ -1,40 +1,6 @@
 class FileHandlers::F3dWasm < FileHandlers::Base
   ENVIRONMENTS = [:browser, :preview_frame].freeze
-  INPUT_TYPES = Mime::EXTENSION_LOOKUP.slice(
-    "gml",
-    "gltf",
-    "glb",
-    "obj",
-    "ply",
-    "pts",
-    "stl",
-    "vtk",
-    "vtp",
-    "vtu",
-    "vtkhdf",
-    "3ds",
-    "wrl",
-    "vrml",
-    "fbx",
-    "off",
-    "x",
-    "dae",
-    "ex2",
-    "exo",
-    "e",
-    "g",
-    "stp",
-    "step",
-    "igs",
-    "iges",
-    "brep",
-    "xbf",
-    "drc",
-    "mdl",
-    "nc",
-    "cdf",
-    "ncdf"
-  ).values.freeze
+  INPUT_TYPES = Mime::EXTENSION_LOOKUP.slice(*%w[gml gltf glb obj ply pts stl vtk vtp vtu vtkhdf 3ds wrl vrml fbx dae dxf off x 3mf ex2 exo e g stp step igs iges brep xbf drc mdl nc cdf ncdf ifc]).values.uniq.freeze
 
   def self.priority
     100
