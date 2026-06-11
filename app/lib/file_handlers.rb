f72ff413c1d0f8d93a9cf8862a4e5dd886a07a7a
James Smith <james@floppy.org.uk>
2026-06-11 10:32:25 +0100

    Use low-level rails cache for handlers

--- app/lib/file_handlers.rb
+++ app/lib/file_handlers.rb
@@ -4,9 +4,12 @@ module FileHandlers
   ALL_HANDLERS = FileHandlers.constants.without(:Base, :Slic3rFamily).map { |it| FileHandlers.const_get("FileHandlers::#{it}") }.freeze
 
   def self.handlers_for(environment:, mime_type:)
-    ALL_HANDLERS # rubocop:disable Pundit/UsePolicyScope
-      .select { |it| it.const_get(:ENVIRONMENTS).include? environment }
-      .select { |it| it.can_load? mime_type }
-      .sort { |a, b| b&.priority <=> a&.priority }
+    Rails.cache.fetch("FileHandlers_handlers_for_#{environment}_#{mime_type}", expires_in: 1.hour) do
+      Rails.logger.debug { "CACHE MISS for FileHandlers_handlers_for_#{environment}_#{mime_type}" }
+      ALL_HANDLERS # rubocop:disable Pundit/UsePolicyScope
+        .select { |it| it.const_get(:ENVIRONMENTS).include? environment }
+        .select { |it| it.can_load? mime_type }
+        .sort { |a, b| b&.priority <=> a&.priority }
+    end
   end
 end
