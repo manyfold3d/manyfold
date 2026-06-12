1ff51215d2ea1296242df7c0561fe54aef958968
James Smith <james@floppy.org.uk>
2026-06-12 16:44:51 +0100

    Use vite rails plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import Rails from 'vite-plugin-rails'
 import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
+    Rails(),
     wasm()
   ],
   css: {
