72b416a89f664c5d8ef0ff68a06ef40b09c6cee6
James Smith <james@floppy.org.uk>
2026-06-15 12:13:37 +0100

    Add remix / source cards

---
+++ app/components/remixes_card.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+class Components::RemixesCard < Components::Base
+  include Phlex::Rails::Helpers::LinkTo
+
+  register_value_helper :policy_scope
+
+  def initialize(model:)
+    @model = model
+  end
+
+  def render?
+    @model.reverse_relationships.where(predicate: "adapted_from").any?
+  end
+
+  def before_template
+    @remixes = @model.reverse_relationships.where(predicate: "adapted_from").map { it.subject }
+  end
+
+  def view_template
+    Card title: t("components.remixes_card.title"), variant: :secondary do
+      div class: "card-text" do
+        @remixes.each do |remix|
+          ModelCard(model: remix)
+        end
+      end
+    end
+  end
+
+  private
+
+  def add_path(list:)
+    list_path(list,
+      list: {
+        list_items_attributes: {
+          "0" => {listable_type: @listable.model_name, listable_id: @listable.id}
+        }
+      })
+  end
+
+  def remove_path(list:)
+    list_path(list,
+      list: {
+        list_items_attributes: {
+          "0" => {id: list.list_items.find_by(listable: @listable), _destroy: "1"}
+        }
+      })
+  end
+
+  def add_dropdown
+    div class: "btn-group" do
+      button type: "button",
+        class: "btn btn-secondary mt-1 me-1 dropdown-toggle",
+        data: {
+          bs_toggle: "dropdown"
+        },
+        aria: {
+          expanded: false,
+          haspopup: "menu",
+          controls: "lists-menu"
+        } do
+        Icon(icon: "stars")
+        whitespace
+        span { t("components.lists_card.add_to_list") }
+      end
+      ul class: "dropdown-menu", role: "menu", id: "lists-menu" do
+        @off_lists.map do |list|
+          DropdownItem label: list.name, path: add_path(list: list), method: :patch
+        end
+        DropdownDivider() if @off_lists.any?
+        DropdownItem icon: "plus-circle", label: t("views.lists.new.title"), path: new_list_path
+      end
+    end
+  end
+
+  def on_lists
+    span { t("components.lists_card.on_lists") }
+    table class: "table table-striped" do
+      @on_lists.map do
+        tr do
+          td { link_to it.name, list_path(it) }
+          td do
+            link_to remove_path(list: it), method: :patch, class: "link-danger link-underline-opacity-0" do
+              Icon(icon: "trash", label: t("components.lists_card.remove"))
+            end
+          end
+        end
+      end
+    end
+  end
+end
