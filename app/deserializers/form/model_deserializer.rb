c422c64fc61e6a9090f0ead2b22e77308154a8c5
James Smith <james@floppy.org.uk>
2026-06-15 11:12:55 +0100

    save model relationships

--- app/deserializers/form/model_deserializer.rb
+++ app/deserializers/form/model_deserializer.rb
@@ -23,8 +23,15 @@ module Form
         :missingtag,
         collection_ids: [],
         tag_list: [],
-        links_attributes: [:id, :url, :text, :_destroy] # i18n-tasks-use t("activerecord.attributes.link.url")
+        links_attributes: [:id, :url, :text, :_destroy], # i18n-tasks-use t("activerecord.attributes.link.url")
+        relationships_attributes: [:id, :predicate, :objekt_id, :_destroy] # i18n-tasks-use t("activerecord.attributes.model.relationships")
       )
+      if allowed[:relationships_attributes]
+        allowed[:relationships_attributes] = allowed[:relationships_attributes].to_h.map do |k, attrs|
+          attrs[:objekt] = ModelPolicy::Scope.new(@user, Model).resolve.find_param(attrs.delete(:objekt_id)) if attrs[:objekt_id] && attrs[:id].nil?
+          attrs
+        end
+      end
       allowed[:collections] = CollectionPolicy::Scope.new(@user, Collection).resolve.where(public_id: allowed.delete(:collection_ids)) if allowed[:collection_ids]
       return allowed unless user_can_set_permissions?
       allowed.deep_merge(caber_relations_attributes(type: :model))
