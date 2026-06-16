d625a4cf83049440f72c594478a6e5cd5e42e1f5
James Smith <james@floppy.org.uk>
2026-06-17 00:25:33 +0100

    change protocol list to hash lookup

--- app/models/print_host.rb
+++ app/models/print_host.rb
@@ -1,9 +1,9 @@
 class PrintHost < ApplicationRecord
   # i18n-tasks-use t("activerecord.models.print_host")
 
-  PROTOCOLS = Print.constants.map { const_get("Print::#{it}::PROTOCOL") }.freeze
+  PROTOCOLS = Print.constants.map { [const_get("Print::#{it}::PROTOCOL"), const_get("Print::#{it}")] }.to_h.freeze
 
   validates :name, presence: true
   validates :endpoint, presence: true
-  validates :protocol, presence: true, inclusion: {in: PROTOCOLS}
+  validates :protocol, presence: true, inclusion: {in: PROTOCOLS.keys}
 end
