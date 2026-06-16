193587ed77e9d4e5f69c6982c1ac851f503b1420
James Smith <james@floppy.org.uk>
2026-06-16 14:32:15 +0100

    add basic print host fields, validation, and tests

--- app/models/print_host.rb
+++ app/models/print_host.rb
@@ -1,2 +1,10 @@
 class PrintHost < ApplicationRecord
+
+  PROTOCOLS = [
+    "moonraker"
+  ].freeze
+
+  validates :name, presence: true
+  validates :endpoint, presence: true
+  validates :protocol, presence: true, inclusion: {in: PROTOCOLS}
 end
