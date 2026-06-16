37d10f04f9b5817e13f21559a4cd370a816dc8a8
James Smith <james@floppy.org.uk>
2026-06-17 00:16:20 +0100

    Change print service arg to print host object

--- app/services/print/moonraker_service.rb
+++ app/services/print/moonraker_service.rb
@@ -5,13 +5,14 @@ class Print::MoonrakerService
   # i18n-tasks-use t("print_hosts.protocols.moonraker")
   PROTOCOL = "moonraker".freeze
 
-  def initialize(server_root:)
-    @server_root = server_root
+  def initialize(print_host:)
+    @print_host = print_host
+  end
   end
 
   def upload(file:, start_print: true)
     raise ArgumentError unless file.mime_type.to_sym == :gcode
-    connection.post(uri, payload(file: file, start_print: start_print))
+    connection.post(upload_uri, payload(file: file, start_print: start_print))
   end
 
   private
@@ -33,7 +34,7 @@ class Print::MoonrakerService
     }
   end
 
-  def uri
-    "#{@server_root}/server/files/upload"
+  def upload_uri
+    "#{@print_host.endpoint}/server/files/upload"
   end
 end
