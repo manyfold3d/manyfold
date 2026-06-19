1e4c3258b98c026046eb94055da4144f932919ec
James Smith <james@floppy.org.uk>
2026-06-19 11:14:59 +0100

    v1 PrusaLink API

---
+++ app/services/print/prusa_link_service.rb
@@ -0,0 +1,67 @@
+require "faraday"
+require "faraday/multipart"
+
+class Print::PrusaLinkService
+  # i18n-tasks-use t("print_hosts.protocols.octoprint")
+  PROTOCOL = "prusalink".freeze
+
+  INPUT_TYPES = [Mime[:gcode]].freeze
+
+  def initialize(print_host:)
+    @print_host = print_host
+  end
+
+  def ok?
+    response = connection.get(info_uri, {}, headers)
+    Rails.logger.warn(response.inspect) unless response.success?
+    response.success?
+  rescue => ex
+    Rails.logger.warn(ex.message)
+    false
+  end
+
+  def upload(file:, start_print: true)
+    raise ArgumentError unless file.mime_type.to_sym == :gcode
+    raise PrintHost::NotReady unless ok?
+    response = v1_upload(file: file)
+    Rails.logger.warn(response.inspect) unless response.success?
+    response.success?
+  end
+
+  private
+
+  def connection
+    Faraday.new do
+      it.request :multipart
+    end
+  end
+
+  def info_uri
+    "#{@print_host.endpoint}/api/printer"
+  end
+
+  def v1_upload(file:)
+    connection.post(
+      "#{@print_host.endpoint}/api/files/local",
+      v1_payload(file: file),
+      headers
+    )
+  end
+
+  def v1_payload(file:, start_print: true)
+    {
+      select: start_print ? "true" : "false",
+      file: Faraday::Multipart::FilePart.new(
+        file.attachment.open,
+        file.mime_type.to_s,
+        file.filename
+      )
+    }
+  end
+
+  def headers
+    {
+      "X-Api-Key" => @print_host.credentials
+    }.compact_blank
+  end
+end
