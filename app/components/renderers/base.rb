f552e5eca3e42b28350d4b4f8711539b10a2013b
James Smith <james@floppy.org.uk>
2026-06-26 12:13:29 +0100

    explicit module for renderer components

--- app/components/renderers/base.rb
+++ app/components/renderers/base.rb
@@ -1,14 +1,16 @@
-class Components::Renderers::Base < Components::Base
-  def self.supports?(file)
-    raise NotImplementedError
-  end
+module Components::Renderers
+  class Base < Components::Base
+    def self.supports?(file)
+      raise NotImplementedError
+    end
 
-  def initialize(file:, derivative: nil)
-    @file = file
-    @derivative = derivative
-  end
+    def initialize(file:, derivative: nil)
+      @file = file
+      @derivative = derivative
+    end
 
-  def view_template
-    raise NotImplementedError
+    def view_template
+      raise NotImplementedError
+    end
   end
 end
--- app/components/renderers/rouge.rb
+++ app/components/renderers/rouge.rb
@@ -1,19 +1,21 @@
-class Components::Renderers::Rouge < Components::Renderers::Base
-  def self.supports?(file)
-    FileHandlers::Rouge.can_load? file&.mime_type
-  end
-
-  def before_template
-    @formatter = ::Rouge::Formatters::HTMLLineTable.new(::Rouge::Formatters::HTML.new)
-    @lexer = ::Rouge::Lexer.all.find { @file.mime_type.in? it.mimetypes } # rubocop:disable Pundit/UsePolicyScope
-  end
-
-  def view_template
-    style do
-      style { raw(safe(::Rouge::Themes::ThankfulEyes.render(scope: ".rouge"))) } # rubocop:disable Rails/OutputSafety
+module Components::Renderers
+  class Rouge < Components::Renderers::Base
+    def self.supports?(file)
+      FileHandlers::Rouge.can_load? file&.mime_type
     end
-    div class: "rouge" do
-      raw(safe(@formatter.format(@lexer.lex(@file.attachment.read)))) # rubocop:disable Rails/OutputSafety
+
+    def before_template
+      @formatter = ::Rouge::Formatters::HTMLLineTable.new(::Rouge::Formatters::HTML.new)
+      @lexer = ::Rouge::Lexer.all.find { @file.mime_type.in? it.mimetypes } # rubocop:disable Pundit/UsePolicyScope
+    end
+
+    def view_template
+      style do
+        style { raw(safe(::Rouge::Themes::ThankfulEyes.render(scope: ".rouge"))) } # rubocop:disable Rails/OutputSafety
+      end
+      div class: "rouge" do
+        raw(safe(@formatter.format(@lexer.lex(@file.attachment.read)))) # rubocop:disable Rails/OutputSafety
+      end
     end
   end
 end
