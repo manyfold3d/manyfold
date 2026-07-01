a2d6e35e3b21e6a231d7f9f87f98f269e1a24a5f
James Smith <james@floppy.org.uk>
2026-07-01 16:25:04 +0100

    move plugin phlex setup into proper before_initialize hook

--- config/initializers/phlex.rb
+++ config/initializers/phlex.rb
@@ -7,17 +7,12 @@ module Components
   extend Phlex::Kit
 end
 
-Rails.autoloaders.main.push_dir(
-  Rails.root.join("app/views"), namespace: Views
-)
+Rails.application.config.before_initialize do
+  Rails.autoloaders.main.push_dir(
+    Rails.root.join("app/views"), namespace: Views
+  )
 
-Rails.autoloaders.main.push_dir(
-  Rails.root.join("app/components"), namespace: Components
-)
-
-PLUGINS.each do |key, gemspec|
-  plugin_component_dir = "#{gemspec.metadata[:path]}/app/components"
-  Rails.autoloaders.main.push_dir(plugin_component_dir, namespace: Components) if Dir.exist?(plugin_component_dir)
-  plugin_view_dir = "#{gemspec.metadata[:path]}/app/views"
-  Rails.autoloaders.main.push_dir(plugin_view_dir, namespace: Views) if Dir.exist?(plugin_view_dir)
+  Rails.autoloaders.main.push_dir(
+    Rails.root.join("app/components"), namespace: Components
+  )
 end
