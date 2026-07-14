fa4a0f4a82a41b98188ddaada0d611b26d1a34c2
James Smith <james@floppy.org.uk>
2026-07-14 15:49:24 +0100

    disable no-void check

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import neostandard from 'neostandard'
 
-export default neostandard({
-  ts: true,
-  ignores: [
-    'public/vite*/*',
-    'coverage/*'
-  ]
-})
+export default [
+  ...neostandard({
+    ts: true,
+    ignores: [
+      'public/vite*/*',
+      'coverage/*'
+    ]
+  }),
+  {
+    rules: {
+      'no-void': 'off' // gets confused with typescript void, it seems
+    }
+  }
+]
