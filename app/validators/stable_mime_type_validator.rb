533dafb8efbf19a3d1cc4325ac207bb04cc6076a
James Smith <james@floppy.org.uk>
2026-09-03 18:03:12 +0100

    Fix overly-aggressive file type validation check

--- app/validators/stable_mime_type_validator.rb
+++ app/validators/stable_mime_type_validator.rb
@@ -3,7 +3,9 @@ class StableMimeTypeValidator < ActiveModel::EachValidator
     return if !record.persisted? || record.attachment.nil? || value.nil?
     # i18n-tasks-use t("activerecord.errors.models.model_file.attributes.filename.cannot_change_type")
     new_filename = record.attachment.metadata["filename"] || record.attachment.id
-    record.errors.add attribute, :cannot_change_type if mime(value) != mime(new_filename)
+    if File.extname(new_filename).present? # If there's no extension, don't throw an error
+      record.errors.add attribute, :cannot_change_type if mime(value) != mime(new_filename)
+    end
   end
 
   def mime(value)
